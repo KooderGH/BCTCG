322e9fd046ff1e2c5d82597f3176f49d781547e8
Gideon9212 <gideon_gallagher@zoho.com>
2024-01-02 04:03:05 -0800

    MOMOCO RD (Kon test please)

--- script/c210660455.lua
+++ script/c210660455.lua
@@ -6,7 +6,7 @@
 -- (4) Once per your turn (Quick): You can negate all face-up card effects on your opponent side of the field until the End Phase.
 -- (5) During either player turn: You can Target 1 card on the field for each 2 cards you have in your Banish Zone; Banish them.
 -- (6) Once per turn (Igntion): You can banish 3 cards from your GY: Draw 1 card.
--- (7) If there is 30 or more card's total counting from both player's Banish Zone's, You can activate this effect: Set your opponent's LP to 0.
+-- (7) If there is 30 or more card's total counting from both player's Banish Zone's, You can activate this effect (Igntion): Set your opponent's LP to 0.
 -- (8) When this card is destroyed; Add 4 cards from your Banish Zone to your Deck, shuffle the deck, then draw 1 card.
 local s,id=GetID()
 function s.initial_effect(c)
@@ -99,6 +99,41 @@ function s.initial_effect(c)
 	e10:SetOperation(s.rmop)
 	e10:SetCountLimit(1)
 	c:RegisterEffect(e10)
+	--(5)Finish
+	--(6)Start
+	--Banish 3 GY Draw 1
+	local e11=Effect.CreateEffect(c)
+	e11:SetDescription(aux.Stringid(id,2))
+	e11:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
+	e11:SetType(EFFECT_TYPE_IGNITION)
+	e11:SetCountLimit(1)
+	e11:SetRange(LOCATION_MZONE)
+	e11:SetCost(s.redrawcost)
+	e11:SetTarget(s.redrawtarget)
+	e11:SetOperation(s.redrawop)
+	c:RegisterEffect(e11)
+	--(6)Finish
+	--(7)Start
+	--Set opp LP to 0
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,3))
+	e12:SetType(EFFECT_TYPE_IGNITION)
+	e12:SetCountLimit(1)
+	e12:SetRange(LOCATION_MZONE)
+	e12:SetCondition(s.wincon)
+	e12:SetOperation(s.winop)
+	c:RegisterEffect(e12)
+	--(7)Finish
+	--(8)Start
+	--When destroyed, shuffle 4 banish, draw 1
+	local e13=Effect.CreateEffect(c)
+	e13:SetDescription(aux.Stringid(id,4))
+	e13:SetCode(EVENT_DESTROYED)
+	e13:SetCountLimit(1)
+	e13:SetTarget(s.shufftarget)
+	e13:SetOperation(s.shuffop)
+	c:RegisterEffect(e13)
+	--(8)Finish
 end
 --(1) functions
 function s.banishfilter(c)
@@ -168,4 +203,59 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
     if #tg>0 then
         Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
     end
+end
+--(6)
+function s.discfilter(c)
+	return c:IsAbleToRemoveAsCost()
+end
+function s.redrawcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.discfilter,tp,LOCATION_GRAVE,0,3,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.discfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.redrawtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.redrawop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+--(7)
+function s.banishwinfilter(c)
+    return c:IsFaceup() or c:IsFacedown()
+end
+function s.wincon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetMatchingGroup(s.banishwinfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)>=30
+end 
+function s.winop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetLP(1-tp,0)
+end
+--(8)
+function s.dsfilter(c)
+	return c:IsAbleToDeck()
+end
+function s.shufftarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.dsfilter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+		and Duel.IsExistingTarget(s.dsfilter,tp,LOCATION_REMOVED,0,4,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.dsfilter,tp,LOCATION_REMOVED,0,4,4,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.shuffop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=4 then return end
+	Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
+	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
+	if ct==5 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
 end

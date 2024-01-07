9c9b91ffc896e05ffe3448a94cc1c49834e4b28c
Gideon9212 <gideon_gallagher@zoho.com>
2024-01-07 02:51:06 -0800

    Kyosaka Nanaho (215 lines of code)

--- script/c210660544.lua
+++ script/c210660544.lua
@@ -81,6 +81,37 @@ function s.initial_effect(c)
 	e9:SetTarget(s.handtg)
 	e9:SetOperation(s.handop)
 	c:RegisterEffect(e9)
+	--(4)Finish
+	--(5)Start
+	--Once while this card is face-up on the field: If it would be destroyed; gain 1000 ATK instead.
+	local e10=Effect.CreateEffect(c)
+	e10:SetCode(EFFECT_DESTROY_REPLACE)
+	e10:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e10:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetCountLimit(1)
+	e10:SetTarget(s.desreptg)
+	c:RegisterEffect(e10)
+	--(5)Finish
+	--(6)Start
+	--This card can attack your opponents monsters once each.
+	local e11=Effect.CreateEffect(c)
+	e11:SetType(EFFECT_TYPE_SINGLE)
+	e11:SetCode(EFFECT_ATTACK_ALL)
+	e11:SetValue(1)
+	c:RegisterEffect(e11)
+	--(6)Finish
+	--(7)Start
+	--Add this card from the GY to your hand
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,0))
+	e12:SetCategory(CATEGORY_TOHAND)
+	e12:SetType(EFFECT_TYPE_IGNITION)
+	e12:SetRange(LOCATION_GRAVE)
+	e12:SetCost(s.thcost)
+	e12:SetTarget(s.thtg)
+	e12:SetOperation(s.thop)
+	c:RegisterEffect(e12)
 end
 --(1)
 function s.ssummoncon(e,c)
@@ -124,3 +155,61 @@ function s.mvop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 --4
+function s.handtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,1-tp,1)
+	--Neither player can activate in response
+	Duel.SetChainLimit(aux.FALSE)
+end
+function s.handop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g==0 then return end
+	Duel.ConfirmCards(tp,g)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local hg=g:Select(tp,1,1,nil)
+	Duel.Remove(hg,POS_FACEUP,REASON_EFFECT)
+	local rg=Group.CreateGroup()
+	for tc in hg:Iter() do
+		if tc:IsLocation(LOCATION_REMOVED) then
+			local tpe=tc:GetType()
+			if (tpe&TYPE_TOKEN)==0 then
+				rg:Merge(Duel.GetMatchingGroup(Card.IsCode,tp,0,LOCATION_DECK+LOCATION_HAND,nil,tc:GetCode()))
+			end
+		end
+	end
+	if #rg>0 then
+		Duel.BreakEffect()
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+	end
+	Duel.ShuffleHand(1-tp)
+end
+--5
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(1000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+	c:RegisterEffect(e1)
+	return true
+end
+--7
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetDecktopGroup(tp,3)
+	if chk==0 then return g:FilterCount(Card.IsAbleToRemoveAsCost,nil,POS_FACEDOWN)==3
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 end
+	Duel.DisableShuffleCheck()
+	Duel.Remove(g,POS_FACEDOWN,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
+	end
+end

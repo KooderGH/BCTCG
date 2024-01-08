32357e6f58d50f0090b3c678b4bf36e8b484e1ae
Gideon9212 <gideon_gallagher@zoho.com>
2024-01-08 02:39:11 -0800

    Force push unfinished muu

--- script/c210660463.lua
+++ script/c210660463.lua
@@ -12,3 +12,196 @@
 -- * 3+: Machine type monsters you control gain 500 ATK/DEF for each Machine type monster you control.
 -- * 4+: Machine type monsters you control can attack your opponents LP directly.
 -- * 5+: Once per turn (Ignition): You can draw 2 cards.
+local s,id=GetID()
+function s.initial_effect(c)
+	--(1)Start
+	--Makes it unsummonable via normal
+	c:EnableUnsummonable()
+	--Cannot be SS by other ways other then it's own effect via above and this function
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+	--SS from Hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTargetRange(POS_FACEUP_DEFENSE,0)
+	e1:SetCondition(s.ssummoncon)
+	c:RegisterEffect(e1)
+	--Move to EMZ
+	local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetOperation(s.mvop)
+    c:RegisterEffect(e2)
+	--Summon cannot be disabled (Hopefully)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e3)
+	--(1)Finish
+	--(2)Start
+	--Cannot be Tributed
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e5)
+	--Cannot be returned to hand
+	local e6=e4:Clone()
+	e6:SetCode(EFFECT_CANNOT_TO_HAND)
+	c:RegisterEffect(e6)
+	--Cannot banish
+	local e7=e4:Clone()
+	e7:SetCode(EFFECT_CANNOT_REMOVE)
+	c:RegisterEffect(e7)
+	--(2)Finish
+	--(3)Start
+	--Cannot be targeted (self)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e8:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetValue(1)
+	c:RegisterEffect(e8)
+	--(3)Finish
+	--(4)Start
+	--This card's Position cannot be changed.
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE)
+	e9:SetCode(EFFECT_SET_POSITION)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetValue(POS_FACEUP_DEFENSE)
+	e9:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	c:RegisterEffect(e9)
+	--(4)Finish
+	--(5)Start
+	--Card's you control cannot be returned to hand or banished.
+	local e10=Effect.CreateEffect(c)
+	e10:SetType(EFFECT_TYPE_FIELD)
+	e10:SetCode(EFFECT_CANNOT_TO_HAND)
+	e10:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetTargetRange(LOCATION_ONFIELD,0)
+	c:RegisterEffect(e10)
+	local e11=e10:Clone()
+	e11:SetCode(EFFECT_CANNOT_REMOVE)
+	c:RegisterEffect(e11)
+	--(5)Finish
+	--(6)Start
+	--If this card is in your GY: You can set your LP to the same LPs your opponent has; Add this card to your hand.
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,0))
+	e12:SetCategory(CATEGORY_TOHAND)
+	e12:SetType(EFFECT_TYPE_IGNITION)
+	e12:SetRange(LOCATION_GRAVE)
+	e12:SetCost(s.graverecoverycost)
+	e12:SetTarget(s.graverecoverytg)
+	e12:SetOperation(s.graverecoveryop)
+	c:RegisterEffect(e12)
+	--(6)Finish
+	--(7)Start
+	--1+: Monsters your opponent control can only target this card for attacks. During each End Phase; This card gains 1000 DEF.
+	local e13=Effect.CreateEffect(c)
+	e13:SetType(EFFECT_TYPE_FIELD)
+	e13:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e13:SetRange(LOCATION_MZONE)
+	e13:SetTargetRange(0,LOCATION_MZONE)
+	e13:SetValue(s.atlimit)
+	e13:SetLabel(1)
+	e13:SetCondition(s.emachcountcondition)
+	c:RegisterEffect(e13)
+	--Add 1000 DEF each turn +1 effect
+	local e14=Effect.CreateEffect(c)
+	e14:SetDescription(aux.Stringid(id,1))
+	e14:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e14:SetRange(LOCATION_MZONE)
+	e14:SetCode(EVENT_PHASE+PHASE_END)
+	e14:SetCountLimit(1)
+	e14:SetCondition(s.defcon)
+	e14:SetOperation(s.defop)
+	c:RegisterEffect(e14)
+	--3+: Machine type monsters you control gain 500 ATK/DEF for each Machine type monster you control.
+	--4+: Machine type monsters you control can attack your opponents LP directly.
+	--5+: Once per turn (Ignition): You can draw 2 cards.
+end
+--1
+function s.emachfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE)
+end
+function s.ssummoncon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    return (Duel.GetLP(1-tp)-Duel.GetLP(tp))>=2000
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.GetFieldGroupCount(tp,LOCATION_EMZONE,0)==0
+        and (Duel.CheckLocation(c:GetControler(),LOCATION_EMZONE,0) or Duel.CheckLocation(c:GetControler(),LOCATION_EMZONE,1))
+        and Duel.IsExistingMatchingCard(s.emachfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.mvop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tp=c:GetControler()
+    if (Duel.CheckLocation(tp,LOCATION_EMZONE,0) or Duel.CheckLocation(tp,LOCATION_EMZONE,1)) then
+        local lftezm=not Duel.IsExistingMatchingCard(Card.IsSequence,tp,LOCATION_MZONE,0,1,nil,5) and 0x20 or 0
+        local rgtemz=not Duel.IsExistingMatchingCard(Card.IsSequence,tp,LOCATION_MZONE,0,1,nil,6) and 0x40 or 0
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+        local selected=Duel.SelectFieldZone(tp,1,LOCATION_MZONE,0,~ZONES_EMZ|(lftezm|rgtemz))
+        selected=selected==0x20 and 5 or 6
+        Duel.MoveSequence(c,selected)
+    end
+end
+--(6)
+function s.graverecoverycost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLp(0)~=Duel.GetLp(1) end
+	Duel.SetLP(tp,Duel.GetLp(1)
+end
+function s.graverecoverytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.graverecoveryop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
+	end
+end
+--(7)Core
+function s.emachfiltertwo(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and not C:IsCode(id)
+end
+function s.emachcountcondition(e)
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetMatchingGroupCount(s.emachfiltertwo,tp,LOCATION_MZONE,0,nil)>=e:GetLabel()
+end
+--Can only ATK
+function s.atlimit(e,c)
+	return c:IsFacedown() or not c:IsCode(id)
+end
+--Def 1000 function 
+function s.defcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and s.emachcountcondition(e)
+end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_DEFENSE)
+		e1:SetValue(1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end

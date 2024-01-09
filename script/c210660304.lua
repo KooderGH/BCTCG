29010680b61192a7f407664d67cafd749b8b9969
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-10 01:12:06 +0200

    Katapult finished. Time to get bullied by Gideon

---
+++ script/c210660304.lua
@@ -0,0 +1,80 @@
+--Mighty Kat-a-pult
+--Scripted by Konstak
+--Effect
+-- (1) Cannot be Normal Summon/Set. You can Special Summon this card from your hand by paying 500 LP.
+-- (2) Cannot be tributed.
+-- (3) Machine monster's you control gain 200 ATK for each machine monster on the field.
+-- (4) When this card is destroyed; You can add one Level 4 or lower Machine monster from your deck to your hand.
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableUnsummonable()
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--cannot be tributed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e3)
+	--Machine monsters player controls gain 200 ATK
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_UPDATE_ATTACK)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_MACHINE))
+	e4:SetValue(s.adval)
+	c:RegisterEffect(e4)
+	--Destroy and draw lv4 or lower machine
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetCategory(CATEGORY_DRAW)
+    e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetTarget(s.drtg)
+    e5:SetOperation(s.opdraw)
+    c:RegisterEffect(e5)
+end
+--Special summon function
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and
+		Duel.CheckLPCost(c:GetControler(),500)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.PayLPCost(tp,500)
+end
+--Atk update function
+function s.adval(e,c)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_MACHINE),c:GetControler(),LOCATION_MZONE,0,nil)*200
+end
+--Destroy and draw function
+function s.dfilter(c)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.opdraw(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

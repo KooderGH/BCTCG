9f66f0f9953d4e797e8ee5d6a8c528aa52c42056
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-11 20:17:30 +0200

    Deth-troy-r possibly finalized. Waiting for Gideon

---
+++ script/c210660632.lua
@@ -0,0 +1,123 @@
+--Mighty Deth-troy-r
+--Scripted by Konstak
+--Effect
+-- (1) You can Special Summon this card from your hand by paying 500 LP. You can only active this effect of "Mighty Deth-Troy-R" once per turn.
+-- (2) If this card is destroyed by battle; Destroy the monster it battled.
+-- (3) If this card is destroyed by a card effect; You can add one Machine type monster from your deck to your hand.
+-- (4) When this card is Summoned; You can add one Level 12 Machine type monster from your deck to your hand, then, discard one card. 
+-- ^ You can only active this effect of "Mighty Deth-Troy-R" once per turn.
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+    --once destroyed; destroy the monster it battled
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(s.descon)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
+	--once destroyed by card effect; add one machine type monster from deck to hand
+	local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(s.drecon)
+	e3:SetTarget(s.dretg)
+	e3:SetOperation(s.dreop)
+	c:RegisterEffect(e3)
+    --summon and add one lv12 machine monster from your deck to your hand
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetCost(s.smcost)
+    e4:SetTarget(s.smtg)
+    e4:SetOperation(s.smop)
+    e4:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
+    c:RegisterEffect(e4)
+    local e5=e4:Clone()
+    e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e5)
+    local e6=e4:Clone()
+    e6:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e6)
+end
+--Special Summon function (1)
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and
+		Duel.CheckLPCost(c:GetControler(),500)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.PayLPCost(tp,300)
+end
+--Destroy monster that destroyed it function (2)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local t=e:GetHandler():GetBattleTarget()
+	e:SetLabelObject(t)
+	return t 
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetLabelObject(),1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():IsRelateToBattle() then
+		Duel.Destroy(e:GetLabelObject(),REASON_EFFECT)
+	end
+end
+--Destroyed by effect and can add function (3)
+function s.drefilter(c)
+	return c:IsRace(RACE_MACHINE)
+end
+function s.drecon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function s.dretg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.drefilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.dreop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.drefilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--Summoned by effect and can add function (4)
+function s.smfilter(c)
+	return c:IsLevel(12) and c:IsRace(RACE_MACHINE)
+end
+function s.smcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+end
+function s.smtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.smfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.smop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.smfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+        Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+end

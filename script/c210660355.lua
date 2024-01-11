4625113db622d7a92e75bc5874f8a79d7cddf8fc
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-11 21:03:46 +0200

    Rekon korps possibly finalized. Waiting for Gideon

---
+++ script/c210660355.lua
@@ -0,0 +1,71 @@
+--Mighty Rekon Korps
+--Scripted by Konstak
+--Effect
+-- (1) Cannot be Normal Summoned/Set. Can be Special Summoned from your hand by paying 1000 LP while controlling at least 2 or more EARTH Machine type monsters.
+-- (2) Cannot be returned.
+-- (3) This card cannot inflict damage to your opponents LP.
+-- (4) When this card destroys a monster by battle; Add one EARTH Machine type monster from your deck to your hand.
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --Special Summon this card
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --Cannot be returned
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_CANNOT_TO_HAND)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+    --no battle damage
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    c:RegisterEffect(e3)
+    --When this card destroys a monster by battle; add one EARTH machine monster from deck to hand
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e4:SetCode(EVENT_BATTLE_DESTROYING)
+    e4:SetTarget(s.atktg)
+    e4:SetOperation(s.atkop)
+    c:RegisterEffect(e4)
+end
+--Special summon function
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(s.filter,c:GetControler(),LOCATION_MZONE,0,2,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.CheckLPCost(c:GetControler(),1000)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.PayLPCost(tp,1000)
+end
+--Destroy and add function
+function s.filter2(c)
+	return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

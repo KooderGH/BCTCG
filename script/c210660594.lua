2734b7514582b608d091525673370196d442a66f
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-12 20:30:09 +0200

    Aethur finalized. Waiting for Gideon

---
+++ script/c210660594.lua
@@ -0,0 +1,117 @@
+--Mighty Aethur Ltd.
+--Scripted by Konstak
+--Effect
+-- (1) If you control no monsters: You can Special Summon this card from your hand.
+-- (2) When this card is Summoned; you can Special Summon one LV4 or lower Earth Machine monster from your deck. You can only active this effect of "Mighty Aethur Ltd." once per turn.
+-- (3) Once per turn, if this card would be destroyed: Gain 500 ATK instead.
+-- (4) If this card battles a Zombie type monster; Banish it at the start of the damage step.
+-- (5) If you control 3 or more EARTH Machine type monsters: All monsters you control gain 800 ATK.
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+    --Summon and Special summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetTarget(s.sstg)
+    e2:SetOperation(s.ssop)
+    e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
+    c:RegisterEffect(e2)
+    local e3=e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    local e4=e2:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    --Destruction attack gain per turn
+    local e5=Effect.CreateEffect(c)
+    e5:SetCode(EFFECT_DESTROY_REPLACE)
+    e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+    e5:SetTarget(s.desatktg)
+    c:RegisterEffect(e5)
+    --Can banish zombie monsters
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,1))
+    e6:SetCategory(CATEGORY_DESTROY)
+    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e6:SetCode(EVENT_BATTLE_START)
+    e6:SetTarget(s.bntg)
+    e6:SetOperation(s.bnop)
+    c:RegisterEffect(e6)
+    --All monsters gain 800 ATK
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD)
+	e7:SetCode(EFFECT_UPDATE_ATTACK)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetTargetRange(LOCATION_MZONE,0)
+	e7:SetCondition(s.atkcon)
+	e7:SetValue(800)
+	c:RegisterEffect(e7)
+end
+--Special Summon Function
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--Once summoned SS machine monster function
+function s.specialfilter(c)
+	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+--Gain 500 once attacked function
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(500)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+	c:RegisterEffect(e1)
+	return true
+end
+--Banish Zombies function
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() then
+    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+--all monsters you control gain 800 for 3 Earth machines function
+function s.countfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE)
+end
+function s.atkcon(e)
+	return Duel.IsExistingMatchingCard(s.countfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,3,nil)
+end

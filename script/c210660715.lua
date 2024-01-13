c5960b4d149bfa10ddb609990fbfe9892972ece2
Gideon9212 <gideon_gallagher@zoho.com>
2024-01-13 00:38:07 -0800

    Mighty Sphinx Korps RD

--- script/c210660715.lua
+++ script/c210660715.lua
@@ -44,6 +44,15 @@ function s.initial_effect(c)
 	local e6=e4:Clone()
 	e6:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e6)
+	local e7=Effect.CreateEffect(c)
+    e7:SetDescription(aux.Stringid(id,1))
+    e7:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+    e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+    e7:SetCode(EVENT_DESTROYED)
+	e7:SetTarget(s.gravtg)
+	e7:SetOperation(s.gravop)
+	c:RegisterEffect(e7)
 end
 function s.spcon(e)
 	local tp=e:GetHandlerPlayer()
@@ -81,4 +90,29 @@ end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(e:GetOwnerPlayer(),LOCATION_HAND,0)
 	Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
+end
+--e7
+function s.gfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToDeck()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+		and Duel.IsExistingTarget(s.gfilter,tp,LOCATION_GRAVE,0,3,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.gfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=3 then return end
+	Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
+	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
+	if ct==3 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
 end

2e7695a776a9f76754d54a2cf1941c7de278d8e4
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-21 16:01:45 +0200

    placeholder scripts for the Garus

---
+++ script/c210660585.lua
@@ -0,0 +1,14 @@
+--Mighty Aegis Garu
+--Scripted by " "
+--Effect
+-- (1) When you take damage from a card in your opponent's possession: You can Special Summon this card from your hand.
+-- (2) When this card is Special Summoned by its effect: Activate the appropriate effect based on the type of damage:
+-- * Battle damage: Change all your opponent's monsters to face-down Defense Position. Monsters changed to face-down Defense Position by this effect cannot change their battle positions.
+-- * Effect damage: Gain LP equal to the amount of damage you took. This card gains DEF equal to the amount of damage you took.
+-- (3) During your opponent's end phase: If this is the only monster you control; You can add up to 2 cards from your GY to your hand.
+local s,id=GetID()
+function s.initial_effect(c)
+
+
+
+end
---
+++ script/c210660609.lua
@@ -0,0 +1,16 @@
+--Dark Aegis Garu
+--Scripted by " "
+--Effect
+-- (1) When you take damage from a card in your opponent's possession: You can Special Summon this card from your hand.
+-- (2) When this card is Special Summoned by its effect: Activate the appropriate effect based on the type of damage:
+-- * Battle damage: Destroy all monster's your opponent controls. You cannot attack the turn you use this effect.
+-- * Effect damage: Inflict damage to your opponent equal to double the damage you took. This card gains ATK equal to the damage you inflicted to your opponent this way.
+-- (3) Other monsters you control cannot attack.
+-- (4) During each end phase: This card gains 1000 ATK.
+-- (5) If this card ATK is 8000 or higher: This card can attack directly.
+local s,id=GetID()
+function s.initial_effect(c)
+
+
+
+end

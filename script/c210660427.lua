ae040d0e830a69661699a1f93a41feee6b569858
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-22 22:59:41 +0200

    Cat Clan Heroes scripted. Waiting for confirmation

---
+++ script/c210660427.lua
@@ -0,0 +1,135 @@
+--Cat Clan Heroes
+--Scripted by Konstak
+--Effect
+-- (1) If this card would be destroyed, it is returned to hand instead.
+-- (2) If this card is returned to hand, select one card your opponent controls, it cannot attack or activate it's effects until the end phase.
+-- (3) If this card is attacked; Banish this card, Negate the attack, and end the battle phase. Then draw 1 card.
+-- (4) You can banish this card from your GY; Gain 2000 LP's.
+local s,id=GetID()
+function s.initial_effect(c)
+    --Once destroyed send to hand (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_DESTROYED)
+    e1:SetValue(LOCATION_HAND)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+    --returned to hand, select card and cannot attack or activate it's effects until end phase. (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_TO_HAND)
+    e2:SetCondition(s.negcon)
+    e2:SetTarget(s.negtg)
+    e2:SetOperation(s.negop)
+    c:RegisterEffect(e2)
+    --if this card is attacked (3)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BE_BATTLE_TARGET)
+    e3:SetCondition(s.atkcon)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
+    --Banish recovery (4)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,3))
+    e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e4:SetCategory(CATEGORY_RECOVER)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCost(aux.selfbanishcost)
+    e4:SetTarget(s.gravetarget)
+    e4:SetOperation(s.graveoperation)
+    c:RegisterEffect(e4)
+end
+--(1)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.SendtoHand(e:GetHandler(),nil,0,REASON_EFFECT)
+    end
+end
+--(2)
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return c:IsPreviousLocation(LOCATION_ONFIELD) or c:IsPreviousLocation(LOCATION_GRAVE)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+    local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and ((tc:IsFaceup() and not tc:IsDisabled()) or tc:IsType(TYPE_TRAPMONSTER)) and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetValue(RESET_TURN_SET)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
+        if tc:IsType(TYPE_MONSTER) then
+            local e3=Effect.CreateEffect(c)
+            e3:SetType(EFFECT_TYPE_SINGLE)
+            e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+            e3:SetValue(1)
+            e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e3)
+            local e4=Effect.CreateEffect(c)
+            e4:SetType(EFFECT_TYPE_SINGLE)
+            e4:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+            e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e4)
+        end
+        if tc:IsType(TYPE_TRAPMONSTER) then
+            local e3=Effect.CreateEffect(c)
+            e3:SetType(EFFECT_TYPE_SINGLE)
+            e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+            e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+            e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e3)
+        end
+    end
+end
+--(3)
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler()==Duel.GetAttackTarget()
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateAttack() then
+        Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+        Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
+        Duel.BreakEffect()
+        Duel.Draw(tp,1,REASON_EFFECT)
+    end
+end
+--(4)
+function s.gravetarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    Duel.SetTargetPlayer(tp)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,0)
+end
+function s.graveoperation(e,tp,eg,ep,ev,re,r,rp)
+    local rt=2000
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    Duel.Recover(p,rt,REASON_EFFECT)
+end

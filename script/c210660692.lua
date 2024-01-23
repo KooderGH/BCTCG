c03def96af09368c40701e6cec9026c02ed92ea2
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-23 23:17:19 +0200

    Issun Boshi scripted!

--- script/c210660692.lua
+++ script/c210660692.lua
@@ -1,5 +1,5 @@
 --Issun Boshi
---Scripted by " "
+--Scripted by Konstak
 --Effect:
 -- (1) When this card is Summoned; Change its battle position to Defense Position.
 -- (2) This card can attack while in face-up Defense Position. If you use this effect, apply their DEF for damage calculation.
@@ -9,5 +9,114 @@
 -- (6) During your opponent's end phase: You can target 1 monster in your GY; Add it to your hand.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --summon and change to defense (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetOperation(s.smop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Attack in defense (2)
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_DEFENSE_ATTACK)
+    e4:SetValue(1)
+    c:RegisterEffect(e4)
+    --Can attack directly (3)
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetCode(EFFECT_DIRECT_ATTACK)
+    c:RegisterEffect(e5)
+    --target one card on the field, destroy it. (4)
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,1))
+    e6:SetCategory(CATEGORY_DESTROY)
+    e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e6:SetCode(EVENT_BATTLED)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCondition(s.descon)
+    e6:SetTarget(s.destg)
+    e6:SetOperation(s.desop)
+    c:RegisterEffect(e6)
+    --dam (5)
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_SINGLE)
+    e7:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    e7:SetValue(1)
+    c:RegisterEffect(e7)
+    --You can target 1 monster in your GY; Add it to your hand. (6)
+    local e8=Effect.CreateEffect(c)
+    e8:SetDescription(aux.Stringid(id,2))
+    e8:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e8:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e8:SetRange(LOCATION_MZONE)
+    e8:SetCode(EVENT_PHASE+PHASE_END)
+    e8:SetCountLimit(1)
+    e8:SetCondition(s.scon)
+    e8:SetTarget(s.stg)
+    e8:SetOperation(s.sop)
+    c:RegisterEffect(e8)
+end
+--(1)
+function s.smop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsAttackPos() then
+        Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+    end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_COPY_INHERIT)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    c:RegisterEffect(e1)
+end
+--(4)
+function s.desfilter(c)
+    return c:IsFaceup() and c:IsAttack(0)
+end
+function s.descon(e,c)
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--(6)
+function s.scon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==1-tp
+end
+function s.searchfilter(c,e,tp)
+    return c:IsAbleToHand()
+end
+function s.stg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.searchfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.sop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.searchfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end

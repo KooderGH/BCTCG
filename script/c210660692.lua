55dd67e06e814563e9f01ab2189cf3ac39334e3a
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-23 22:25:40 +0200

    Issun Boshi placeholder

---
+++ script/c210660692.lua
@@ -0,0 +1,13 @@
+--Issun Boshi
+--Scripted by " "
+--Effect:
+-- (1) When this card is Summoned; Change its battle position to Defense Position.
+-- (2) This card can attack while in face-up Defense Position. If you use this effect, apply their DEF for damage calculation.
+-- (3) This card can attack directly.
+-- (4) After damage calulation, If you control another 0 ATK monster: You can target one card on the field; destroy it.
+-- (5) You take no battle damage involving this card.
+-- (6) During your opponent's end phase: You can target 1 monster in your GY; Add it to your hand.
+local s,id=GetID()
+function s.initial_effect(c)
+
+end

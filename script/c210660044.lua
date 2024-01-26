606c7720f2a4f7210512a44ee70433b7b19b9862
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-26 23:22:50 +0200

    Lesser demon up to date

--- script/c210660044.lua
+++ script/c210660044.lua
@@ -1,50 +1,59 @@
 --Lesser Demon Cat
 --Scripted by Konstak.
 --Effect:
--- (1) Cannot attack the turn it is Summoned.
--- (2) If this card attacks, it is changed to Defense Position at the end of the Battle Phase, and its battle position cannot be changed until the End Phase of your next turn.
--- (3) If this card battled, at the end of the Damage Step; Draw 1 card
--- (4) If this card would be sent to the GY; Banish it instead.
+-- (1) If this card attacks, it is changed to Defense Position at the end of the Battle Phase, and its battle position cannot be changed until the End Phase of your next turn.
+-- (2) If this card battled, at the end of the Damage Step; Draw 1 card
+-- (3) If this card would be sent to the GY; Banish it instead.
+-- (4) During your opponent's end phase: You can target 1 monster your opponent controls; Destroy it and deal damage equal to that monster's original attack.
+-- (5) If you control no monsters: You can Special Summon this card from your Hand or GY. If summoned this way, your oppponent takes no battle damage involving this card.
 local s,id=GetID()
 function s.initial_effect(c)
-    --Cannot attack (1)
+    --to defense (1)
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetOperation(s.atklimit)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(s.poscon)
+    e1:SetOperation(s.posop)
     c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    --Draw once battled (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_DAMAGE_STEP_END)
+    e2:SetTarget(s.drtg)
+    e2:SetOperation(s.drop)
     c:RegisterEffect(e2)
-    local e3=e1:Clone()
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    --Banish once it's on GY (3)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+    e3:SetValue(LOCATION_REMOVED)
     c:RegisterEffect(e3)
-    --to defense (2)
+    --During your opponent's end phase, You can target 1 monster your opponent controls; Destroy it (4)
     local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e4:SetCode(EVENT_PHASE+PHASE_BATTLE)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
     e4:SetCountLimit(1)
-    e4:SetCondition(s.poscon)
-    e4:SetOperation(s.posop)
+    e4:SetCondition(s.drcon2)
+    e4:SetTarget(s.drtg2)
+    e4:SetOperation(s.drop2)
     c:RegisterEffect(e4)
-    --Draw once battled (3)
+    --SS if you have no monsters; If summoned this way, your oppponent takes no battle damage involving this card. (5)
     local e5=Effect.CreateEffect(c)
-    e5:SetDescription(aux.Stringid(id,0))
-    e5:SetCategory(CATEGORY_DRAW)
-    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e5:SetCode(EVENT_DAMAGE_STEP_END)
-    e5:SetTarget(s.drtg)
-    e5:SetOperation(s.drop)
+    e5:SetType(EFFECT_TYPE_FIELD)
+    e5:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e5:SetCode(EFFECT_SPSUMMON_PROC)
+    e5:SetRange(LOCATION_HAND)
+    e5:SetCondition(s.spcon)
+    e5:SetOperation(s.spop)
     c:RegisterEffect(e5)
-    --Banish once it's on GY (4)
-    local e6=Effect.CreateEffect(c)
-    e6:SetType(EFFECT_TYPE_SINGLE)
-    e6:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-    e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-    e6:SetValue(LOCATION_REMOVED)
-    c:RegisterEffect(e6)
 end
 --(1)
 function s.atklimit(e,tp,eg,ep,ev,re,r,rp)
@@ -79,4 +88,37 @@ end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
     local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
     Duel.Draw(tp,1,REASON_EFFECT)
+end
+--(4)
+function s.drcon2(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==1-tp
+end
+function s.drtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.drop2(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local tc=Duel.GetFirstTarget()
+    local dmg=tc:GetBaseAttack()
+    if tc:IsRelateToEffect(e) then
+        Duel.Destroy(tc,REASON_EFFECT)
+        Duel.Damage(1-tp,dmg,REASON_EFFECT)
+    end
+end
+--(5)
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    c:RegisterEffect(e1)
 end

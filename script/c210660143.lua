cebd55210d6cc3bad6945bd7e6f1f0e611e1a2e4
Gideon9212 <gideon_gallagher@zoho.com>
2024-01-26 04:20:43 -0800

    Nurse Cat buff (new effect)(Scripted)

--- script/c210660143.lua
+++ script/c210660143.lua
@@ -4,6 +4,7 @@
 -- (2) Once per turn, you can activate this effect: Gain 400 LP for each card on the field.
 -- (3) You take no Battle Damage from Battles involving this card.
 -- (4) You can banish this card from your GY (Quick); Gain 300 LP for each card in your GY.
+-- (5) If a monster(s) you control would be destroyed by battle or card effect, you can discard this card instead. You can only use this effect of "Nurse Cat" once per duel.
 local s,id=GetID()
 function s.initial_effect(c)
 	--recover banish hand (1)
@@ -47,6 +48,16 @@ function s.initial_effect(c)
 	e4:SetTarget(s.gravetarget)
 	e4:SetOperation(s.graveoperation)
 	c:RegisterEffect(e4)
+	--Replace (5)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EFFECT_DESTROY_REPLACE)
+	e5:SetRange(LOCATION_HAND)
+	e5:SetTarget(s.reptg)
+	e5:SetValue(s.repval)
+	e5:SetOperation(s.repop)
+	e5:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	c:RegisterEffect(e1)
 end
 --e1
 function s.btarget(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -80,4 +91,19 @@ function s.graveoperation(e,tp,eg,ep,ev,re,r,rp)
 	local rt=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)*300
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	Duel.Recover(p,rt,REASON_EFFECT)
+end
+--e5
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) 
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT+REASON_DISCARD)
 end

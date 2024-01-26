82d51639017760d7e3abe2c633430dbae26d1d13
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-01-26 22:13:09 +0200

    Ice cat text moment

--- script/c210660042.lua
+++ script/c210660042.lua
@@ -2,10 +2,10 @@
 --Scripted by Konstak
 --Effect
 -- (1) Cannot be Normal Summoned/Set. Can only be Special Summoned (from your hand) while you control no monsters.
--- (2) During either player's turn (Quick): You can Target 1 card on the field; negate it until the end of this turn. You can only use this effect of "Ice Cat" once per turn.
+-- (2) During either player's turn (Quick): You can Target 1 card on the field and banish this card on the field; negate it until the end of this turn. You can only use this effect of "Ice Cat" once per turn.
 -- (3) When this card is attacked; Double this card's attack until end of damage calulation.
--- (4) During your opponent's End Phase; Return this card to your hand, then draw 1 card.
--- (5) If this card is destroyed while on the field; Banish it, then end the current turn.
+-- (4) If you control 3 or more monsters; Destroy this card.
+-- (5) If this card is destroyed while on the field; Banish it.
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableUnsummonable()

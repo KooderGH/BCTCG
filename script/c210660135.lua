d3784699f7a3697ea372a5fa36d3a76ed77caec2
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-02-18 00:10:07 +0200

    Crane scripted. Waiting for confirmation!

---
+++ script/c210660135.lua
@@ -0,0 +1,97 @@
+--The Grateful Crane
+--Scripted by Konstak
+--Effect
+-- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
+-- (2) You take no battle damage involving this card.
+-- (3) You can only use 1 of these effects of "The Grateful Crane" per turn, and only once that turn.
+-- * When this card is Normal Summoned; Special Summon as many "The Grateful Crane" from your Deck, Hand, and/or GY as possible.
+-- * If this card is sent to the GY; Add 1 WIND monster from your GY except "The Grateful Crane" for each WIND monster you control.
+local s,id=GetID()
+function s.initial_effect(c)
+    --self destroy (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --No battle damage (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    c:RegisterEffect(e2)
+    --once normal summoned, Special summon as many grateful cranes as possible (3 *1)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_SUMMON_SUCCESS)
+    e3:SetCountLimit(1,id)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    --once sent to the graveyard destroy S/T based on the number of WIND machine you control (3 *2)
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e5:SetCode(EVENT_TO_GRAVE)
+    e5:SetCountLimit(1,id)
+    e5:SetTarget(s.rmtg)
+    e5:SetOperation(s.rmop)
+    c:RegisterEffect(e5)
+end
+s.listed_names={210660135}
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--Special summon
+function s.spfilter(c,e,tp)
+	return c:IsCode(210660135) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+--once sent to GY add Wind monsters from GY
+function s.rmfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_MACHINE)
+end
+function s.addfilter(c,e,tp)
+    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_MACHINE) and not c:IsCode(id)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chkc then return chkc:IsOnField() end
+    local gt=Duel.GetMatchingGroupCount(s.rmfilter,tp,LOCATION_MZONE,0,nil)
+    if chk==0 then return gt>0 and Duel.IsExistingTarget(nil,tp,LOCATION_GRAVE,0,gt,nil) end
+    local g=Duel.SelectTarget(tp,s.addfilter,tp,LOCATION_GRAVE,0,gt,gt,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local tg=Duel.GetTargetCards(e)
+    if #tg>0 then
+        Duel.SendtoHand(tg,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,tg)
+    end
+end

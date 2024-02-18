3cbf9b29baab9731e2f8513dd57456d2170992f6
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-02-18 20:22:40 +0200

    Placeholders for all non-SNS ultra soul

---
+++ script/c210660451.lua
@@ -0,0 +1,10 @@
+--Ushiwakamaru
+--Scripted by " "
+--Effect
+--
+local s,id=GetID()
+function s.initial_effect(c)
+
+
+
+end
---
+++ script/c210660692.lua
@@ -0,0 +1,10 @@
+--Issun Boshi
+--Scripted by " "
+--Effect
+--
+local s,id=GetID()
+function s.initial_effect(c)
+
+
+
+end

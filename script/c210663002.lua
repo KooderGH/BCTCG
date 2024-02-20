2801191885b654e140a1a9cb47c4591eaef84791
Konstak05 <72495166+Konstak05@users.noreply.github.com>
2024-02-20 04:57:19 +0200

    Type-K41 scripted!

--- script/c210663002.lua
+++ script/c210663002.lua
@@ -1,17 +1,20 @@
 --Type-K41 Defence
---Scripted by Konstak. (WiP)
+--Scripted by Konstak.
 --Effects:
---2 "Mighty Katapult" (Fusion Monster)
+--2 "Mighty Aethur Ltd." (Fusion Monster)
 --(1) Cannot be used as Fusion Material.
---(2) Must be Special Summoned only (from your Extra Deck) in Defense Position by sending 2 "Mighty Katapult" from your field to the GY. This Card Summon cannot be negated.
+--(2) Must be Special Summoned only (from your Extra Deck) in Defense Position by sending 2 "Mighty Aethur Ltd." from your field to the GY. This Card Summon cannot be negated.
 --(3) Cannot be returned to hand or Tributed.
 --(4) This card's Position cannot be changed.
---(5) Once per turn (Igntion): You can Special Summon 2 "Piledriver K41 Token" (Machine/DARK/Level 2/ATK 1000/DEF 2000).
+--(5) Once per turn (Igntion): You can Special Summon 2 "Piledriver K41 Token" (Machine/DARK/Level 2/ATK 1000/DEF 1500).
 --(6) Once per turn; You can Tribute 1 "Piledriver K41 Token" monster; You can choose 1 unused Main Monster Zone or Spell & Trap Zone; it cannot be used while this monster is face-up on the field.
---(7) You can only activate one of the following effects of "Type-K41 Defence" once per turn (Ignition):
---* You can Tribute 1 "Piledriver K41 Token" monster; Special Summon 1 "Driller K41 Token" (Machine/DARK/Level 2/ATK 1500/DEF 1500). These token's can attack your opponent directly.
---* You can Tribute 1 "Driller K41 Token" monster; Special Summon 1 "Thermae K41 Token" (Machine/DARK/Level 2/ATK 2500/DEF 3000). These token's can attack your opponents monsters once each.
---(8) While you control a Token, this card cannot be targeted for attacks and cannot be targeted by card effects.
+--(7) You can only activate two of the following effects of "Type-K41 Defence" once per turn (Ignition):
+--* You can Tribute 2 "Piledriver K41 Token" monster; Special Summon 1 "Driller K41 Token" (Machine/DARK/Level 2/ATK 1500/DEF 2000). These token's can attack your opponent directly.
+--* You can Tribute 1 "Driller K41 Token" monster; Special Summon 1 "Thermae K41 Token" (Machine/DARK/Level 2/ATK 2000/DEF 2500). These token's can attack your opponents monsters once each.
+--* You can Tribute 1 "Driller K41 Token" monster; Gain 1 Guard Counter.
+--* You can Tribute 1 "Thermae K41 Token" monster; Gain 2 Guard Counter.
+--(8) Monsters you control gain 1000 DEF for each Guard Counter on this card.
+--(9) While you control a Token, this card cannot be targeted for attacks and cannot be targeted by card effects.
 
 local s,id=GetID()
 function s.initial_effect(c)
@@ -81,7 +84,7 @@ function s.initial_effect(c)
     e9:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
     e9:SetType(EFFECT_TYPE_IGNITION)
     e9:SetRange(LOCATION_MZONE)
-    e9:SetCountLimit(1)
+    e9:SetCountLimit(2,{id,1},EFFECT_COUNT_CODE_OATH)
     e9:SetCost(s.zcost)
     e9:SetTarget(s.sptg2)
     e9:SetOperation(s.spop2)
@@ -92,16 +95,58 @@ function s.initial_effect(c)
     e10:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
     e10:SetType(EFFECT_TYPE_IGNITION)
     e10:SetRange(LOCATION_MZONE)
-    e10:SetCountLimit(1)
+    e10:SetCountLimit(2,{id,1},EFFECT_COUNT_CODE_OATH)
     e10:SetCost(s.zcost2)
     e10:SetTarget(s.sptg3)
     e10:SetOperation(s.spop3)
     c:RegisterEffect(e10)
+    --Place one counter when tribute Driller
+    local e11=Effect.CreateEffect(c)
+    e11:SetDescription(aux.Stringid(id,4))
+    e11:SetCategory(CATEGORY_COUNTER)
+    e11:SetType(EFFECT_TYPE_IGNITION)
+    e11:SetRange(LOCATION_MZONE)
+    e11:SetCountLimit(2,{id,1},EFFECT_COUNT_CODE_OATH)
+    e11:SetCost(s.zcost2)
+    e11:SetTarget(s.addct)
+    e11:SetOperation(s.addc)
+    c:RegisterEffect(e11)
+    --Place two counters when tribute Thermae
+    local e12=Effect.CreateEffect(c)
+    e12:SetDescription(aux.Stringid(id,5))
+    e12:SetCategory(CATEGORY_COUNTER)
+    e12:SetType(EFFECT_TYPE_IGNITION)
+    e12:SetRange(LOCATION_MZONE)
+    e12:SetCountLimit(2,{id,1},EFFECT_COUNT_CODE_OATH)
+    e12:SetCost(s.zcost3)
+    e12:SetTarget(s.addct2)
+    e12:SetOperation(s.addc2)
+    c:RegisterEffect(e12)
+    --Monsters player controls gain 1000 DEF
+    local e13=Effect.CreateEffect(c)
+    e13:SetType(EFFECT_TYPE_FIELD)
+    e13:SetCode(EFFECT_UPDATE_DEFENSE)
+    e13:SetRange(LOCATION_MZONE)
+    e13:SetTargetRange(LOCATION_MZONE,0)
+    e13:SetValue(s.atkval)
+    c:RegisterEffect(e13)
+    --Cannot be destroyed
+    local e14=Effect.CreateEffect(c)
+    e14:SetType(EFFECT_TYPE_SINGLE)
+    e14:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e14:SetRange(LOCATION_MZONE)
+    e14:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e14:SetCondition(s.indcon)
+    e14:SetValue(1)
+    c:RegisterEffect(e14)
+    local e15=e14:Clone()
+    e15:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    c:RegisterEffect(e15)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
 	if contact then sumtype=0 end
-	return c:IsType(ATTRIBUTE_EARTH,fc,sumtype,tp) and c:IsFaceup() and c:IsCode(210660304) and c:IsRace(RACE_MACHINE,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
+	return c:IsType(ATTRIBUTE_EARTH,fc,sumtype,tp) and c:IsFaceup() and c:IsCode(210660594) and c:IsRace(RACE_MACHINE,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
 end
 function s.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
@@ -125,18 +170,17 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,210668001,0,TYPES_TOKEN,1000,2000,2,RACE_MACHINE,ATTRIBUTE_DARK) then
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,210668001,0,TYPES_TOKEN,1000,1500,2,RACE_MACHINE,ATTRIBUTE_DARK) then
 		local token1=Duel.CreateToken(tp,210668001)
         local token2=Duel.CreateToken(tp,210668001)
 		Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
-		Duel.SpecialSummonComplete()
 		Duel.SpecialSummonStep(token2,0,tp,tp,false,false,POS_FACEUP)
 		Duel.SpecialSummonComplete()
 	end
 end
 --Remove Zone Function
 function s.tokenFilter(c)
-    return c:IsCode(210668001)
+    return c:IsFaceup() and c:IsCode(210668001)
 end
   function s.zcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.tokenFilter,1,false,nil,nil) end
@@ -177,10 +221,15 @@ function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,210668002,0,TYPES_TOKEN,1500,1500,2,RACE_MACHINE,ATTRIBUTE_DARK) then
-		local token1=Duel.CreateToken(tp,210668002)
-		Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
-		Duel.SpecialSummonComplete()
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,210668002,0,TYPES_TOKEN,1500,2000,2,RACE_MACHINE,ATTRIBUTE_DARK) then
+        local token1=Duel.CreateToken(tp,210668002)
+        Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
+        --direct attack
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DIRECT_ATTACK)
+        token1:RegisterEffect(e1)
+        Duel.SpecialSummonComplete()
 	end
 end
 --Special Summon Token 3 Function
@@ -202,9 +251,56 @@ function s.sptg3(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop3(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,210668003,0,TYPES_TOKEN,2500,3000,2,RACE_MACHINE,ATTRIBUTE_DARK) then
-		local token1=Duel.CreateToken(tp,210668003)
-		Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
-		Duel.SpecialSummonComplete()
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,210668003,0,TYPES_TOKEN,2000,2500,2,RACE_MACHINE,ATTRIBUTE_DARK) then
+        local token1=Duel.CreateToken(tp,210668003)
+        Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
+        --direct attack
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_ATTACK_ALL)
+        e1:SetValue(1)
+        token1:RegisterEffect(e1)
+        Duel.SpecialSummonComplete()
 	end
+end
+--Driller Guard Counter 
+s.counter_place_list={0x1021}
+function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1021)
+end
+function s.addc(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1021+COUNTER_NEED_ENABLE,1)
+	end
+end
+--Thermae Guard Counter
+function s.thermaeFilter(c)
+    return c:IsFaceup() and c:IsCode(210668003)
+end
+  function s.zcost3(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.thermaeFilter,1,false,nil,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local sg=Duel.SelectReleaseGroupCost(tp,s.thermaeFilter,1,1,false,nil,nil)
+    Duel.Release(sg,REASON_COST)
+end
+function s.addct2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,2,0,0x1021)
+end
+function s.addc2(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1021+COUNTER_NEED_ENABLE,2)
+	end
+end
+--Counter
+function s.atkval(e,c)
+	return Duel.GetCounter(0,1,1,0x1021)*1000
+end
+--Cannot be Attacked/Targeted by effects condition
+function s.TokenFilter(c)
+    return c:IsFaceup() and c:IsCode(210668001) or c:IsCode(210668002) or c:IsCode(210668003)
+end
+function s.indcon(e)
+    return Duel.IsExistingMatchingCard(s.TokenFilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end

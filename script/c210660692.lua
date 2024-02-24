25c241eabb5b2c7d2e575dc0e0b235df91885d8a
Konstak05 <zkonst05@gmail.com>
2024-02-24 17:20:07 +0200

    Unfinished Issun Boshi script

--- script/c210660692.lua
+++ script/c210660692.lua
@@ -1,10 +1,32 @@
 --Issun Boshi
---Scripted by " "
+--Scripted by Konstak
 --Effect
---
+-- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
+-- (2) This card can attack directly.
+-- (3) If you control 3 or more WIND monsters, this card cannot be targeted for attacks.
+-- (4) You can only use 1 of these effects of "Issun Boshi" per turn, and only once that turn.
+-- * When this card deals battle damage; Draw 1 card.
+-- * If this card is sent to the GY while your opponent controls more cards than the combined number of cards in your hand and that you control; Draw cards equal to their surplus. You cannot activate this effect if you control a non WIND monster.
 local s,id=GetID()
 function s.initial_effect(c)
-
-
-
+    --self destroy (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --Can attack directly
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_DIRECT_ATTACK)
+    c:RegisterEffect(e2)
+end
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end

3dc41b581c242239c4c0564f2064268542ea7fd0
Konstak05 <zkonst05@gmail.com>
2024-02-24 21:20:39 +0200

    e3 and e4 on Shitakiri

--- script/c210660633.lua
+++ script/c210660633.lua
@@ -17,21 +17,32 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_SELF_DESTROY)
     e1:SetCondition(s.sdcon)
     c:RegisterEffect(e1)
-	--Add counter
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetOperation(aux.chainreg)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetCode(EVENT_CHAIN_SOLVED)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetOperation(s.acop)
-	c:RegisterEffect(e3)
+    --Add counter
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetOperation(aux.chainreg)
+    c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+    e3:SetCode(EVENT_CHAIN_SOLVED)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetOperation(s.acop)
+    c:RegisterEffect(e3)
+    --add one wind monster from your deck or GY to hand
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCost(s.srcost)
+    e4:SetTarget(s.srtg)
+    e4:SetOperation(s.srop)
+    c:RegisterEffect(e4)
 end
+s.counter_place_list={COUNTER_SPELL}
 --Self Destroy Function
 function s.sdfilter(c)
     return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
@@ -43,5 +54,25 @@ end
 function s.acop(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
 		e:GetHandler():AddCounter(COUNTER_SPELL,1)
+	end
+end
+--Special Summon Search function
+function s.srcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,COUNTER_SPELL,2,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,COUNTER_SPELL,2,REASON_COST)
+end
+function s.srfilter(c)
+    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToHand()
+end
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end

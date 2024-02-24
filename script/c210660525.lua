9e8a7ab2a1175dc6db1f264344f753a358c31bf1
Konstak05 <zkonst05@gmail.com>
2024-02-24 18:20:18 +0200

    Kintaro scripted

--- script/c210660525.lua
+++ script/c210660525.lua
@@ -30,10 +30,22 @@ function s.initial_effect(c)
     e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e3:SetCountLimit(1,id)
     e3:SetTarget(s.sstg)
     e3:SetOperation(s.ssop)
-    e3:SetCountLimit(1,id)
     c:RegisterEffect(e3)
+    --once sent to the graveyard Discard 1 hand from your hand and target 1 card on the field
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e4:SetCode(EVENT_TO_GRAVE)
+    e4:SetCountLimit(1,id)
+    e4:SetCost(s.descost)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
 end
 --Self Destroy Function
 function s.sdfilter(c)
@@ -68,4 +80,23 @@ function s.ssop(e,tp,eg,ep,ev,re,r,rp)
         Duel.SendtoDeck(g2,nil,SEQ_DECKTOP,REASON_EFFECT)
         Duel.ConfirmDecktop(tp,1)
     end
+end
+--once sent to the graveyard function
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end

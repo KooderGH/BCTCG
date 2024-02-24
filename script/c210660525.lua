061eaa3549a0c114874d696daf7ade91a3cdfb3d
Konstak05 <zkonst05@gmail.com>
2024-02-24 18:10:26 +0200

    unfinished Issun Boshi script

--- script/c210660525.lua
+++ script/c210660525.lua
@@ -1,10 +1,71 @@
 --Kintaro
---Scripted by " "
+--Scripted by Konstak
 --Effect
---
+-- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
+-- (2) If you control no monster(s), you can Special Summon this card from your hand.
+-- (3) You can only use 1 of these effects of "Kintaro" per turn, and only once that turn.
+-- * If this card is Special Summoned; Add 2 WIND Machine monsters from your deck to your hand, then, place 1 card from your hand on top of your deck.
+-- * If this card is sent to the GY: Discard 1 card from your hand and target 1 card on the field; Destroy it.
 local s,id=GetID()
 function s.initial_effect(c)
-
-
-
+    --self destroy (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --Special Summon this card
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e2:SetCode(EFFECT_SPSUMMON_PROC)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetCondition(s.spcon)
+    c:RegisterEffect(e2)
+    --summon and add one WIND machine monster from your deck to your hand
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e3:SetTarget(s.sstg)
+    e3:SetOperation(s.ssop)
+    e3:SetCountLimit(1,id)
+    c:RegisterEffect(e3)
+end
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--Special Summon Function
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--Special Summon Search function
+function s.spfilter(c)
+    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,2,nil) and Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+    local g2=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_HAND,0,1,1,nil)
+    if #g2>0 then
+        Duel.SendtoDeck(g2,nil,SEQ_DECKTOP,REASON_EFFECT)
+        Duel.ConfirmDecktop(tp,1)
+    end
 end

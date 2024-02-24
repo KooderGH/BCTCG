34e98b0068d2ff2fbef6a28322fc26f20d1ba6a1
Konstak05 <zkonst05@gmail.com>
2024-02-24 18:31:12 +0200

    Sarukani anti-fairy?? based. (unfinished script)

--- script/c210660322.lua
+++ script/c210660322.lua
@@ -1,10 +1,44 @@
 --Sarukani
---Scripted by " "
+--Scripted by Konstak.
 --Effect
---
+-- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
+-- (2) If you control 2 or more WIND monsters, you can tribure 1 WIND monster to Special Summon this card from your hand.
+-- (3) During the damage step, if this card battles a Fairy or Zombie monster, this card gains 2900 ATK until the end of the damage step.
+-- (4) You can only use 1 of these effects of "Sarukani" per turn, and only once that turn.
+-- * If this card is in your GY: You can banish 1 WIND monster from your GY to add this card to your hand.
+-- * If this card is sent to the GY: You can tribute 1 WIND monster on your side of the field; Special Summon this card but it cannot attack this turn.
 local s,id=GetID()
 function s.initial_effect(c)
-
-
-
+    --self destroy (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --Special Summon this card (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e2:SetCode(EFFECT_SPSUMMON_PROC)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetCondition(s.spcon)
+    c:RegisterEffect(e2)
+end
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--Special Summon Function
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,3,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end

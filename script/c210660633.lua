30c727c8dc47ad830e2243a7246a5f5a698fde12
Konstak05 <zkonst05@gmail.com>
2024-02-24 20:47:03 +0200

    Shitakiri e2 added

--- script/c210660633.lua
+++ script/c210660633.lua
@@ -8,6 +8,7 @@
 -- * If this card is sent to the GY; For every 2 WIND monsters you control, add 1 Spell card from your GY to your hand.
 local s,id=GetID()
 function s.initial_effect(c)
+    c:EnableCounterPermit(COUNTER_SPELL)
     --self destroy (1)
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
@@ -16,8 +17,20 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_SELF_DESTROY)
     e1:SetCondition(s.sdcon)
     c:RegisterEffect(e1)
-
-
+	--Add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(aux.chainreg)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(s.acop)
+	c:RegisterEffect(e3)
 end
 --Self Destroy Function
 function s.sdfilter(c)
@@ -25,4 +38,10 @@ function s.sdfilter(c)
 end
 function s.sdcon(e)
     return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--add counter
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
+		e:GetHandler():AddCounter(COUNTER_SPELL,1)
+	end
 end

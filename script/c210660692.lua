582efd2f66b996d2a325d2650091534977ef4682
Konstak05 <zkonst05@gmail.com>
2024-02-24 17:46:08 +0200

    add e4 on Issun

--- script/c210660692.lua
+++ script/c210660692.lua
@@ -31,6 +31,15 @@ function s.initial_effect(c)
     e3:SetCondition(s.btcon)
     e3:SetValue(1)
     c:RegisterEffect(e3)
+    --battle damage
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DRAW)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_BATTLE_DAMAGE)
+    e4:SetCondition(s.bdcon)
+    e4:SetOperation(s.bdop)
+    c:RegisterEffect(e4)
 end
 --Self Destroy Function
 function s.sdfilter(c)
@@ -45,4 +54,11 @@ function s.btfilter(c)
 end
 function s.btcon(e)
     return Duel.IsExistingMatchingCard(s.btfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,3,nil)
+end
+--Battle damage function
+function s.bdcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp
+end
+function s.bdop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(tp,1,REASON_EFFECT)
 end

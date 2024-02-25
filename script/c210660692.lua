b8cacf9b17697ca1d2e8465d9143cdae6d2ad194
Konstak05 <zkonst05@gmail.com>
2024-02-25 04:17:39 +0200

    Unfinished Issun script

--- script/c210660692.lua
+++ script/c210660692.lua
@@ -40,7 +40,18 @@ function s.initial_effect(c)
     e4:SetCondition(s.bdcon)
     e4:SetOperation(s.bdop)
     c:RegisterEffect(e4)
-    --Missing e5
+    --once sent to the graveyard
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_DRAW)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e5:SetCode(EVENT_TO_GRAVE)
+    e5:SetCountLimit(1,id)
+	e5:SetCondition(s.drcon)
+	e5:SetTarget(s.drtg)
+	e5:SetOperation(s.drop)
+    c:RegisterEffect(e5)
 end
 --Self Destroy Function
 function s.sdfilter(c)
@@ -62,4 +73,30 @@ function s.bdcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.bdop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Draw(tp,1,REASON_EFFECT)
+end
+--Draw once sent to GY function
+function s.ndrfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+    if not Duel.IsExistingMatchingCard(s.ndrfilter,tp,LOCATION_MZONE,0,1,nil)
+	local t=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	local s=Duel.GetFieldGroupCount(tp,LOCATION_HAND+LOCATION_ONFIELD,0)
+	then return t>s
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local t=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	local s=Duel.GetFieldGroupCount(tp,LOCATION_HAND+LOCATION_ONFIELD,0)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,t-s) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(t-s)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,t-s)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local t=Duel.GetFieldGroupCount(p,0,LOCATION_ONFIELD)
+	local s=Duel.GetFieldGroupCount(p,LOCATION_HAND+LOCATION_ONFIELD,0)
+	if t>s then
+		Duel.Draw(p,t-s,REASON_EFFECT)
+	end
 end

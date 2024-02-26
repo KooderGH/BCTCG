32882d6510f2e1a111b9f1b8e5ff1be00fe03320
Gideon9212 <gideon_gallagher@zoho.com>
2024-02-26 01:07:02 -0800

    Courier Cat scripted

--- script/c210661658.lua
+++ script/c210661658.lua
@@ -22,6 +22,16 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     e2:SetOperation(s.sucop)
     c:RegisterEffect(e2)
+	--act limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetTargetRange(0,1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.actcon)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
 end
 --lcheck
 function s.lcheck(g,lc,sumtype,tp)
@@ -33,13 +43,21 @@ local c=e:GetHandler()
     local g=c:GetMaterial()
     local atk=0
     local tc=g:GetFirst()
-    for tc in aux.Next(g) do
-        local lk=tc:GetAttack()
-        atk=atk+lk
-    end
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetValue(atk)
-    e1:SetReset(RESET_EVENT+RESET)
+	for tc in aux.Next(g) do
+		local batk=tc:GetTextAttack()
+		if batk>0 then
+			atk=atk+batk
+		end
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	c:RegisterEffect(e1)
+end
+--e3
+function s.actcon(e)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
 end

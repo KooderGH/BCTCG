90a39601620203a3116ac45f54679f9182f90483
Konstak05 <zkonst05@gmail.com>
2024-03-01 18:59:21 +0200

    Nerf Thermae and Aethur

--- script/c210660417.lua
+++ script/c210660417.lua
@@ -3,7 +3,7 @@
 --Effect
 -- (1) If you control a EARTH Machine monster: You can Special Summon this card from your hand by paying 500 LP.
 -- (2) (Quick effect) You destroy all Set cards your opponent controls. You can only use this effect of Mighty Thermae D-Lux once per Duel.
--- (3) You can Tribute this card; Add 2 Level 4 or lower Earth Machine type monsters from your deck to your hand.
+-- (3) You can Tribute this card; you can Special Summon one LV4 or lower Earth Machine monster from your deck. You can only activate this effect of "Mighty Thermae D-Lux" once per turn.
 -- (4) If this card deals direct battle damage; You can add one Trap card from your Deck to your hand.
 local s,id=GetID()
 function s.initial_effect(c)
@@ -17,38 +17,38 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     --Destroy All set monsters opp controls
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
-	c:RegisterEffect(e2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
     --Tribute add
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCost(s.trcost)
-	e3:SetTarget(s.trtg)
-	e3:SetOperation(s.trop)
-	e3:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
-	c:RegisterEffect(e3)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCost(s.trcost)
+    e3:SetTarget(s.sstg)
+    e3:SetOperation(s.ssop)
+    e3:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
+    c:RegisterEffect(e3)
     --deal direct damage add
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetCategory(CATEGORY_SEARCH)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_BATTLE_DAMAGE)
-	e4:SetCondition(s.srcon)
-	e4:SetTarget(s.srtg)
-	e4:SetOperation(s.srop)
-	c:RegisterEffect(e4)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_BATTLE_DAMAGE)
+    e4:SetCondition(s.srcon)
+    e4:SetTarget(s.srtg)
+    e4:SetOperation(s.srop)
+    c:RegisterEffect(e4)
 end
 --Special summon function
 function s.specialfilter(c)
@@ -75,25 +75,26 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.destroyfilter,tp,0,LOCATION_ONFIELD,nil)
 	Duel.Destroy(g,REASON_EFFECT)
 end
---tribute add function 
-function s.tributefilter(c)
-	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand() and not c:IsCode(id)
-end
+--tribute sp function 
 function s.trcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
-function s.trtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tributefilter,tp,LOCATION_DECK,0,2,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+function s.specialfilter(c)
+	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
-function s.trop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.tributefilter,tp,LOCATION_DECK,0,2,2,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
 --deal direct damage add function
 function s.directfilter(c)
--- script/c210660594.lua
+++ script/c210660594.lua
@@ -2,29 +2,29 @@
 --Scripted by Konstak
 --Effect
 -- (1) If you control no monsters: You can Special Summon this card from your hand.
--- (2) When this card is Summoned; you can Special Summon one LV4 or lower Earth Machine monster from your deck. You can only active this effect of "Mighty Aethur Ltd." once per turn.
--- (3) Once per turn, if this card would be destroyed: Gain 500 ATK instead.
+-- (2) When this card is Summoned; Add 1 Level 4 or lower Earth Machine type monsters from your deck to your hand. You can only activate this effect of "Mighty Aethur Ltd." once per turn.
+-- (3) Once per turn, if this card would be destroyed by battle: Gain 500 ATK instead.
 -- (4) If this card battles a Zombie type monster; Banish it at the start of the damage step.
 -- (5) If you control 3 or more EARTH Machine type monsters: All monsters you control gain 400 ATK.
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon this card
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.spcon)
-	c:RegisterEffect(e1)
+    --Special Summon this card
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    c:RegisterEffect(e1)
     --Summon and Special summon
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetTarget(s.sstg)
-    e2:SetOperation(s.ssop)
+    e2:SetTarget(s.trtg)
+    e2:SetOperation(s.trop)
     e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
     c:RegisterEffect(e2)
     local e3=e2:Clone()
@@ -39,7 +39,7 @@ function s.initial_effect(c)
     e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
     e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e5:SetRange(LOCATION_MZONE)
-	e5:SetCountLimit(1)
+    e5:SetCountLimit(1)
     e5:SetTarget(s.desatktg)
     c:RegisterEffect(e5)
     --Can banish zombie monsters
@@ -52,14 +52,14 @@ function s.initial_effect(c)
     e6:SetOperation(s.bnop)
     c:RegisterEffect(e6)
     --All monsters gain 400 ATK
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_FIELD)
-	e7:SetCode(EFFECT_UPDATE_ATTACK)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetTargetRange(LOCATION_MZONE,0)
-	e7:SetCondition(s.atkcon)
-	e7:SetValue(400)
-	c:RegisterEffect(e7)
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_FIELD)
+    e7:SetCode(EFFECT_UPDATE_ATTACK)
+    e7:SetRange(LOCATION_MZONE)
+    e7:SetTargetRange(LOCATION_MZONE,0)
+    e7:SetCondition(s.atkcon)
+    e7:SetValue(400)
+    c:RegisterEffect(e7)
 end
 --Special Summon Function
 function s.spcon(e,c)
@@ -71,23 +71,22 @@ end
 function s.specialfilter(c)
 	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
-function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+function s.trtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.ssop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-    if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-    end
+function s.trop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
 --Gain 500 once attacked function
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) end
+	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)

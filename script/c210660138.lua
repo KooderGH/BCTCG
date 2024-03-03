7dea39a77e7e1e95709d80e603138622da394824
Konstak05 <zkonst05@gmail.com>
2024-03-03 02:44:17 +0200

    Kaguya Scripted

--- script/c210660138.lua
+++ script/c210660138.lua
@@ -1,11 +1,11 @@
 --Princess Kaguya
---Scripted by Konstak and Gideon
+--Scripted by Konstak
 --Effect
 -- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
 -- (2) If your opponent controls 2 or more monsters while you control a WIND Attribute monster, you can Special Summon this card from your hand.
 -- (3) You can only use 1 of these effects of "Princess Kaguya" per turn, and only once that turn.
--- * When this card is Summoned; Your Opponent Skips their next Battle Phase. You cannot activate this effect if your opponent has no monsters OR controls at least 1 WIND monser.
--- * If this card is sent to the GY: Target 1 monster on your opponent side of the field; It cannot attack while it's face-up on the field.
+-- * Once per turn: You can have all wind monsters gain 400 ATK for each wind monster you control until your opponent's end phase.
+-- * If this card is send to the GY; You can Special Summon one WIND Machine monster from your hand.
 local s,id=GetID()
 function s.initial_effect(c)
     --self destroy (1)
@@ -24,6 +24,26 @@ function s.initial_effect(c)
     e2:SetRange(LOCATION_HAND)
     e2:SetCondition(s.spcon)
     c:RegisterEffect(e2)
+    --gain atk
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id)
+    e3:SetTarget(s.atktg)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
+    --Once destroyed Special Summon 1 WIND Machine Monster
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetCountLimit(1,id)
+    e4:SetTarget(s.sstg)
+    e4:SetOperation(s.ssop)
+    c:RegisterEffect(e4)
 end
 --Self Destroy Function
 function s.sdfilter(c)
@@ -40,4 +60,42 @@ function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>=2 and Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--ATK Gain function
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil)
+    local tc=g:GetFirst()
+    while tc do
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetValue(s.adval)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,2)
+        tc:RegisterEffect(e1)
+        tc=g:GetNext()
+    end
+end
+function s.adval(e,c)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WIND),c:GetControler(),LOCATION_MZONE,0,nil)*400
+end
+--Once destroyed function
+function s.specialfilter(c)
+    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
 end

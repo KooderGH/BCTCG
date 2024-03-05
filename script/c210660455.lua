2a72da7fa9101887388d551208ae1252640c4443
Konstak05 <zkonst05@gmail.com>
2024-03-05 21:26:48 +0200

    Nerf Momoco

--- script/c210660455.lua
+++ script/c210660455.lua
@@ -1,12 +1,12 @@
 --Wonder MOMOCO
 --Scripted by Gideon. Got help from naim and pyrQ.
 -- (1) Cannot be Normal Summoned Set. Must be Special Summoned by its own effect and cannot be Special Summoned by other ways. This card Summon cannot be negated. You can Special Summon this card from your Hand, GY, or Banish Zone when there is 5 or more Monster's banished in your Banish Zone with different monster types. Then move this card to your Extra Monster Zone. 
--- (2) Cannot be returned to hand, banished, or tributed.
+-- (2) Cannot be returned to hand or tributed.
 -- (3) Cannot be targeted by card effects.
 -- (4) Once per your turn (Quick): You can negate all face-up card effects on your opponent side of the field until the End Phase.
 -- (5) During either player turn: You can Target 1 card on the field for each 2 cards you have in your Banish Zone; Banish them.
--- (6) Once per turn (Igntion): You can banish 3 cards from your GY: Draw 1 card.
--- (7) If there is 30 or more card's total counting from both player's Banish Zone's, You can activate this effect (Igntion): Set your opponent's LP to 0.
+-- (6) Once per turn (Igntion): You can banish 4 cards from your GY: Draw 1 card.
+-- (7) If there is 35 or more card's total counting from both player's Banish Zone's, You can activate this effect (Igntion): Set your opponent's LP to 0.
 -- (8) When this card is destroyed and set to the GY; Add 4 cards from your Banish Zone to your Deck, shuffle the deck, then draw 1 card.
 local s,id=GetID()
 function s.initial_effect(c)
@@ -58,83 +58,79 @@ function s.initial_effect(c)
 	local e6=e4:Clone()
 	e6:SetCode(EFFECT_CANNOT_TO_HAND)
 	c:RegisterEffect(e6)
-	--Cannot banish
-	local e7=e4:Clone()
-	e7:SetCode(EFFECT_CANNOT_REMOVE)
-	c:RegisterEffect(e7)
 	--(2)Finish
 	--(3)Start
 	--Cannot be targeted (self)
-	local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e8:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e8:SetRange(LOCATION_MZONE)
-	e8:SetValue(1)
-	c:RegisterEffect(e8)
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetValue(1)
+	c:RegisterEffect(e7)
 	--(3)Finish
 	--(4)Start
 	--Negate the effects of opp
-	local e9=Effect.CreateEffect(c)
-	e9:SetDescription(aux.Stringid(id,0))
-	e9:SetCategory(CATEGORY_DISABLE)
-	e9:SetType(EFFECT_TYPE_QUICK_O)
-	e9:SetCode(EVENT_FREE_CHAIN)
-	e9:SetRange(LOCATION_MZONE)
-	e9:SetCountLimit(1)
-	e9:SetTarget(s.distg)
-	e9:SetOperation(s.disop)
-	c:RegisterEffect(e9)
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,0))
+	e8:SetCategory(CATEGORY_DISABLE)
+	e8:SetType(EFFECT_TYPE_QUICK_O)
+	e8:SetCode(EVENT_FREE_CHAIN)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetCountLimit(1)
+	e8:SetTarget(s.distg)
+	e8:SetOperation(s.disop)
+	c:RegisterEffect(e8)
 	--(4)Finish
 	--(5)Start
 	--Banish
-	local e10=Effect.CreateEffect(c)
-	e10:SetDescription(aux.Stringid(id,1))
-	e10:SetCategory(CATEGORY_REMOVE)
-	e10:SetType(EFFECT_TYPE_QUICK_O)
-	e10:SetCode(EVENT_FREE_CHAIN)
-	e10:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e10:SetRange(LOCATION_MZONE)
-	e10:SetTarget(s.rmtg)
-	e10:SetOperation(s.rmop)
-	e10:SetCountLimit(1)
-	c:RegisterEffect(e10)
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,1))
+	e9:SetCategory(CATEGORY_REMOVE)
+	e9:SetType(EFFECT_TYPE_QUICK_O)
+	e9:SetCode(EVENT_FREE_CHAIN)
+	e9:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetTarget(s.rmtg)
+	e9:SetOperation(s.rmop)
+	e9:SetCountLimit(1)
+	c:RegisterEffect(e9)
 	--(5)Finish
 	--(6)Start
 	--Banish 3 GY Draw 1
-	local e11=Effect.CreateEffect(c)
-	e11:SetDescription(aux.Stringid(id,2))
-	e11:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
-	e11:SetType(EFFECT_TYPE_IGNITION)
-	e11:SetCountLimit(1)
-	e11:SetRange(LOCATION_MZONE)
-	e11:SetCost(s.redrawcost)
-	e11:SetTarget(s.redrawtarget)
-	e11:SetOperation(s.redrawop)
-	c:RegisterEffect(e11)
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,2))
+	e10:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
+	e10:SetType(EFFECT_TYPE_IGNITION)
+	e10:SetCountLimit(1)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetCost(s.redrawcost)
+	e10:SetTarget(s.redrawtarget)
+	e10:SetOperation(s.redrawop)
+	c:RegisterEffect(e10)
 	--(6)Finish
 	--(7)Start
 	--Set opp LP to 0
-	local e12=Effect.CreateEffect(c)
-	e12:SetDescription(aux.Stringid(id,3))
-	e12:SetType(EFFECT_TYPE_IGNITION)
-	e12:SetCountLimit(1)
-	e12:SetRange(LOCATION_MZONE)
-	e12:SetCondition(s.wincon)
-	e12:SetOperation(s.winop)
-	c:RegisterEffect(e12)
+	local e11=Effect.CreateEffect(c)
+	e11:SetDescription(aux.Stringid(id,3))
+	e11:SetType(EFFECT_TYPE_IGNITION)
+	e11:SetCountLimit(1)
+	e11:SetRange(LOCATION_MZONE)
+	e11:SetCondition(s.wincon)
+	e11:SetOperation(s.winop)
+	c:RegisterEffect(e11)
 	--(7)Finish
 	--(8)Start
 	--When destroyed, shuffle 4 banish, draw 1
-	local e13=Effect.CreateEffect(c)
-	e13:SetDescription(aux.Stringid(id,4))
-	e13:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e13:SetCode(EVENT_TO_GRAVE)
-	e13:SetCountLimit(1)
-	e13:SetCondition(s.shufflecon)
-	e13:SetTarget(s.shufftarget)
-	e13:SetOperation(s.shuffop)
-	c:RegisterEffect(e13)
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,4))
+	e12:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e12:SetCode(EVENT_TO_GRAVE)
+	e12:SetCountLimit(1)
+	e12:SetCondition(s.shufflecon)
+	e12:SetTarget(s.shufftarget)
+	e12:SetOperation(s.shuffop)
+	c:RegisterEffect(e12)
 	--(8)Finish
 end
 --(1) functions
@@ -211,9 +207,9 @@ function s.discfilter(c)
 	return c:IsAbleToRemoveAsCost()
 end
 function s.redrawcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.discfilter,tp,LOCATION_GRAVE,0,3,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.discfilter,tp,LOCATION_GRAVE,0,4,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.discfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	local g=Duel.SelectMatchingCard(tp,s.discfilter,tp,LOCATION_GRAVE,0,4,4,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.redrawtarget(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -231,7 +227,7 @@ function s.banishwinfilter(c)
     return c:IsFaceup() or c:IsFacedown()
 end
 function s.wincon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetMatchingGroupCount(s.banishwinfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)>=30
+    return Duel.GetMatchingGroupCount(s.banishwinfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)>=35
 end 
 function s.winop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SetLP(1-tp,0)

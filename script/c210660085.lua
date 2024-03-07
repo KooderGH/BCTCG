d1b1fa7e8c8021e3de2a1f94b80f57994460a7b4
Konstak05 <zkonst05@gmail.com>
2024-03-08 01:34:03 +0200

    Unfinished Vars script

--- script/c210660085.lua
+++ script/c210660085.lua
@@ -1,10 +1,62 @@
 --Vars
---Scripted by " "
+--Scripted by Senor Pizza. Help by Konstak
 --Effect
---
+--(1) You can Normal Summon this card without tribute if you control no monsters OR only control Dragon type monsters.
+--(2) When this card is Special Summoned: You can target one monster your opponent controls, return it.
+--(3) Neither player can Special Summon monsters while this card is face-up on the field.
+--(4) If you control a non-dragon monster on the field: Banish this card. (Mandatory)
+--(5) If this face-up card on the field is destroyed by a card effect; Destroy all Normal Summoned monster's on the field.
+--(6) Can be treated as 2 Tributes for the Tribute Summon of a Dragon monster.
 local s,id=GetID()
 function s.initial_effect(c)
-
-
-
+    --You can Normal Summon this card without tribute if you control no monsters OR only control Dragon type monsters.(1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_SUMMON_PROC)
+    e1:SetCondition(s.nscon)
+    c:RegisterEffect(e1)
+    --Once SS: return 1 card (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetTarget(s.retg)
+    e2:SetOperation(s.reop)
+    c:RegisterEffect(e2)
+	--disable spsummon (3)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	c:RegisterEffect(e3)
+    --Can be treated as 2 Tributes for the Tribute Summon of a Dragon monster.(6)
+    local e6=Effect.CreateEffect(c)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetCode(EFFECT_DOUBLE_TRIBUTE)
+    e6:SetValue(function (e,c) return c:IsRace(RACE_DRAGON) end)
+    c:RegisterEffect(e6)
+end
+function s.nscon(e,c)
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_DRAGON),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--e2
+function s.retg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.reop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
 end

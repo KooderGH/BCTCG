737a41f4d2f24fefad15b24803c5fe841ceb03c0
Konstak05 <zkonst05@gmail.com>
2024-03-11 23:11:47 +0200

    Update Gaia script

--- script/c210660493.lua
+++ script/c210660493.lua
@@ -1,6 +1,6 @@
 --Gaia the Creator
 --Scripted by Gideon
--- (1) Cannot be Normal Summoned Set. Must be Special Summoned by its own effect and cannot be Special Summoned by other ways. This card Summon cannot be negated. You can Special Summon this card from your hand or GY by discarding 3 card's from your hand while controlling no monsters. Then move this card to your Extra Monster Zone. 
+-- (1) Cannot be Normal Summoned Set. Must be Special Summoned by its own effect and cannot be Special Summoned by other ways. This card Summon cannot be negated. You can Special Summon this card from your hand or GY by discarding 3 card's from your hand while controlling no monsters. Then move this card to your Extra Monster Zone. You can only activate this effect once per duel.
 -- (2) Cannot be returned to hand, banished, or tributed.
 -- (3) Cannot be targeted by card effects. This effect cannot be negated.
 -- (4) While you control this card, you cannot Normal Summon / Set Monsters. This effect cannot be negated.
@@ -12,150 +12,151 @@
 -- (10)During the battle phase: This card is unaffected by other card effects.
 local s,id=GetID()
 function s.initial_effect(c)
-	--(1)Start
-	--Makes it unsummonable via normal
-	c:EnableUnsummonable()
-	--Cannot be SS by other ways other then it's own effect via above and this function
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.FALSE)
-	c:RegisterEffect(e0)
-	--SS from Hand / GY
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
-	e1:SetCondition(s.ssummoncon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1)
-	--Move to EMZ
-	local e2=Effect.CreateEffect(c)
+    --(1)Start
+    --Makes it unsummonable via normal
+    c:EnableUnsummonable()
+    --Cannot be SS by other ways other then it's own effect via above and this function
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e0:SetValue(aux.FALSE)
+    c:RegisterEffect(e0)
+    --SS from Hand / GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    e1:SetCondition(s.ssummoncon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --Move to EMZ
+    local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
     e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     e2:SetOperation(s.mvop)
     c:RegisterEffect(e2)
-	--Summon cannot be disabled (Hopefully)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	c:RegisterEffect(e3)
-	--(1)Finish
-	--(2)Start
-	--Cannot be Tributed
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetCode(EFFECT_UNRELEASABLE_SUM)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetValue(1)
-	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
-	c:RegisterEffect(e5)
-	--Cannot be returned to hand
-	local e6=e4:Clone()
-	e6:SetCode(EFFECT_CANNOT_TO_HAND)
-	c:RegisterEffect(e6)
-	--Cannot banish
-	local e7=e4:Clone()
-	e7:SetCode(EFFECT_CANNOT_REMOVE)
-	c:RegisterEffect(e7)
-	--(2)Finish
-	--(3)Start
-	--Cannot be targeted (self)
-	local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e8:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
-	e8:SetRange(LOCATION_MZONE)
-	e8:SetValue(1)
-	c:RegisterEffect(e8)
-	--(3)Finish
-	--(4)Start
-	--While you control this card, you cannot Normal Summon / Set Monsters.
-	local e9=Effect.CreateEffect(c)
-	e9:SetType(EFFECT_TYPE_FIELD)
-	e9:SetRange(LOCATION_MZONE)
-	e9:SetCode(EFFECT_CANNOT_SUMMON)
-	e9:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
-	e9:SetTargetRange(1,0)
-	c:RegisterEffect(e9)
-	local e10=e9:Clone()
-	e10:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
-	c:RegisterEffect(e10)
-	local e11=e9:Clone()
-	e11:SetCode(EFFECT_CANNOT_MSET)
-	c:RegisterEffect(e11)
-	--(4)Finish
-	--(5)Start
-	--Neither player can activate the effects of monsters in the hand. This effect cannot be negated.
-	local e12=Effect.CreateEffect(c)
-	e12:SetType(EFFECT_TYPE_FIELD)
-	e12:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
-	e12:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e12:SetRange(LOCATION_MZONE)
-	e12:SetTargetRange(1,1)
-	e12:SetValue(s.aclimit)
-	c:RegisterEffect(e12)
-	--(5)Finish
-	--(6)Start
-	--Inflict's Piercing Damage. This effect cannot be negated.
-	local e13=Effect.CreateEffect(c)
+    --Summon cannot be disabled (Hopefully)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    c:RegisterEffect(e3)
+    --(1)Finish
+    --(2)Start
+    --Cannot be Tributed
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e4:SetCode(EFFECT_UNRELEASABLE_SUM)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetValue(1)
+    c:RegisterEffect(e4)
+    local e5=e4:Clone()
+    e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+    c:RegisterEffect(e5)
+    --Cannot be returned to hand
+    local e6=e4:Clone()
+    e6:SetCode(EFFECT_CANNOT_TO_HAND)
+    c:RegisterEffect(e6)
+    --Cannot banish
+    local e7=e4:Clone()
+    e7:SetCode(EFFECT_CANNOT_REMOVE)
+    c:RegisterEffect(e7)
+    --(2)Finish
+    --(3)Start
+    --Cannot be targeted (self)
+    local e8=Effect.CreateEffect(c)
+    e8:SetType(EFFECT_TYPE_SINGLE)
+    e8:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e8:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e8:SetRange(LOCATION_MZONE)
+    e8:SetValue(1)
+    c:RegisterEffect(e8)
+    --(3)Finish
+    --(4)Start
+    --While you control this card, you cannot Normal Summon / Set Monsters.
+    local e9=Effect.CreateEffect(c)
+    e9:SetType(EFFECT_TYPE_FIELD)
+    e9:SetRange(LOCATION_MZONE)
+    e9:SetCode(EFFECT_CANNOT_SUMMON)
+    e9:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+    e9:SetTargetRange(1,0)
+    c:RegisterEffect(e9)
+    local e10=e9:Clone()
+    e10:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
+    c:RegisterEffect(e10)
+    local e11=e9:Clone()
+    e11:SetCode(EFFECT_CANNOT_MSET)
+    c:RegisterEffect(e11)
+    --(4)Finish
+    --(5)Start
+    --Neither player can activate the effects of monsters in the hand. This effect cannot be negated.
+    local e12=Effect.CreateEffect(c)
+    e12:SetType(EFFECT_TYPE_FIELD)
+    e12:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+    e12:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e12:SetRange(LOCATION_MZONE)
+    e12:SetTargetRange(1,1)
+    e12:SetValue(s.aclimit)
+    c:RegisterEffect(e12)
+    --(5)Finish
+    --(6)Start
+    --Inflict's Piercing Damage. This effect cannot be negated.
+    local e13=Effect.CreateEffect(c)
     e13:SetType(EFFECT_TYPE_SINGLE)
     e13:SetCode(EFFECT_PIERCE)
-	e13:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e13:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     c:RegisterEffect(e13)
-	--(6)Finish
-	--(7)Start
-	--Reverse trap
-	local e14=Effect.CreateEffect(c)
-	e14:SetType(EFFECT_TYPE_FIELD)
-	e14:SetCode(EFFECT_REVERSE_UPDATE)
-	e14:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
-	e14:SetRange(LOCATION_MZONE)
-	e14:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	c:RegisterEffect(e14)
-	--(7)Finish
-	--(8)Start
-	--Goyo on drugs
-	local e15=Effect.CreateEffect(c)
-	e15:SetDescription(aux.Stringid(id,0))
-	e15:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e15:SetCode(EVENT_BATTLE_DESTROYING)
-	e15:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e15:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e15:SetCondition(aux.bdogcon)
-	e15:SetTarget(s.battletg)
-	e15:SetOperation(s.battlepop)
-	c:RegisterEffect(e15)
-	--(8)Finish
-	--(9)Start
-	local e16=Effect.CreateEffect(c)
-	e16:SetDescription(aux.Stringid(id,1))
-	e16:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e16:SetCode(EVENT_LEAVE_FIELD)
-	e16:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e16:SetCondition(s.thcon)
-	e16:SetTarget(s.gtarget)
-	e16:SetOperation(s.gactivate)
-	c:RegisterEffect(e16)
-	--(9)Finish
-	--(10)Start
-	--During the battle phase: This card is unaffected by other card effects.
-	local e17=Effect.CreateEffect(c)
-	e17:SetType(EFFECT_TYPE_SINGLE)
-	e17:SetCode(EFFECT_IMMUNE_EFFECT)
-	e17:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e17:SetRange(LOCATION_MZONE)
-	e17:SetCondition(s.imcon)
-	e17:SetValue(1)
-	c:RegisterEffect(e17)
+    --(6)Finish
+    --(7)Start
+    --Reverse trap
+    local e14=Effect.CreateEffect(c)
+    e14:SetType(EFFECT_TYPE_FIELD)
+    e14:SetCode(EFFECT_REVERSE_UPDATE)
+    e14:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
+    e14:SetRange(LOCATION_MZONE)
+    e14:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+    c:RegisterEffect(e14)
+    --(7)Finish
+    --(8)Start
+    --Goyo on drugs
+    local e15=Effect.CreateEffect(c)
+    e15:SetDescription(aux.Stringid(id,0))
+    e15:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e15:SetCode(EVENT_BATTLE_DESTROYING)
+    e15:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e15:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e15:SetCondition(aux.bdogcon)
+    e15:SetTarget(s.battletg)
+    e15:SetOperation(s.battlepop)
+    c:RegisterEffect(e15)
+    --(8)Finish
+    --(9)Start
+    local e16=Effect.CreateEffect(c)
+    e16:SetDescription(aux.Stringid(id,1))
+    e16:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e16:SetCode(EVENT_LEAVE_FIELD)
+    e16:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+    e16:SetCondition(s.thcon)
+    e16:SetTarget(s.gtarget)
+    e16:SetOperation(s.gactivate)
+    c:RegisterEffect(e16)
+    --(9)Finish
+    --(10)Start
+    --During the battle phase: This card is unaffected by other card effects.
+    local e17=Effect.CreateEffect(c)
+    e17:SetType(EFFECT_TYPE_SINGLE)
+    e17:SetCode(EFFECT_IMMUNE_EFFECT)
+    e17:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e17:SetRange(LOCATION_MZONE)
+    e17:SetCondition(s.imcon)
+    e17:SetValue(1)
+    c:RegisterEffect(e17)
 end
 --(1)
 function s.ssummoncon(e,c)

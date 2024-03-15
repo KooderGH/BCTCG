a71d8f4cf80f81256b0b1687362b6f1334d38999
Konstak05 <zkonst05@gmail.com>
2024-03-15 23:57:26 +0200

    Buff Unit-00 #1

--- script/c210660412.lua
+++ script/c210660412.lua
@@ -1,53 +1,69 @@
 --Eva Unit-00
---Scripted by Konstak, help from Gideon.
+--Scripted by Konstak, help from Gideon, Buffed By Konstak
 --Effect:
---(1) When summoned, place in defense. 
---(2) Card's cannot be returned to hand while this card is face-up on the field.
---(3) Cannot be destroyed by battle.
---(4) During your standby phase: If this card is the only monster you control; Draw 1 card.
+-- (1) You can Special Summon this card from your hand if you control no Monsters OR if you control a LIGHT Monster.
+-- (2) When this card is Summoned, It is placed in defense position.
+-- (3) Card's cannot be returned to hand while this card is face-up on the field.
+-- (4) Cannot be destroyed by battle.
+-- (5) During your standby phase: If this card is the only monster you control; Draw 1 card.
 local s,id=GetID()
 function s.initial_effect(c)
+	-- Special Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
     --to defense
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_POSITION)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetTarget(s.deftg)
-    e1:SetOperation(s.defop)
-    c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_POSITION)
+    e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetTarget(s.deftg)
+    e2:SetOperation(s.defop)
     c:RegisterEffect(e2)
-    local e3=e1:Clone()
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    local e3=e2:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e3)
-    --to cannot return to hand
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_FIELD)
-    e4:SetCode(EFFECT_CANNOT_TO_HAND)
-    e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
+    local e4=e2:Clone()
+    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e4)
-    --battle indes
+    --to cannot return to hand
     local e5=Effect.CreateEffect(c)
-    e5:SetType(EFFECT_TYPE_SINGLE)
-    e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e5:SetValue(1)
+    e5:SetType(EFFECT_TYPE_FIELD)
+    e5:SetCode(EFFECT_CANNOT_TO_HAND)
+    e5:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
     c:RegisterEffect(e5)
-    --to draw 1 card
+    --battle indes
     local e6=Effect.CreateEffect(c)
-    e6:SetDescription(aux.Stringid(id,0))
-    e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-    e6:SetCategory(CATEGORY_DRAW)
-    e6:SetCode(EVENT_PHASE+PHASE_STANDBY)
-    e6:SetRange(LOCATION_MZONE)
-    e6:SetCountLimit(1)
-    e6:SetCondition(s.condition)
-    e6:SetOperation(s.operation)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e6:SetValue(1)
     c:RegisterEffect(e6)
+    --to draw 1 card
+    local e7=Effect.CreateEffect(c)
+    e7:SetDescription(aux.Stringid(id,1))
+    e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e7:SetCategory(CATEGORY_DRAW)
+    e7:SetCode(EVENT_PHASE+PHASE_STANDBY)
+    e7:SetRange(LOCATION_MZONE)
+    e7:SetCountLimit(1)
+    e7:SetCondition(s.condition)
+    e7:SetOperation(s.operation)
+    c:RegisterEffect(e7)
 end
+--e1
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_LIGHT),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--e2
 function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return e:GetHandler():IsAttackPos() end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
@@ -58,6 +74,7 @@ function s.defop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
 	end
 end
+--e3
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp==Duel.GetTurnPlayer() and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
 end

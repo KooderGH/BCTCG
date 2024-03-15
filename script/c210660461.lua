f40624daa752bb77248ebaa3362f91df17aa9106
Konstak05 <zkonst05@gmail.com>
2024-03-16 01:48:11 +0200

    Revert Legelan script so it's tribute immune

--- script/c210660461.lua
+++ script/c210660461.lua
@@ -3,7 +3,7 @@
 --Effect
 -- (1) Cannot be Normal Summoned or Set. Can only be Special Summoned from your hand by Tributing 2 Fiend monsters you control, then move this card to your Extra Monster Zone. You can only activate this effect once per duel.
 -- (2) This cards Summon and Effects cannot be negated.
--- (3) Cannot be returned to hand or banished.
+-- (3) Cannot be returned to hand, banished, or tributed while on the field.
 -- (4) After 10 turns have passed after you Summoned this card (counting the turn you Summoned this card as the 1st turn), you win the Duel.
 -- (5) Once per turn during your Main phase: You can Special Summon one Fiend monster from your hand or GY.
 -- (6) If you control 4 or more Fiend monsters, your opponent cannot enter the battle phase.
@@ -49,24 +49,40 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
     --(2)Finish
     --(3)Start
-    --Cannot be returned to hand
+    --Cannot be Tributed
     local e4=Effect.CreateEffect(c)
     e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
-    e4:SetCode(EFFECT_CANNOT_TO_HAND)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_NEGATE)
+    e4:SetCode(EFFECT_UNRELEASABLE_SUM)
     e4:SetRange(LOCATION_MZONE)
     e4:SetValue(1)
     c:RegisterEffect(e4)
-    --Cannot banish
     local e5=e4:Clone()
-    e5:SetCode(EFFECT_CANNOT_REMOVE)
+    e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
     c:RegisterEffect(e5)
+    --Cannot be returned to hand
+    local e6=e4:Clone()
+    e6:SetCode(EFFECT_CANNOT_TO_HAND)
+    c:RegisterEffect(e6)
+    --Cannot banish while on the field
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_SINGLE)
+    e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e7:SetCode(EFFECT_CANNOT_REMOVE)
+    e7:SetRange(LOCATION_MZONE)
+    c:RegisterEffect(e7)
+    local e8=e7:Clone()
+    e8:SetType(EFFECT_TYPE_FIELD)
+    e8:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e8:SetTargetRange(1,1)
+    e8:SetTarget(s.rmlimit)
+    c:RegisterEffect(e8)
     --(3)Finish
     --(4)Start
-    local e6=Effect.CreateEffect(c)
-    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS+EFFECT_FLAG_CANNOT_NEGATE)
-    e6:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e6:SetOperation(s.activate)
+    local e9=Effect.CreateEffect(c)
+    e9:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS+EFFECT_FLAG_CANNOT_NEGATE)
+    e9:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e9:SetOperation(s.activate)
     aux.GlobalCheck(s,function()
         local ge1=Effect.CreateEffect(c)
         ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -80,38 +96,38 @@ function s.initial_effect(c)
         ge2:SetOperation(s.winop)
         Duel.RegisterEffect(ge2,0)
         end)
-    c:RegisterEffect(e6)
+    c:RegisterEffect(e9)
     --(4)Finish
     --(5)Start
     --Special summon a fiend
-    local e7=Effect.CreateEffect(c)
-    e7:SetDescription(aux.Stringid(id,0))
-    e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e7:SetType(EFFECT_TYPE_IGNITION)
-    e7:SetProperty(EFFECT_FLAG_CANNOT_NEGATE)
-    e7:SetCountLimit(1,{id,1})
-    e7:SetRange(LOCATION_MZONE)
-    e7:SetTarget(s.specialfiendtarget)
-    e7:SetOperation(s.specialfiendop)
-    c:RegisterEffect(e7)
+    local e10=Effect.CreateEffect(c)
+    e10:SetDescription(aux.Stringid(id,0))
+    e10:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e10:SetType(EFFECT_TYPE_IGNITION)
+    e10:SetProperty(EFFECT_FLAG_CANNOT_NEGATE)
+    e10:SetCountLimit(1,{id,1})
+    e10:SetRange(LOCATION_MZONE)
+    e10:SetTarget(s.specialfiendtarget)
+    e10:SetOperation(s.specialfiendop)
+    c:RegisterEffect(e10)
     --(5)Finish
     --(6)Start
-    local e8=Effect.CreateEffect(c)
-    e8:SetType(EFFECT_TYPE_FIELD)
-    e8:SetCode(EFFECT_CANNOT_BP)
-    e8:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_NEGATE)
-    e8:SetRange(LOCATION_MZONE)
-    e8:SetTargetRange(0,1)
-    e8:SetCondition(s.bpcond)
-    c:RegisterEffect(e8)
+    local e11=Effect.CreateEffect(c)
+    e11:SetType(EFFECT_TYPE_FIELD)
+    e11:SetCode(EFFECT_CANNOT_BP)
+    e11:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_NEGATE)
+    e11:SetRange(LOCATION_MZONE)
+    e11:SetTargetRange(0,1)
+    e11:SetCondition(s.bpcond)
+    c:RegisterEffect(e11)
     --(6)Finish
     --(7)Start
-    local e9=Effect.CreateEffect(c)
-    e9:SetType(EFFECT_TYPE_SINGLE)
-    e9:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-    e9:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-    e9:SetValue(LOCATION_REMOVED)
-    c:RegisterEffect(e9)
+    local e12=Effect.CreateEffect(c)
+    e12:SetType(EFFECT_TYPE_SINGLE)
+    e12:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+    e12:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+    e12:SetValue(LOCATION_REMOVED)
+    c:RegisterEffect(e12)
     --(7)Finish
 end
 --(1) functions
@@ -151,6 +167,10 @@ function s.mvop(e,tp,eg,ep,ev,re,r,rp)
         selected=selected==0x20 and 5 or 6
         Duel.MoveSequence(c,selected)
     end
+end
+--(3) functions
+function s.rmlimit(e,c,tp,r,re)
+    return c==e:GetHandler() and re:GetHandler()~=e:GetHandler()
 end
 --(4) functions
 function s.activate(e,tp,eg,ep,ev,re,r,rp)

cf5f5829abe57537694090828085f24096c704b0
Konstak05 <zkonst05@gmail.com>
2024-03-21 17:20:07 +0200

    SnS and script Seal

--- script/c210662008.lua
+++ script/c210662008.lua
@@ -1,5 +1,10 @@
--- Blank
+-- Sir Seal
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --This card can attack your opponents monsters once each.
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_ATTACK_ALL)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
 end

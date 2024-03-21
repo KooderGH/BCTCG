d7ed4ca1b97b7360124bd93e42fe24bae68ae913
Konstak05 <zkonst05@gmail.com>
2024-03-21 20:48:39 +0200

    add credit

--- script/c210662005.lua
+++ script/c210662005.lua
@@ -1,4 +1,5 @@
 -- Jackie Peng
+--Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
     --direct attack
--- script/c210662010.lua
+++ script/c210662010.lua
@@ -1,4 +1,5 @@
 -- Kang Roo
+--Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
     --pierce

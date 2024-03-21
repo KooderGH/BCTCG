0e1bcf4a2c2404fe4f4bbc46f27652858ae2ffa4
Konstak05 <zkonst05@gmail.com>
2024-03-21 19:53:05 +0200

    Nerf Black enemies further

--- script/c210662046.lua
+++ script/c210662046.lua
@@ -1,6 +1,15 @@
 -- Doge Dark
 local s,id=GetID()
 function s.initial_effect(c)
+	c:EnableUnsummonable()
+	-- Special Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
     --draw
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_DRAW)	
@@ -15,6 +24,11 @@ function s.initial_effect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
     c:RegisterEffect(e2)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(1-tp,1) end

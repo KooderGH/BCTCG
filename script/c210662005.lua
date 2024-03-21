5efc9b6269d6b656a6e7b6167109d89bfd497a47
Konstak05 <zkonst05@gmail.com>
2024-03-21 19:59:15 +0200

    Fix IDs

--- script/c210662005.lua
+++ script/c210662005.lua
@@ -1,24 +1,9 @@
--- Pigge
---Scripted by Konstak
+-- Jackie Peng
 local s,id=GetID()
 function s.initial_effect(c)
-    --draw
+    --direct attack
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DRAW)	
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTarget(s.drtg)
-    e1:SetOperation(s.drop)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_DIRECT_ATTACK)
     c:RegisterEffect(e1)
-end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)	
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    Duel.Draw(p,d,REASON_EFFECT)
 end

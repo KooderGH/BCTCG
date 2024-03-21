4db54ec56a1890758489dc86523eb09da88eb575
Konstak05 <zkonst05@gmail.com>
2024-03-21 20:16:20 +0200

    SnS and script Kang Roo

--- script/c210662010.lua
+++ script/c210662010.lua
@@ -1,9 +1,9 @@
--- Jackie Peng
+-- Kang Roo
 local s,id=GetID()
 function s.initial_effect(c)
-    --direct attack
+    --pierce
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_DIRECT_ATTACK)
+    e1:SetCode(EFFECT_PIERCE)
     c:RegisterEffect(e1)
 end

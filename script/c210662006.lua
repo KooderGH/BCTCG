cb7d538009940a072057bffaa74643572ddc2fee
Konstak05 <zkonst05@gmail.com>
2024-03-21 19:44:58 +0200

    Script Jackie Peng

--- script/c210662006.lua
+++ script/c210662006.lua
@@ -1,5 +1,9 @@
--- Blank
+-- Jackie Peng
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --direct attack
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_DIRECT_ATTACK)
+    c:RegisterEffect(e1)
 end

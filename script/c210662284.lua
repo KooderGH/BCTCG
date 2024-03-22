b2c4a3a81647a7f14d0167d78ae642eeb70310c5
Konstak05 <zkonst05@gmail.com>
2024-03-22 19:56:08 +0200

    Revive attempt 1

--- script/c210662284.lua
+++ script/c210662284.lua
@@ -1,5 +1,59 @@
--- Blank
+-- Zoge
+--Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --Counters (1)
+    c:EnableCounterPermit(0x4003)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_COUNTER)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.addct)
+    e1:SetOperation(s.addc)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Revive (2)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COUNTER)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCost(s.sumcost)
+    e4:SetTarget(s.sumtg)
+    e4:SetOperation(s.sumop)
+    c:RegisterEffect(e4)
+end
+--e2
+s.counter_list={0x4003}
+function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x4003)
+end
+function s.addc(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x4003,1)
+	end
+end
+--SS from GY function
+function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x4003,1,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x4003,1,REASON_COST)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+	end
 end

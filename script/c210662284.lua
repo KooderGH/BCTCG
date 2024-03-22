b5c03d3912e8a0e11860a906713c26eec36bf757
Konstak05 <zkonst05@gmail.com>
2024-03-22 21:54:34 +0200

    Zoge reworked again

--- script/c210662284.lua
+++ script/c210662284.lua
@@ -2,33 +2,69 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-    --Revive
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,1))
-    e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COUNTER)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetRange(LOCATION_GRAVE)
-    e1:SetCondition(s.sumcon)
-    e1:SetTarget(s.sumtg)
-    e1:SetOperation(s.sumop)
-    c:RegisterEffect(e1)
+	--Place as Spell (3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+	e1:SetTarget(s.spelltarget)
+	e1:SetOperation(s.spellop)
+	c:RegisterEffect(e1)
+	--Target 2400 or lower as spell then SS to opp
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetCondition(s.destcondition)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
 end
---1
-function s.sumcon(e,tp,c)
-	if c==nil then return true end
-	return tp==Duel.GetTurnPlayer() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-		Duel.CheckLPCost(tp,500)
+--e1
+function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
 end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spellop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+	c:RegisterEffect(e1)
 end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-        Duel.PayLPCost(tp,500)
-        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+--e2
+function s.destcondition(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
+end
+function s.desfilter(c,atk)
+	return c:IsFaceup()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,c:GetAttack())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		local c=e:GetHandler()
+		if c:IsRelateToEffect(e) then
+			Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+		end
 	end
 end

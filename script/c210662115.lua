d1d95e07e97333f7dfe275c22aed2f5ffc3df25c
Konstak05 <zkonst05@gmail.com>
2024-03-23 00:27:49 +0200

    Heavenly Hippoe and Angelic Gory added and scripted

---
+++ script/c210662115.lua
@@ -0,0 +1,38 @@
+-- Angelic Gory
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableUnsummonable()
+	-- Special Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+    --draw
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DRAW)	
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetTarget(s.drtg)
+    e2:SetOperation(s.drop)
+    c:RegisterEffect(e2)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_LIGHT),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(1-tp,1) end
+    Duel.SetTargetPlayer(1-tp)
+    Duel.SetTargetParam(1)	
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    Duel.Draw(p,d,REASON_EFFECT)
+end

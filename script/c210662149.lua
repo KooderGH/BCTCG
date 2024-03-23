ccd901e5969aa722008f13626321d8632aede293
Konstak05 <zkonst05@gmail.com>
2024-03-23 03:46:53 +0200

    Le'noir and Zamelle scripted again

--- script/c210662149.lua
+++ script/c210662149.lua
@@ -2,20 +2,53 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --draw
+	c:EnableUnsummonable()
+    --special summon tribute
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DRAW)	
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTarget(s.drtg)
-    e1:SetOperation(s.drop)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    --No battle damage
+    --draw
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    e2:SetCategory(CATEGORY_DRAW)	
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetTarget(s.drtg)
+    e2:SetOperation(s.drop)
     c:RegisterEffect(e2)
+    --No battle damage
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    c:RegisterEffect(e2)
+end
+function s.blackfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.blackfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.blackfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(1-tp,2) end
--- script/c210662310.lua
+++ script/c210662310.lua
@@ -2,16 +2,44 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
+	c:EnableUnsummonable()
+    --special summon tribute
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COUNTER)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetRange(LOCATION_GRAVE)
-    e1:SetCondition(s.sumcon)
-    e1:SetTarget(s.sumtg)
-    e1:SetOperation(s.sumop)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+	--Revive
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COUNTER)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCondition(s.sumcon)
+    e2:SetTarget(s.sumtg)
+    e2:SetOperation(s.sumop)
+    c:RegisterEffect(e2)
+end
+function s.Zombiefilter(c)
+	return c:IsRace(RACE_ZOMBIE)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.Zombiefilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.Zombiefilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
 end
 function s.sumcon(e,tp,c)
 	if c==nil then return true end

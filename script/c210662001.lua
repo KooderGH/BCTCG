c3470f5619f1b1f61ac29096e9434c4224347a40
Konstak05 <zkonst05@gmail.com>
2024-03-23 18:15:13 +0200

    Buff Snache

--- script/c210662001.lua
+++ script/c210662001.lua
@@ -1,5 +1,31 @@
--- Blank
+-- Snache
+--Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --to defense
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_POSITION)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.deftg)
+    e1:SetOperation(s.defop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+end
+--e2
+function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return e:GetHandler():IsAttackPos() end
+    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
+end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
 end

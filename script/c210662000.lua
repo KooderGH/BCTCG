304e2eb8dd211f005f561f59dd07ee8ca2a285e1
Konstak05 <zkonst05@gmail.com>
2024-03-23 18:43:59 +0200

    Buff Doge!

--- script/c210662000.lua
+++ script/c210662000.lua
@@ -1,5 +1,27 @@
 -- Blank
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={210662268}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(1-tp,210662268,0,TYPES_TOKEN,1200,0,1,RACE_WARRIOR,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(1-tp,210662268,0,TYPES_TOKEN,1200,0,1,RACE_WARRIOR,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) then
+		local token=Duel.CreateToken(tp,210662268)
+		Duel.SpecialSummon(token,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE)
+	end
 end

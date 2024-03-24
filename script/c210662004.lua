a6384fabbdf09bb3c24abc3c6dc0472af812d7ca
Konstak05 <zkonst05@gmail.com>
2024-03-24 23:24:22 +0200

    Pigge and Mooth update

--- script/c210662004.lua
+++ script/c210662004.lua
@@ -8,17 +8,22 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_TO_GRAVE)
     e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTarget(s.drtg)
     e1:SetOperation(s.drop)
     c:RegisterEffect(e1)
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)	
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    Duel.Draw(p,d,REASON_EFFECT)
+    local c=e:GetHandler()
+    if Duel.IsPlayerCanDraw(1-tp,1) then
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCode(EVENT_PHASE+PHASE_DRAW)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+    e1:SetCountLimit(1)
+    e1:SetOperation(s.drawop)
+    c:RegisterEffect(e1)
+    end
+end
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(tp,1,REASON_EFFECT)
 end

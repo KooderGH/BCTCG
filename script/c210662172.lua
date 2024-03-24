969ed7c86146edbfff370c2bbaff0449669ecad5
Konstak05 <zkonst05@gmail.com>
2024-03-24 05:25:43 +0200

    LeMurr on BCETCG

---
+++ script/c210662172.lua
@@ -0,0 +1,64 @@
+-- LeMurr
+local s,id=GetID()
+function s.initial_effect(c)
+    --during damage calculation FIRE lose half of their ATK/DEF (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetTarget(s.atktg)
+    e1:SetOperation(s.atkop)
+    c:RegisterEffect(e1)
+    --Cannot Attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e2)
+    --Can target 1 card on the field. destroy that target (Ignition) (4)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id)
+    e3:SetTarget(s.destg)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
+end
+--during damage calculation function
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-bc:GetAttack()/2)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-bc:GetDefense()/2)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e2)
+    end
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end

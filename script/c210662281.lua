ed33c6db1f0c52a362faabf3680623848ebcf9cb
Konstak05 <zkonst05@gmail.com>
2024-03-24 05:51:09 +0200

    SnS and script Festival Ms. Sign

--- script/c210662281.lua
+++ script/c210662281.lua
@@ -1,5 +1,15 @@
--- Blank
+-- Festival Ms.Sign
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.atktarget)
+	c:RegisterEffect(e2)
+end
+function s.atktarget(e,c)
+	return c:GetLevel()>=4
 end

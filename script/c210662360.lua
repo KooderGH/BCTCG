b50ef9d0f4ccb03ff6272e8910634fc08e90bef7
Konstak05 <zkonst05@gmail.com>
2024-03-24 03:56:13 +0200

    Buff Shibalien elite and SnS alien cyclone

--- script/c210662360.lua
+++ script/c210662360.lua
@@ -1,36 +1,45 @@
 -- Shibalien Elite
 local s,id=GetID()
 function s.initial_effect(c)
-    --during damage calculation FIRE lose half of their ATK/DEF (1)
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_BATTLE_START)
-    e1:SetTarget(s.atktg)
-    e1:SetOperation(s.atkop)
-    c:RegisterEffect(e1)
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.warptg)
+	e1:SetOperation(s.warpop)
+	c:RegisterEffect(e1)
 end
---during damage calculation function
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
+function s.warpfilter(c)
+	return c:IsAbleToRemove() and c:IsMonster() and not c:IsCode(id)
 end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=e:GetHandler():GetBattleTarget()
-    if c:IsRelateToBattle() then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(-bc:GetAttack()/2)
-        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-        bc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_UPDATE_DEFENSE)
-        e2:SetValue(-bc:GetDefense()/2)
-        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-        bc:RegisterEffect(e2)
-    end
+function s.warptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.warpop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.HintSelection(g)
+		if Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+			e1:SetLabel(Duel.GetTurnCount())
+			e1:SetReset(RESET_PHASE+PHASE_MAIN1+RESET_SELF_TURN,1)
+			e1:SetLabelObject(tc)
+			e1:SetCountLimit(1)
+			e1:SetOperation(s.retop)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetTurnCount()~=e:GetLabel() and tp==Duel.GetTurnPlayer() then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.ReturnToField(e:GetLabelObject())
+	end
 end

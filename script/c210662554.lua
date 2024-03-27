0fce98646b8a22d9c2a0a0b7ec400d1fa674cee4
Konstak05 <zkonst05@gmail.com>
2024-03-28 01:05:08 +0200

    Sinner Snache and Cerberus kids on BCETCG

---
+++ script/c210662554.lua
@@ -0,0 +1,20 @@
+-- Cerberus Kids
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Shield Mechanic
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCountLimit(1)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTarget(s.desatktg)
+    c:RegisterEffect(e1)
+end
+--Shield Mechanic
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() end
+	return true
+end

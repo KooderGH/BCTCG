74801ea5f6c8c76ca35ca140dfffb40fd582bcdc
Konstak05 <zkonst05@gmail.com>
2024-03-28 13:22:39 +0200

    Sinner Snache scripted

--- script/c210662553.lua
+++ script/c210662553.lua
@@ -2,19 +2,55 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Shield Mechanic
+    --Death Disable Field
     local e1=Effect.CreateEffect(c)
-    e1:SetCode(EFFECT_DESTROY_REPLACE)
-    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCountLimit(1)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTarget(s.desatktg)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTarget(s.surgetg)
+    e1:SetOperation(s.surgeop)
     c:RegisterEffect(e1)
 end
---Shield Mechanic
-function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() end
-	return true
+function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    local Zone=d1
+    if tc then
+        local seq=tc:GetSequence()
+        local nseq=4-seq
+        Duel.Destroy(tc,REASON_EFFECT)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE_FIELD)
+        e1:SetRange(LOCATION_SZONE)
+        e1:SetLabel(nseq+16)
+        e1:SetOperation(s.disop)
+        e1:SetReset(RESET_PHASE+PHASE_END)
+        Duel.RegisterEffect(e1,tp)
+    else
+        local seq=Zone
+        local nseq=4-seq
+        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD)
+            e1:SetCode(EFFECT_DISABLE_FIELD)
+            e1:SetRange(LOCATION_SZONE)
+            e1:SetLabel(nseq+16)
+            e1:SetOperation(s.disop)
+            e1:SetReset(RESET_PHASE+PHASE_END)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.disop(e,tp)
+    return 0x1<<e:GetLabel()
 end

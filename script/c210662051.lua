e0f5a10c8d9f1a32ee50659a4edb7571605f4763
Konstak05 <zkonst05@gmail.com>
2024-03-29 13:02:09 +0200

    Updated Gory Black

--- script/c210662051.lua
+++ script/c210662051.lua
@@ -2,45 +2,83 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableUnsummonable()
-	-- Special Summon this card
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.spcon)
-	c:RegisterEffect(e1)
-    --draw
+    c:EnableUnsummonable()
+    -- Special Summon this card
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    c:RegisterEffect(e1)
+    -- Special Summon Tribute
     local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DRAW)	
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetOperation(s.drop)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetCode(EFFECT_SPSUMMON_PROC)
+    e2:SetCondition(s.spcon2)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
     c:RegisterEffect(e2)
-    --No battle damage
+    --draw
     local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_DRAW)	
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetOperation(s.drop)
     c:RegisterEffect(e3)
+    --No battle damage
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    c:RegisterEffect(e4)
 end
+--Special Summon SS Function
 function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=e:GetHandlerPlayer()
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
+--Special Summon Tribute Function
+function s.blackfilter(c)
+    return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.spcon2(e,c)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.blackfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.SelectReleaseGroup(tp,s.blackfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
+end
+--Draw Function
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if Duel.IsPlayerCanDraw(1-tp,1) then
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_GRAVE)
-    e1:SetCode(EVENT_PHASE+PHASE_DRAW)
-	e1:SetReset(RESET_PHASE+PHASE_END,2)
-    e1:SetCountLimit(1)
-    e1:SetOperation(s.drawop)
-    c:RegisterEffect(e1)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+        e1:SetRange(LOCATION_GRAVE)
+        e1:SetCode(EVENT_PHASE+PHASE_DRAW)
+        e1:SetReset(RESET_PHASE+PHASE_END,2)
+        e1:SetCountLimit(1)
+        e1:SetOperation(s.drawop)
+        c:RegisterEffect(e1)
     end
 end
 function s.drawop(e,tp,eg,ep,ev,re,r,rp)

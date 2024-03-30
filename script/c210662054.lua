c551a60c5ce90505ccb20f332784f7a8a179be25
Konstak05 <zkonst05@gmail.com>
2024-03-31 00:39:21 +0200

    Buff Le'boin and Balance SMH

--- script/c210662054.lua
+++ script/c210662054.lua
@@ -2,22 +2,55 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Metal Mechanic
+    c:EnableUnsummonable()
+    --special summon tribute
     local e1=Effect.CreateEffect(c)
-    e1:SetCode(EFFECT_DESTROY_REPLACE)
-    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTarget(s.desatktg)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    --self destroy
+    --Metal Mechanic
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_DESTROY_REPLACE)
+    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EFFECT_SELF_DESTROY)
-    e2:SetCondition(s.sdcon)
+    e2:SetTarget(s.desatktg)
     c:RegisterEffect(e2)
+    --self destroy
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_SELF_DESTROY)
+    e3:SetCondition(s.sdcon)
+    c:RegisterEffect(e3)
+end
+function s.smhfilter(c)
+    return c:IsFaceup() and c:IsCode(210662047)
+end
+function s.spcon(e,c)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.smhfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.SelectReleaseGroup(tp,s.smhfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
 end
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()

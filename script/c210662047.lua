55a7b21d68a65496c73ebdf96099a76144be62d9
Konstak05 <zkonst05@gmail.com>
2024-03-30 20:31:25 +0200

    Metal Hippoe on BCETCG

---
+++ script/c210662047.lua
@@ -0,0 +1,44 @@
+-- Metal Hippoe
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Metal Mechanic
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTarget(s.desatktg)
+    c:RegisterEffect(e1)
+    --self destroy
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_SELF_DESTROY)
+    e3:SetCondition(s.sdcon)
+    c:RegisterEffect(e3)
+    --Cannot be targeted
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+end
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsFaceup() end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_DEFENSE)
+    e1:SetValue(-50)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+    c:RegisterEffect(e1)
+    return true
+end
+function s.sdcon(e)
+    local c=e:GetHandler()
+    return c:GetDefense()<=0
+end

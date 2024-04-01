5648ac45fcabee523fa088690e00f3998b406b9b
Konstak05 <zkonst05@gmail.com>
2024-04-01 04:04:16 +0300

    SnS and Script Aku Gory

--- script/c210662556.lua
+++ script/c210662556.lua
@@ -2,55 +2,34 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Death Disable Field
+    --Savage Blow
     local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_ATKCHANGE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTarget(s.surgetg)
-    e1:SetOperation(s.surgeop)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.svgcon)
+    e1:SetTarget(s.svgtg)
+    e1:SetOperation(s.svgop)
     c:RegisterEffect(e1)
 end
-function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
+--Savage Blow
+function s.svgcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle()
+end
+function s.svgtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
-function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
+function s.svgop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    local d1=6
-    while d1>3 do
-        d1=Duel.TossDice(tp,1)
+    local d1=Duel.TossDice(tp,1)
+    if c:IsFaceup() and c:IsRelateToEffect(e) and d1<=2 then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(c:GetAttack())
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        c:RegisterEffect(e1)
     end
-    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
-    local Zone=d1
-    if tc then
-        local seq=tc:GetSequence()
-        local nseq=4-seq
-        Duel.Destroy(tc,REASON_EFFECT)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_DISABLE_FIELD)
-        e1:SetRange(LOCATION_SZONE)
-        e1:SetLabel(nseq+16)
-        e1:SetOperation(s.disop)
-        e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
-        Duel.RegisterEffect(e1,tp)
-    else
-        local seq=Zone
-        local nseq=4-seq
-        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_FIELD)
-            e1:SetCode(EFFECT_DISABLE_FIELD)
-            e1:SetRange(LOCATION_SZONE)
-            e1:SetLabel(nseq+16)
-            e1:SetOperation(s.disop)
-            e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
-            Duel.RegisterEffect(e1,tp)
-        end
-    end
-end
-function s.disop(e,tp)
-    return 0x1<<e:GetLabel()
 end

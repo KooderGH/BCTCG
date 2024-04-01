467e9c92114757dbaf57a60ea7f8ffa0b90c4a82
Konstak05 <zkonst05@gmail.com>
2024-04-01 22:46:48 +0300

    SnS Cerberus Kids

--- script/c210662286.lua
+++ script/c210662286.lua
@@ -2,108 +2,108 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-	--Place as Spell
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
+    --Place as Spell
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,2))
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetRange(LOCATION_MZONE)
+    e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
-	e1:SetTarget(s.spelltarget)
-	e1:SetOperation(s.spellop)
-	c:RegisterEffect(e1)
+    e1:SetTarget(s.spelltarget)
+    e1:SetOperation(s.spellop)
+    c:RegisterEffect(e1)
     local e2=e1:Clone()
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
     local e3=e1:Clone()
     e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e3)
-	--Then SS to opp
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,3))
-	e4:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    --Then SS to opp
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,3))
+    e4:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e4:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_SZONE)
+    e4:SetRange(LOCATION_SZONE)
     e4:SetCode(EVENT_PHASE+PHASE_END)
     e4:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e4:SetCondition(s.destcondition)
-	e4:SetTarget(s.destg)
-	e4:SetOperation(s.desop)
-	c:RegisterEffect(e4)
-	--When Special Summoned, Skip opp battle
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e5:SetCondition(s.skipbpsscon)
-	e5:SetOperation(s.skipbpssop)
-	c:RegisterEffect(e5)
+    e4:SetCondition(s.destcondition)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
+    --Slow Ability
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e5:SetCondition(s.slowcon)
+    e5:SetOperation(s.slowop)
+    c:RegisterEffect(e5)
 end
 --e1
 function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
 end
 function s.spellop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local c=e:GetHandler()
-	Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-	local e1=Effect.CreateEffect(c)
-	e1:SetCode(EFFECT_CHANGE_TYPE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
-	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-	c:RegisterEffect(e1)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
 end
 --e2
 function s.destcondition(e,tp,eg,ep,ev,re,r,rp)
-	return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
+    return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
 end
 function s.desfilter(c,atk)
-	return c:IsFaceup()
+    return c:IsFaceup()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
-	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,c:GetAttack())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+    local c=e:GetHandler()
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
+    if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,c:GetAttack())
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		local c=e:GetHandler()
-		if c:IsRelateToEffect(e) then
-			Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
-		end
-	end
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) then
+            Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+        end
+    end
 end
---e5
-function s.skipbpsscon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsRelateToBattle()
+--Slow Ability Function
+function s.slowcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle()
 end
-function s.skipbpssop(e,tp,eg,ep,ev,re,r,rp)
-	local effp=e:GetHandler():GetControler()
-	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_CANNOT_BP)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(0,1)
-		if Duel.GetTurnPlayer()==effp then
-			e1:SetLabel(Duel.GetTurnCount())
-			e1:SetCondition(s.skipcon)
-			e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
-		else
-			e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
-		end
-		Duel.RegisterEffect(e1,effp)
-		Duel.NegateAttack()
-	end
+function s.slowop(e,tp,eg,ep,ev,re,r,rp)
+    local effp=e:GetHandler():GetControler()
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_CANNOT_BP)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(0,1)
+        if Duel.GetTurnPlayer()==effp then
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetCondition(s.skipcon)
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+        else
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+        end
+        Duel.RegisterEffect(e1,effp)
+        Duel.NegateAttack()
+    end
 end
 function s.skipcon(e)
     return Duel.GetTurnCount()~=e:GetLabel()
--- script/c210662554.lua
+++ script/c210662554.lua
@@ -2,19 +2,38 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Shield Mechanic
+    --Freeze Ability
     local e1=Effect.CreateEffect(c)
-    e1:SetCode(EFFECT_DESTROY_REPLACE)
-    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCountLimit(1)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTarget(s.desatktg)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.freezecon)
+    e1:SetOperation(s.freezeop)
     c:RegisterEffect(e1)
 end
---Shield Mechanic
-function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() end
-	return true
+--Freeze Function
+function s.freezecon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRelateToBattle()
+end
+function s.freezeop(e,tp,eg,ep,ev,re,r,rp)
+    local effp=e:GetHandler():GetControler()
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_SKIP_DP)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(0,1)
+        if Duel.GetTurnPlayer()==effp then
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetCondition(s.skipcon)
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+        else
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+        end
+        Duel.RegisterEffect(e1,effp)
+        Duel.NegateAttack()
+    end
+end
+function s.skipcon(e)
+    return Duel.GetTurnCount()~=e:GetLabel()
 end

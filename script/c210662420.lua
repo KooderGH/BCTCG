3ea6dbf17b0172661a4205f55845958b033d4c8c
Konstak05 <zkonst05@gmail.com>
2024-04-02 01:54:06 +0300

    Script Loris

--- script/c210662420.lua
+++ script/c210662420.lua
@@ -2,28 +2,37 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Negate effects
+    --Curse Ability
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_DISABLE)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
-    e1:SetTarget(s.negtarget)
-    e1:SetOperation(s.negoperation)
+    e1:SetTarget(s.cursetg)
+    e1:SetOperation(s.curseop)
     c:RegisterEffect(e1)
+    --Long Distance Ability
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e2:SetCondition(s.ldcon)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 end
---e3
-function s.negtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+--Curse Ability Function
+function s.cursetg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_SZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+    local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_SZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
-function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+function s.curseop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
         Duel.NegateRelatedChain(tc,RESET_TURN_SET)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
@@ -31,12 +40,13 @@ function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
         e1:SetCode(EFFECT_DISABLE)
         e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
         tc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e2:SetCode(EFFECT_DISABLE_EFFECT)
-        e2:SetValue(RESET_TURN_SET)
-        e2:SetReset(RESET_PHASE+PHASE_MAIN1,2)
-        tc:RegisterEffect(e2)
-	end
+    end
+end
+--Long Distance Function
+function s.lorisfilter(c)
+    return not c:IsCode(id)
+end
+function s.ldcon(e,c)
+    if c==nil then end
+    return Duel.IsExistingMatchingCard(s.lorisfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end

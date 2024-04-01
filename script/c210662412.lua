65d717adea5a611d5fd046103efd8afb20686108
Konstak05 <zkonst05@gmail.com>
2024-04-01 13:11:04 +0300

    adding their template scripts

---
+++ script/c210662412.lua
@@ -0,0 +1,42 @@
+-- Sir Rel
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Negate effects
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.negtarget)
+    e1:SetOperation(s.negoperation)
+    c:RegisterEffect(e1)
+end
+--e3
+function s.negtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetValue(RESET_TURN_SET)
+        e2:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+        tc:RegisterEffect(e2)
+	end
+end

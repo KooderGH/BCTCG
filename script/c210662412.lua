ef8c14348299b608ea758ace6ae3e53c30b073a1
Konstak05 <zkonst05@gmail.com>
2024-04-01 23:10:19 +0300

    Set in Stone and script Sir Rel

--- script/c210662412.lua
+++ script/c210662412.lua
@@ -2,28 +2,32 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Negate effects
+    --Curse on Battle Ability
     local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
-    e1:SetTarget(s.negtarget)
-    e1:SetOperation(s.negoperation)
+    e1:SetCondition(s.cursecon)
+    e1:SetTarget(s.cursetg)
+    e1:SetOperation(s.curseop)
     c:RegisterEffect(e1)
 end
---e3
-function s.negtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+--Curse on Battle Function
+function s.cursecon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
 end
-function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+function s.cursetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.curseop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) then
         Duel.NegateRelatedChain(tc,RESET_TURN_SET)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
@@ -38,5 +42,6 @@ function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
         e2:SetValue(RESET_TURN_SET)
         e2:SetReset(RESET_PHASE+PHASE_MAIN1,2)
         tc:RegisterEffect(e2)
-	end
+        Duel.NegateAttack()
+    end
 end

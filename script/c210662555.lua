5cc5252b6af6d403a90568d3fb844532e107a4d8
Konstak05 <zkonst05@gmail.com>
2024-04-01 03:31:29 +0300

    Aku Gory, C Peng and Devil Wife on BCETCG

---
+++ script/c210662555.lua
@@ -0,0 +1,56 @@
+-- Condemned Peng
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Death Disable Field
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTarget(s.surgetg)
+    e1:SetOperation(s.surgeop)
+    c:RegisterEffect(e1)
+end
+function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    local Zone=d1
+    if tc then
+        local seq=tc:GetSequence()
+        local nseq=4-seq
+        Duel.Destroy(tc,REASON_EFFECT)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE_FIELD)
+        e1:SetRange(LOCATION_SZONE)
+        e1:SetLabel(nseq+16)
+        e1:SetOperation(s.disop)
+        e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+        Duel.RegisterEffect(e1,tp)
+    else
+        local seq=Zone
+        local nseq=4-seq
+        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD)
+            e1:SetCode(EFFECT_DISABLE_FIELD)
+            e1:SetRange(LOCATION_SZONE)
+            e1:SetLabel(nseq+16)
+            e1:SetOperation(s.disop)
+            e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.disop(e,tp)
+    return 0x1<<e:GetLabel()
+end
---
+++ script/c210662636.lua
@@ -0,0 +1,29 @@
+-- Devil Wife
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --draw
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DRAW)	
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetOperation(s.drop)
+    c:RegisterEffect(e1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.IsPlayerCanDraw(1-tp,1) then
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCode(EVENT_PHASE+PHASE_DRAW)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+    e1:SetCountLimit(1)
+    e1:SetOperation(s.drawop)
+    c:RegisterEffect(e1)
+    end
+end
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(tp,1,REASON_EFFECT)
+end

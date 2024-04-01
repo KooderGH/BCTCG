f118cf0a7df024860936607b0f762097cf32aaae
Konstak05 <zkonst05@gmail.com>
2024-04-02 02:44:07 +0300

    Script Lowkey

--- script/c210662423.lua
+++ script/c210662423.lua
@@ -2,28 +2,65 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Negate effects
+    c:EnableUnsummonable()
+    --special summon tribute
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetTarget(s.negtarget)
-    e1:SetOperation(s.negoperation)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+    --Curse on Battle Ability
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCondition(s.cursecon)
+    e2:SetTarget(s.cursetg)
+    e2:SetOperation(s.curseop)
+    c:RegisterEffect(e2)
 end
---e3
-function s.negtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+function s.Relicfilter(c)
+    return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_PLANT)
 end
-function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+function s.spcon(e,c)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.Relicfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.SelectReleaseGroup(tp,s.Relicfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
+end
+--Curse on Battle Function
+function s.cursecon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
+end
+function s.cursetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.curseop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) then
         Duel.NegateRelatedChain(tc,RESET_TURN_SET)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
@@ -31,12 +68,6 @@ function s.negoperation(e,tp,eg,ep,ev,re,r,rp)
         e1:SetCode(EFFECT_DISABLE)
         e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
         tc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e2:SetCode(EFFECT_DISABLE_EFFECT)
-        e2:SetValue(RESET_TURN_SET)
-        e2:SetReset(RESET_PHASE+PHASE_MAIN1,2)
-        tc:RegisterEffect(e2)
-	end
+        Duel.NegateAttack()
+    end
 end

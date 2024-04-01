14d6a9d09f4d27a66e9d062efa894a96ed965c50
Konstak05 <zkonst05@gmail.com>
2024-04-02 02:32:01 +0300

    Oldhorn scripted

--- script/c210662413.lua
+++ script/c210662413.lua
@@ -17,6 +17,18 @@ function s.initial_effect(c)
     local e3=e1:Clone()
     e3:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e3)
+    --Curse on Battle Ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_DISABLE)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetCondition(s.cursecon)
+    e4:SetTarget(s.cursetg)
+    e4:SetOperation(s.curseop)
+    c:RegisterEffect(e4)
 end
 --To Defense Function
 function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -27,5 +39,28 @@ function s.defop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
         Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+    end
+end
+--Curse on Battle Function
+function s.cursecon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
+end
+function s.cursetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.curseop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+        tc:RegisterEffect(e1)
+        Duel.NegateAttack()
     end
 end

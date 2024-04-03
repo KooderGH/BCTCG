e9294f1f0df1b2833a3223654d396288c766e2b0
Konstak05 <zkonst05@gmail.com>
2024-04-04 02:04:33 +0300

    Fully Script CyberFace

--- script/c210662058.lua
+++ script/c210662058.lua
@@ -2,6 +2,17 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
     --Metal Mechanic
     local e1=Effect.CreateEffect(c)
     e1:SetCode(EFFECT_DESTROY_REPLACE)
@@ -32,6 +43,28 @@ function s.initial_effect(c)
     e4:SetType(EFFECT_TYPE_SINGLE)
     e4:SetCode(EFFECT_NO_BATTLE_DAMAGE)
     c:RegisterEffect(e4)
+end
+function s.cffilter(c)
+    return c:IsFaceup() and c:IsCode(210662497)
+end
+function s.spcon(e,c)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.cffilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.SelectReleaseGroup(tp,s.cffilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
 end
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()

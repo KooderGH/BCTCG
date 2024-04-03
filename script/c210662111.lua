a71f8ebeacf2e6954e441ef23b6bf92c323fc826
Konstak05 <zkonst05@gmail.com>
2024-04-03 23:10:01 +0300

    Pigge Back and Pigge Victorious on BCETCG

---
+++ script/c210662111.lua
@@ -0,0 +1,31 @@
+-- Pigge Back
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Draw lv3 Red Monster
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetCode(EVENT_DESTROYED)
+    e1:SetTarget(s.drtg)
+    e1:SetOperation(s.drop)
+    c:RegisterEffect(e1)
+end
+--Destroy and draw function
+function s.drfilter(c)
+    return c:IsLevel(3) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end

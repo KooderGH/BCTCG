a0172dd6b906916715f0bfcfe04766177448cc94
Konstak05 <zkonst05@gmail.com>
2024-04-04 01:52:07 +0300

    SnS and script CyberFace

--- script/c210662058.lua
+++ script/c210662058.lua
@@ -18,6 +18,20 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_SELF_DESTROY)
     e2:SetCondition(s.sdcon)
     c:RegisterEffect(e2)
+    --Guaranteed Wave
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetTarget(s.wavetg)
+    e3:SetOperation(s.waveop)
+    c:RegisterEffect(e3)
+    --No battle damage
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    c:RegisterEffect(e4)
 end
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
@@ -33,4 +47,20 @@ end
 function s.sdcon(e)
     local c=e:GetHandler()
     return c:GetDefense()<=0
+end
+--Wave Ability Function
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    if tc then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
 end

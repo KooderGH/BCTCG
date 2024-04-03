3c59a96e1677e02440ca100738238a08e9111d2c
Konstak05 <zkonst05@gmail.com>
2024-04-04 01:48:58 +0300

    Add CyberFace (100th Enemy!)

---
+++ script/c210662058.lua
@@ -0,0 +1,36 @@
+-- CyberFace
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Metal Mechanic
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTarget(s.desatktg)
+    c:RegisterEffect(e1)
+    --self destroy
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EFFECT_SELF_DESTROY)
+    e2:SetCondition(s.sdcon)
+    c:RegisterEffect(e2)
+end
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsFaceup() end
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_DEFENSE)
+        e1:SetValue(-50)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+        c:RegisterEffect(e1)
+    return true
+end
+function s.sdcon(e)
+    local c=e:GetHandler()
+    return c:GetDefense()<=0
+end

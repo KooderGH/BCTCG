f59b6321980fea9a617fadc59a5b3625ba5099cd
Konstak05 <zkonst05@gmail.com>
2024-04-03 04:00:29 +0300

    Event gory black on BCETCG

---
+++ script/c210662087.lua
@@ -0,0 +1,62 @@
+-- Gory Groom
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableUnsummonable()
+    -- Special Summon Tribute
+    local e1=Effect.CreateEffect(c)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --draw
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DRAW)	
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetOperation(s.drop)
+    c:RegisterEffect(e2)
+end
+function s.blackfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.blackfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.blackfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.IsPlayerCanDraw(1-tp,1) then
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCode(EVENT_PHASE+PHASE_DRAW)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+    e1:SetCountLimit(1)
+    e1:SetOperation(s.drawop)
+    c:RegisterEffect(e1)
+    end
+end
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(1-tp,2,REASON_EFFECT)
+end

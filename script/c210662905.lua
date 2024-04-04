1c5e17e35f7fb70a82e17affd6d4c03cf5b4f9b4
Konstak05 <zkonst05@gmail.com>
2024-04-04 23:59:18 +0300

    SnS and script 905

--- script/c210662905.lua
+++ script/c210662905.lua
@@ -2,17 +2,33 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Pay 250 LP
+    --Cannot Attack
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetCountLimit(1)
-    e1:SetOperation(s.lpop)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_ATTACK)
     c:RegisterEffect(e1)
+    --Can target 1 card on the field. destroy that target (Ignition)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
 end
-function s.lpop(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetHandler():IsRelateToEffect(e) then
-        Duel.Damage(1-tp,500,REASON_EFFECT)
+--Destroy That Target Function
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.Destroy(tc,REASON_EFFECT)
     end
 end

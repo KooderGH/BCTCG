03881ab50384a214c42bd46d7ec4f477cfa11bf7
Konstak05 <zkonst05@gmail.com>
2024-04-05 01:51:32 +0300

    SnS and script 904

--- script/c210662904.lua
+++ script/c210662904.lua
@@ -1,18 +1,64 @@
 -- ??
 --Scripted By Konstak
 local s,id=GetID()
+local COUNTER_CAULDRON=0x14e
 function s.initial_effect(c)
-    --Pay 1000 LP
+    c:EnableCounterPermit(COUNTER_CAULDRON)
+    --activate
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetCountLimit(1)
-    e1:SetOperation(s.lpop)
+    e1:SetCategory(CATEGORY_COUNTER)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
+    --counter
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_COUNTER)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
+    --Inflict Damage
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.dmgtg)
+    e3:SetOperation(s.dmgop)
+    c:RegisterEffect(e3)
 end
-function s.lpop(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetHandler():IsRelateToEffect(e) then
-        Duel.Damage(1-tp,1000,REASON_EFFECT)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return Duel.IsCanAddCounter(tp,COUNTER_CAULDRON,1,c) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        c:AddCounter(COUNTER_CAULDRON,1)
     end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.dmgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:GetCounter(COUNTER_CAULDRON)>0 end
+    local dam=c:GetCounter(COUNTER_CAULDRON)*200
+    Duel.SetTargetPlayer(1-tp)
+    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function s.dmgop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d=e:GetHandler():GetCounter(COUNTER_CAULDRON)*200
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    Duel.Damage(p,d,REASON_EFFECT)
 end

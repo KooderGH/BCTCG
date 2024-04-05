82e05cd1de1db595cbcdfd2a80ffadcc6f346a16
Konstak05 <zkonst05@gmail.com>
2024-04-05 23:38:54 +0300

    Nerf Sluggy and Lucky Dags to Black enemies only

--- script/c210662437.lua
+++ script/c210662437.lua
@@ -2,28 +2,28 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Draw lv4 Monster
+    --Add Monster
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e1:SetCode(EVENT_DESTROYED)
-    e1:SetTarget(s.drtg)
-    e1:SetOperation(s.drop)
+    e1:SetTarget(s.addtg)
+    e1:SetOperation(s.addop)
     c:RegisterEffect(e1)
 end
---Destroy and draw function
-function s.drfilter(c)
-    return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToHand()
+--Destroy and add function
+function s.addfilter(c)
+    return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)

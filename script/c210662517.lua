14b1f27ecc764fe056a11fc02eec4633898f4c9a
Konstak05 <zkonst05@gmail.com>
2024-04-06 21:28:08 +0300

    SnS and script Koronium

--- script/c210662517.lua
+++ script/c210662517.lua
@@ -29,6 +29,16 @@ function s.initial_effect(c)
     e3:SetCode(EFFECT_SELF_DESTROY)
     e3:SetCondition(s.sdcon)
     c:RegisterEffect(e3)
+    --Wave on Battle
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,3))
+    e4:SetCategory(CATEGORY_DISABLE)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e4:SetCondition(s.wavecon)
+    e4:SetTarget(s.wavetg)
+    e4:SetOperation(s.waveop)
+    c:RegisterEffect(e4)
 end
 function s.smsfilter(c)
     return c:IsFaceup() and c:IsCode(210662147)
@@ -66,4 +76,24 @@ end
 function s.sdcon(e)
     local c=e:GetHandler()
     return c:GetDefense()<=0
+end
+--Wave on Battle Function
+function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    Duel.NegateAttack()
+    if tc then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
 end

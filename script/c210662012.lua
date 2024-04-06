31d1eeeece55856cf35a1f487baba05b48f8bf84
Konstak05 <zkonst05@gmail.com>
2024-04-07 02:17:29 +0300

    Rework Teacher Bear and Camelle

--- script/c210662012.lua
+++ script/c210662012.lua
@@ -2,37 +2,5 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableUnsummonable()
-    --special summon tribute
-    local e1=Effect.CreateEffect(c)
-    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCode(EFFECT_SPSUMMON_PROC)
-    e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
-    c:RegisterEffect(e1)
-end
-function s.traitlessfilter(c)
-	return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR)
-end
-function s.spcon(e,c)
-	if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.traitlessfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.traitlessfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
-	if g then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-	return true
-	end
-	return false
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+
 end
--- script/c210662035.lua
+++ script/c210662035.lua
@@ -2,48 +2,15 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableUnsummonable()
-    --special summon tribute
-    local e1=Effect.CreateEffect(c)
-    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCode(EFFECT_SPSUMMON_PROC)
-    e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
-    c:RegisterEffect(e1)
     --Base Destroyer Ability
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_BATTLE_CONFIRM)
-    e2:SetCondition(function() return Duel.GetAttackTarget()==nil end)
-    e2:SetOperation(s.desop)
-    c:RegisterEffect(e2)
-end
-function s.traitlessfilter(c)
-    return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR)
-end
-function s.spcon(e,c)
-    if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.traitlessfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.traitlessfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
-    if g then
-        g:KeepAlive()
-        e:SetLabelObject(g)
-    return true
-    end
-    return false
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=e:GetLabelObject()
-    if not g then return end
-    Duel.Release(g,REASON_COST)
-    g:DeleteGroup()
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,1))
+    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_CONFIRM)
+    e1:SetCondition(function() return Duel.GetAttackTarget()==nil end)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
 end
 --Base Destroyer Function
 function s.desop(e,tp,eg,ep,ev,re,r,rp)

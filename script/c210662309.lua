efd308aa7f6165f7f25895825a4f65548b2165a0
Konstak05 <zkonst05@gmail.com>
2024-04-06 04:51:11 +0300

    SnS Coffin Zoge

--- script/c210662309.lua
+++ script/c210662309.lua
@@ -1,5 +1,31 @@
 -- Coffin Zoge
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--Revive
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COUNTER)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCondition(s.sumcon)
+    e1:SetTarget(s.sumtg)
+    e1:SetOperation(s.sumop)
+    c:RegisterEffect(e1)
+end
+function s.sumcon(e,tp,c)
+    if c==nil then return true end
+    return tp==Duel.GetTurnPlayer() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.CheckLPCost(tp,250)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.PayLPCost(tp,250)
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+    end
 end

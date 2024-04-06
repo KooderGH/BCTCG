def3a8371c903bd0537290677ed2e90c9de0daa0
Konstak05 <zkonst05@gmail.com>
2024-04-06 20:48:55 +0300

    Add Sluggy Angel on BCETCG

---
+++ script/c210662343.lua
@@ -0,0 +1,31 @@
+-- Sluggy (Angel)
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Add Monster
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetCode(EVENT_DESTROYED)
+    e1:SetTarget(s.addtg)
+    e1:SetOperation(s.addop)
+    c:RegisterEffect(e1)
+end
+--Destroy and add function
+function s.addfilter(c)
+    return c:IsLevel(3) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
+end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end

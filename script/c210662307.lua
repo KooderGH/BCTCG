b9ce0ca84fdca6db79394fe3ee71896dd2f70d8b
Konstak05 <zkonst05@gmail.com>
2024-04-06 04:38:28 +0300

    SnS & Script Icy Doge (Snooze is gonna hate me)

--- script/c210662307.lua
+++ script/c210662307.lua
@@ -7,4 +7,19 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e1:SetValue(1)
     c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EFFECT_CHANGE_DAMAGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(1,0)
+    e2:SetValue(s.damval)
+    c:RegisterEffect(e2)
+    local e3=e2:Clone()
+    e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+    c:RegisterEffect(e3)
+end
+function s.damval(e,re,val,r,rp,rc)
+    if (r&REASON_EFFECT)~=0 then return 0
+    else return val end
 end

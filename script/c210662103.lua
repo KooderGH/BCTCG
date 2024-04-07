d06ba26c60405029a465d6b1a2cf5dda2ad4d429
Konstak05 <zkonst05@gmail.com>
2024-04-07 22:17:54 +0300

    SnS and Script Ecto Doge

--- script/c210662103.lua
+++ script/c210662103.lua
@@ -1,5 +1,14 @@
 -- Ecto Doge
 local s,id=GetID()
 function s.initial_effect(c)
-
+    aux.AddEquipProcedure(c,nil,s.filter)
+    --Atk update
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_EQUIP)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(200)
+    c:RegisterEffect(e1)
+end
+function s.filter(c)
+    return c:IsAttribute(ATTRIBUTE_FIRE) or c:IsAttribute(ATTRIBUTE_DARK) or c:IsAttribute(ATTRIBUTE_WIND)
 end

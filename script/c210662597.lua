d8e76743b3854d2e7261ec13e982f305291c78dd
Konstak05 <zkonst05@gmail.com>
2024-04-08 00:13:19 +0300

    Ban system down and buff Emelia

--- script/c210662597.lua
+++ script/c210662597.lua
@@ -7,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
@@ -16,20 +15,12 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.PayLPCost(tp,1000)
 end
 function s.filter(c)
-	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and aux.SpElimFilter(c,true,true)
+	return c:IsRace(RACE_MACHINE) and aux.SpElimFilter(c,true,true)
 end
 function s.tfilter(c)
 	return not c:IsAbleToRemove()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,nil)
-		return #g>0 and not g:IsExists(s.tfilter,1,nil)
-	end
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
-end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,nil)
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_HAND,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 end

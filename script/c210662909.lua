f96a011804a281baa9686cb3c3feeb5d473622d0
Konstak05 <zkonst05@gmail.com>
2024-04-10 20:33:21 +0300

    Add Crab Cats on BCETCG

---
+++ script/c210662909.lua
@@ -0,0 +1,6 @@
+-- King Crab
+--Scripted by ""
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
---
+++ script/c210662917.lua
@@ -0,0 +1,6 @@
+-- Zrab Zat
+--Scripted by ""
+local s,id=GetID()
+function s.initial_effect(c)
+
+end

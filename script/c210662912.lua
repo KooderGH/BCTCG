090e3d9b4e4fbbfb0114d62ac309dd11a9f65c62
Konstak05 <zkonst05@gmail.com>
2024-04-11 04:07:10 +0300

    Script Divine Crab Cat

--- script/c210662912.lua
+++ script/c210662912.lua
@@ -1,6 +1,69 @@
--- Zrab Zat
---Scripted by ""
+-- Divine Crab Cat
+--Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --Can banish zombie monsters
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetTarget(s.bntg)
+    e1:SetOperation(s.bnop)
+    c:RegisterEffect(e1)
+    --Add Monster
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.addtg)
+    e2:SetOperation(s.addop)
+    c:RegisterEffect(e2)
+    --Long Distance Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e3:SetCondition(s.ldcon)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)    
+end
+--Banish Zombies function
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() then
+        Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+--Add function
+function s.crabfilteradd(c)
+    return (c:IsCode(210662173) or c:IsCode(210662910) or c:IsCode(210662911) or c:IsCode(210662912) or c:IsCode(210662913) or c:IsCode(210662914) or c:IsCode(210662915) or c:IsCode(210662916) or c:IsCode(210662917) or c:IsCode(210662918)) and c:IsAbleToHand()
+end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.crabfilteradd,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.crabfilteradd,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Long Distance Function
+function s.crabfilter(c)
+    return c:IsFaceup() and (c:IsCode(210662173) or c:IsCode(210662910) or c:IsCode(210662911) or c:IsCode(210662912) or c:IsCode(210662913) or c:IsCode(210662914) or c:IsCode(210662915) or c:IsCode(210662916) or c:IsCode(210662917) or c:IsCode(210662918))
+end
+function s.ldcon(e,c)
+    if c==nil then end
+    return Duel.IsExistingMatchingCard(s.crabfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,2,nil)
 end

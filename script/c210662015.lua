7682cad3b2abda21bf8661be97f2a87ce1509747
Konstak05 <zkonst05@gmail.com>
2024-04-11 12:16:19 +0300

    rework Squire Rel

--- script/c210662015.lua
+++ script/c210662015.lua
@@ -1,6 +1,10 @@
 -- Squire Rel
---Scripted by " "
+--Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    aux.AddUnionProcedure(c,s.monsterfilter)
+end
+--Union filter
+function s.monsterfilter(c)
+    return c:IsFaceup()
 end

7ea869df46f1c74b6c3b96525bc9d0fcdf0ae156
Konstak05 <zkonst05@gmail.com>
2024-04-12 18:29:37 +0300

    SnS again and Script 04R

--- script/c210663004.lua
+++ script/c210663004.lua
@@ -72,20 +72,20 @@ function s.initial_effect(c)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
-	if contact then sumtype=0 end
-	return c:IsType(ATTRIBUTE_EARTH,fc,sumtype,tp) and c:IsFaceup() and c:IsCode(210660355) and c:IsRace(RACE_MACHINE,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
+    if contact then sumtype=0 end
+    return c:IsType(ATTRIBUTE_EARTH,fc,sumtype,tp) and c:IsFaceup() and c:IsCode(210660355) and c:IsRace(RACE_MACHINE,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
 end
 function s.splimit(e,se,sp,st)
-	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+    return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
 function s.contactfil(tp)
-	return Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil,tp)
+    return Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil,tp)
 end
 function s.cfilter(c,tp)
-	return c:IsAbleToGraveAsCost() and (c:IsControler(tp) or c:IsFaceup())
+    return c:IsAbleToGraveAsCost() and (c:IsControler(tp) or c:IsFaceup())
 end
 function s.contactop(g,tp,c)
-	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+    Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
 end
 --Banish Zombies function
 function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -96,49 +96,39 @@ end
 function s.bnop(e,tp,eg,ep,ev,re,r,rp)
     local bc=e:GetHandler():GetBattleTarget()
     if bc:IsRelateToBattle() then
-    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+        Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
     end
 end
 --Destroy and add function
 function s.addfilter(c)
-	return c:IsCode(210663000) and c:IsAbleToDeck()
+    return c:IsCode(210663000) and c:IsAbleToDeck()
 end
 function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
 end
 function s.addop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoDeck(g,nil,0,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoDeck(g,nil,0,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
 --addcounter
 s.counter_list={0x4001}
 function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x4001)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x4001)
 end
 function s.addc(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
+    if e:GetHandler():IsRelateToEffect(e) then
         e:GetHandler():AddCounter(0x4001,1)
-	end
+    end
 end
 --Destroy
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-
-    -- Zones in the same column cannot be used
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_DISABLE_FIELD)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetOperation(s.znop)
-    e1:SetReset(RESET_PHASE+PHASE_STANDBY,2)
-    Duel.RegisterEffect(e1,tp)
-
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
     local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
     Duel.Hint(HINT_ZONE,tp,seq)
@@ -151,25 +141,25 @@ function s.seqop(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
     local seq1=c:GetSequence() --register the sequence it comes from
     local dg=c:GetColumnGroup() --and the group of cards there
+    local zones=c:GetColumnZone(LOCATION_ONFIELD)
     Duel.MoveSequence(c,seq2)
     if c:GetSequence()==seq2 and seq1~=seq2 then
         Duel.BreakEffect()
         if #dg>0 then
             Duel.BreakEffect()
-            Duel.Destroy(dg,REASON_EFFECT)
-		end
-	end
+            Duel.SendtoGrave(dg,REASON_EFFECT)
+        end
+        -- Zones in the same column cannot be used
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE_FIELD)
+        e1:SetRange(LOCATION_MZONE)
+        e1:SetLabel(zones)
+        e1:SetOperation(s.znop)
+        e1:SetReset(RESET_PHASE+PHASE_STANDBY,2)
+        Duel.RegisterEffect(e1,tp)
+    end
 end
 function s.znop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local zones=c:GetColumnZone(LOCATION_ONFIELD)
-    for tc in c:GetColumnGroup():Iter() do
-        local ctrl=tc:IsControler(tp)
-        local seq=tc:GetSequence()
-        zones=zones&~(ctrl and (1<<seq) or (1<<(16+seq)))
-        if tc:IsInExtraMZone() then
-            zones=zones&~(ctrl and (1<<(27-seq)) or (1<<(11-seq)))
-		end
-	end
-	return zones
+    return e:GetLabel()
 end

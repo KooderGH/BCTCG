47e86d94a4299e5919fb62730fd58c7b05b7a2cb
Konstak05 <zkonst05@gmail.com>
2024-04-13 02:07:19 +0300

    more progress on Sodom

--- script/c210660083.lua
+++ script/c210660083.lua
@@ -17,10 +17,66 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_SUMMON_PROC)
     c:RegisterEffect(e1)
+    --When destroyed by card effect (5)
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,2))
+    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetCondition(s.descon)
+    e5:SetTarget(s.destg)
+    e5:SetOperation(s.desop)
+    c:RegisterEffect(e5)
+    --When destroyed by battle (6)
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,3))
+    e6:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e6:SetCode(EVENT_DESTROYED)
+    e6:SetCondition(s.descon2)
+    e6:SetTarget(s.destg2)
+    e6:SetOperation(s.desop2)
+    c:RegisterEffect(e6)
     --Can be treated as 2 Tributes for the Tribute Summon of a Dragon monster.(7)
     local e7=Effect.CreateEffect(c)
     e7:SetType(EFFECT_TYPE_SINGLE)
     e7:SetCode(EFFECT_DOUBLE_TRIBUTE)
     e7:SetValue(function (e,c) return c:IsRace(RACE_DRAGON) end)
     c:RegisterEffect(e7)
+end
+--Add when destroyed by effect function (5)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function s.addfilter(c)
+    return c:IsMonster() and c:IsRace(RACE_DRAGON) and c:IsAbleToHand()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Add when destroyed by battle function (6)
+function s.descon2(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsReason(REASON_BATTLE)
+end
+function s.destg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsAbleToHand() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+    local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.desop2(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+    end
 end

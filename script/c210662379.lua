1e7e9823f734567ff9822ea2f658a6c527f23a44
Konstak05 <zkonst05@gmail.com>
2024-04-14 23:05:37 +0300

    Winged Pigge SnS and scripted

--- script/c210662379.lua
+++ script/c210662379.lua
@@ -13,6 +13,23 @@ function s.initial_effect(c)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+    --Weaken Ability
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCondition(s.weakencon)
+    e2:SetOperation(s.weakenop)
+    c:RegisterEffect(e2)
+    --Can banish zombie monsters
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_START)
+    e3:SetTarget(s.bntg)
+    e3:SetOperation(s.bnop)
+    c:RegisterEffect(e3)
 end
 function s.angelfilter(c)
     return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
@@ -35,4 +52,39 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     if not g then return end
     Duel.Release(g,REASON_COST)
     g:DeleteGroup()
+end
+--Weaken Ability Function
+function s.weakencon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.weakenop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetRange(LOCATION_MZONE)
+        e1:SetTargetRange(0,LOCATION_MZONE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,1)
+        e1:SetValue(s.atkval)
+        c:RegisterEffect(e1,tp)
+        local e2=e1:Clone()
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        c:RegisterEffect(e2,tp)
+    end
+end
+function s.atkval(e,c)
+	return -c:GetBaseAttack()/4
+end
+--Banish Zombies function
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() then
+    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+    end
 end

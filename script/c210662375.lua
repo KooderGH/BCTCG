99cda85bdea14b608c2f723ac78f0381b30bdb64
Konstak05 <zkonst05@gmail.com>
2024-04-14 21:44:04 +0300

    SnS and script Youcan

--- script/c210662375.lua
+++ script/c210662375.lua
@@ -2,5 +2,49 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --Also treated as a LIGHT monster on the field
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_ADD_ATTRIBUTE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(ATTRIBUTE_LIGHT)
+    c:RegisterEffect(e1)
+    --Wave on Battle
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCondition(s.wavecon)
+    e2:SetTarget(s.wavetg)
+    e2:SetOperation(s.waveop)
+    c:RegisterEffect(e2)
+    --Immune to Wave, Surge, Single Attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+end
+--Wave on Battle Function
+function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    Duel.NegateAttack()
+    if tc then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
 end

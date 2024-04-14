4034492a5bbccb07b49413f570b3e466bfef4584
Konstak05 <zkonst05@gmail.com>
2024-04-14 23:53:10 +0300

    SnS and script Exiel

--- script/c210662593.lua
+++ script/c210662593.lua
@@ -32,6 +32,32 @@ function s.initial_effect(c)
     e3:SetTarget(s.addtg)
     e3:SetOperation(s.addop)
     c:RegisterEffect(e3)
+    --Colossal Mechanic
+    local e4=Effect.CreateEffect(c)
+    e4:SetCode(EFFECT_DESTROY_REPLACE)
+    e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetTarget(s.desatktg)
+    c:RegisterEffect(e4)
+    --self destroy Colossal Mechanic
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCode(EFFECT_SELF_DESTROY)
+    e5:SetCondition(s.sdcon)
+    c:RegisterEffect(e5)
+    --Exiel ability
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,0))
+    e6:SetCategory(CATEGORY_REMOVE)
+    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e6:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e6:SetCondition(s.exielcon)
+    e6:SetTarget(s.exieltg)
+    e6:SetOperation(s.exielop)
+    c:RegisterEffect(e6)
 end
 function s.angelfilter(c)
 	return c:IsFaceup() and c:IsCode(210662254)
@@ -77,5 +103,71 @@ function s.addop(e,tp,eg,ep,ev,re,r,rp)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Colossal Mechanic Functions
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsFaceup() end
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+        c:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(500)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+        c:RegisterEffect(e2)
+    return true
+end
+function s.sdcon(e)
+    local c=e:GetHandler()
+    return c:GetAttack()>=3300
+end
+--Exiel Ability Function
+function s.exielcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.exieltg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
+end
+function s.exielop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+	if tc:IsRelateToBattle() then
+		if Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_UPDATE_ATTACK)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,8)
+            e1:SetValue(-tc:GetAttack()/4)
+            tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e2:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+			e2:SetLabel(Duel.GetTurnCount())
+			e2:SetReset(RESET_PHASE+PHASE_MAIN1,4)
+			e2:SetLabelObject(tc)
+			e2:SetCountLimit(1)
+			e2:SetOperation(s.returnop)
+			Duel.RegisterEffect(e2,tp)
+		end
+	end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    if ct==4 then
+        ct=0
+        c:SetTurnCounter(ct)
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.ReturnToField(e:GetLabelObject())
     end
 end

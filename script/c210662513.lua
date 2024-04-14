ed2dc24d2eb77638b57b05e20dd2481d709d7c3f
Konstak05 <zkonst05@gmail.com>
2024-04-14 22:30:37 +0300

    Nerf St. Dober

--- script/c210662513.lua
+++ script/c210662513.lua
@@ -41,14 +41,14 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 function s.angelfilter(c)
-    return c:IsAttribute(ATTRIBUTE_LIGHT)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
 end
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.angelfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.angelfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.angelfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    local g=Duel.SelectReleaseGroup(tp,s.angelfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
     if g then
         g:KeepAlive()
         e:SetLabelObject(g)

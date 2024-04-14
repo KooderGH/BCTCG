54e433296ae9dc1144acb01791076d981fdb4873
Konstak05 <zkonst05@gmail.com>
2024-04-14 20:53:16 +0300

    SnS and script St. Dober

--- script/c210662513.lua
+++ script/c210662513.lua
@@ -31,40 +31,48 @@ function s.initial_effect(c)
     e3:SetTarget(s.bntg)
     e3:SetOperation(s.bnop)
     c:RegisterEffect(e3)
+    --Surge Attack on Battle
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e4:SetCondition(s.surgecon)
+    e4:SetTarget(s.surgetg)
+    e4:SetOperation(s.surgeop)
+    c:RegisterEffect(e4)
 end
 function s.angelfilter(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT)
+    return c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 function s.spcon(e,c)
-	if c==nil then return true end
+    if c==nil then return true end
     return Duel.CheckReleaseGroup(c:GetControler(),s.angelfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.angelfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
-	if g then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-	return true
-	end
-	return false
+    local g=Duel.SelectReleaseGroup(tp,s.angelfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
 end
 --Replace destroy function
 function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsReason(REASON_BATTLE) and not c:IsReason(REASON_REPLACE) end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(500)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
-	c:RegisterEffect(e1)
-	return true
+    local c=e:GetHandler()
+    if chk==0 then return c:IsReason(REASON_BATTLE) and not c:IsReason(REASON_REPLACE) end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+    c:RegisterEffect(e1)
+    return true
 end
 --Banish Zombies function
 function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -75,6 +83,53 @@ end
 function s.bnop(e,tp,eg,ep,ev,re,r,rp)
     local bc=e:GetHandler():GetBattleTarget()
     if bc:IsRelateToBattle() then
-    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+        Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
     end
+end
+--Surge on Battle Function
+function s.surgecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    local Zone=d1
+    if tc then
+        local seq=tc:GetSequence()
+        local nseq=seq
+        Duel.Destroy(tc,REASON_EFFECT)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE_FIELD)
+        e1:SetRange(LOCATION_SZONE)
+        e1:SetLabel(nseq+16)
+        e1:SetOperation(s.disop)
+        e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+        Duel.RegisterEffect(e1,tp)
+    else
+        local seq=Zone
+        local nseq=seq
+        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD)
+            e1:SetCode(EFFECT_DISABLE_FIELD)
+            e1:SetRange(LOCATION_SZONE)
+            e1:SetLabel(nseq+16)
+            e1:SetOperation(s.disop)
+            e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.disop(e,tp)
+    return 0x1<<e:GetLabel()
 end

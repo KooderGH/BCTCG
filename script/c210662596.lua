4edf435a728d579844060e37ff778db3975616c6
Konstak05 <zkonst05@gmail.com>
2024-04-14 17:36:28 +0300

    SnS and script Nana

--- script/c210662596.lua
+++ script/c210662596.lua
@@ -1,27 +1,18 @@
 -- Nana & Doge
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    --activate
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
-    --Atk
+    --Atk up
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_SET_ATTACK_FINAL)
-    e2:SetRange(LOCATION_FZONE)
+    e2:SetRange(LOCATION_SZONE)
     e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-    e2:SetValue(s.val)
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_MACHINE))
+    e2:SetValue(-300)
     c:RegisterEffect(e2)
-    --Def
-    local e3=e2:Clone()
-    e3:SetCode(EFFECT_SET_DEFENSE_FINAL)
-    c:RegisterEffect(e3)
 end
-function s.val(e,c)
-	local r=c:GetRace()
-	if (r&RACE_MACHINE)>0 then return -200
-	else return 0 end
-end
-

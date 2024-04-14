216eb265fc24f230477b20f58723adf63766ec30
Konstak05 <zkonst05@gmail.com>
2024-04-14 21:09:06 +0300

    progress on Rajakong

--- script/c210662613.lua
+++ script/c210662613.lua
@@ -1,4 +1,4 @@
--- Divine Cyclone
+-- Heavenly Rajakong
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
@@ -13,38 +13,26 @@ function s.initial_effect(c)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    --Opponent No Battle Damage
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
-    e2:SetValue(1)
-    c:RegisterEffect(e2)
-    --Attack all each time
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_ATTACK_ALL)
-    e3:SetValue(1)
-    c:RegisterEffect(e3)
 end
-function s.angelfilter(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT)
+function s.papaoufilter(c)
+    return c:IsFaceup() and c:IsCode(210662115)
 end
 function s.spcon(e,c)
-	if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.angelfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.papaoufilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.angelfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
-	if g then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-	return true
-	end
-	return false
+    local g=Duel.SelectReleaseGroup(tp,s.papaoufilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
 end

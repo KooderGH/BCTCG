8d1919f9c7c6b94033c1e7828013aae748c46f63
Konstak05 <zkonst05@gmail.com>
2024-04-15 03:43:34 +0300

    Buff Sign Variants

--- script/c210662021.lua
+++ script/c210662021.lua
@@ -2,6 +2,11 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
+    --activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
     --Special summon count limit
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
--- script/c210662281.lua
+++ script/c210662281.lua
@@ -1,14 +1,19 @@
 -- Festival Ms.Sign
 local s,id=GetID()
 function s.initial_effect(c)
-	--cannot attack
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_ATTACK)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.atktarget)
-	c:RegisterEffect(e2)
+    --activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+    --cannot attack
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CANNOT_ATTACK)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+    e2:SetTarget(s.atktarget)
+    c:RegisterEffect(e2)
 end
 function s.atktarget(e,c)
 	return c:GetLevel()>=4

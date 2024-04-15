aa36ed56e5198f1a222026531de1a817a1dfafdf
Konstak05 <zkonst05@gmail.com>
2024-04-15 23:48:15 +0300

    SnS and script Zroco

--- script/c210662292.lua
+++ script/c210662292.lua
@@ -2,21 +2,86 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Cannot Attack
+    --Burrow DOWN ability
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_CANNOT_ATTACK)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.burrowdowntg)
+    e1:SetOperation(s.burrowdownop)
     c:RegisterEffect(e1)
-    --Can target 1 card on the field. destroy that target (Ignition)
+    --Burrow UP ability
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetTarget(s.destg)
-    e2:SetOperation(s.desop)
+    e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e2:SetCondition(s.burrowupcon)
+    e2:SetTarget(s.burrowuptg)
+    e2:SetOperation(s.burrowupop)
     c:RegisterEffect(e2)
+    --Cannot Attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_CANNOT_ATTACK)
+    c:RegisterEffect(e3)
+    --Can target 1 card on the field. destroy that target (Ignition)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
+end
+--Burrow DOWN ability
+function s.burrowdowntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.burrowdownop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
+end
+--Burrow UP ability
+function s.burrowupcon(e,tp,eg,ep,ev,re,r,rp)
+    return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
+end
+function s.desfilter(c,atk)
+    return c:IsFaceup()
+end
+function s.burrowuptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local c=e:GetHandler()
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
+    if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,c:GetAttack())
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+end
+function s.burrowupop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) then
+            Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+        end
+    end
 end
 --Destroy That Target Function
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)

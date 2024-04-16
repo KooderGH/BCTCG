c5c876a9349e3a30b633364550dd4b3bb6429aaf
Konstak05 <zkonst05@gmail.com>
2024-04-16 04:26:24 +0300

    SnS and script Zir Zeal

--- script/c210662290.lua
+++ script/c210662290.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Place as Spell
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,2))
+    e1:SetDescription(aux.Stringid(id,0))
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -18,10 +18,10 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
     --Then SS to opp
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,3))
+    e3:SetDescription(aux.Stringid(id,1))
     e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
     e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
     e3:SetRange(LOCATION_SZONE)
     e3:SetCode(EVENT_PHASE+PHASE_END)
     e3:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
@@ -29,6 +29,13 @@ function s.initial_effect(c)
     e3:SetTarget(s.destg)
     e3:SetOperation(s.desop)
     c:RegisterEffect(e3)
+    --Freeze Ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e4:SetCondition(s.freezecon)
+    e4:SetOperation(s.freezeop)
+    c:RegisterEffect(e4)
 end
 --e1
 function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -70,4 +77,31 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
             Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
         end
     end
+end
+--Freeze Function
+function s.freezecon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
+end
+function s.freezeop(e,tp,eg,ep,ev,re,r,rp)
+    local effp=e:GetHandler():GetControler()
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_SKIP_DP)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(0,1)
+        if Duel.GetTurnPlayer()==effp then
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetCondition(s.skipcon)
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+        else
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+        end
+        Duel.RegisterEffect(e1,effp)
+        Duel.NegateAttack()
+    end
+end
+function s.skipcon(e)
+    return Duel.GetTurnCount()~=e:GetLabel()
 end

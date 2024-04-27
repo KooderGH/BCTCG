0c93d73e1729a34f8faf47948b25eb33d758053b
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-28 01:28:22 +0300

    SnS and script cutter cat

--- script/c210661445.lua
+++ script/c210661445.lua
@@ -15,6 +15,41 @@ function s.initial_effect(c)
     e0:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
     e0:SetValue(1)
     c:RegisterEffect(e0)
+    --during damage calculation FIRE lose half of their ATK/DEF
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetTarget(s.atktg)
+    e1:SetOperation(s.atkop)
+    c:RegisterEffect(e1)
+    --Once while this card is face-up on the field: If it would be destroyed; gain double ATK instead.
+    local e2=Effect.CreateEffect(c)
+    e2:SetCode(EFFECT_DESTROY_REPLACE)
+    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.desreptg)
+    c:RegisterEffect(e2)
+    --"Grandon Corps" monsters you control cannot be banished by card effects
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EFFECT_CANNOT_REMOVE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTargetRange(1,1)
+    e3:SetTarget(s.rmlimit)
+    c:RegisterEffect(e3)
+    --This card cannot be destroyed by card effects.
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e4:SetValue(1)
+    c:RegisterEffect(e4)
 end
 --lcheck
 function s.machinefilter(c,scard,sumtype,tp)
@@ -22,4 +57,44 @@ function s.machinefilter(c,scard,sumtype,tp)
 end
 function s.lcheck(g,lc,sumtype,tp)
     return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+end
+--during damage calculation function
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-bc:GetAttack()/1.25)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-bc:GetDefense()/1.25)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e2)
+    end
+end
+--Replace destroy function
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return not c:IsReason(REASON_REPLACE) end
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(c:GetAttack())
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+        c:RegisterEffect(e1)
+    return true
+end
+--Cannot remove
+function s.rmlimit(e,c,tp,r)
+    return (c:IsCode(210661443) or c:IsCode(210661444) or c:IsCode(210661445) or c:IsCode(210661446) or c:IsCode(210661447)) and c:IsFaceup()
+        and c:IsControler(e:GetHandlerPlayer()) and not c:IsImmuneToEffect(e) and r&REASON_EFFECT>0
 end

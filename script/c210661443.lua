ddfcaaa3121b200f619107b98285e680125fe85c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-27 23:38:45 +0300

    Driller SnS and scripted

--- script/c210661443.lua
+++ script/c210661443.lua
@@ -15,6 +15,51 @@ function s.initial_effect(c)
     e0:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
     e0:SetValue(1)
     c:RegisterEffect(e0)
+    --Can target 1 S/T on the field. destroy that target (Ignition)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.destg)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
+    --Toxic Ability
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCondition(s.toxiccon)
+    e2:SetTarget(s.toxictg)
+    e2:SetOperation(s.toxicop)
+    c:RegisterEffect(e2)
+    --Attack all each time
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_ATTACK_ALL)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+    --Cannot be attacked
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e4:SetLabel(1)
+    e4:SetCondition(s.cbacon)
+    e4:SetValue(1)
+    c:RegisterEffect(e4)
+    --Cannot be targeted by card effects
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetLabel(1)
+    e5:SetCondition(s.cbacon)
+    e5:SetValue(1)
+    c:RegisterEffect(e5)
 end
 --lcheck
 function s.machinefilter(c,scard,sumtype,tp)
@@ -22,4 +67,72 @@ function s.machinefilter(c,scard,sumtype,tp)
 end
 function s.lcheck(g,lc,sumtype,tp)
     return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+end
+--Weaken
+function s.weakentg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return true end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.weakenop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+        e1:SetValue(-tc:GetAttack()/4)
+        tc:RegisterEffect(e1)
+    end
+end
+--Destroy That Target Function
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_SZONE+LOCATION_FZONE,LOCATION_SZONE+LOCATION_FZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_SZONE+LOCATION_FZONE,LOCATION_SZONE+LOCATION_FZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--Toxic on Battle function
+function s.toxiccon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.toxictg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.toxicop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc:IsRelateToBattle() and tc and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-tc:GetAttack()/4)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-tc:GetAttack()/4)
+        tc:RegisterEffect(e2)
+        Duel.NegateAttack()
+    end
+end
+--if there's another grandon Function
+function s.grandonfilter(c)
+    return c:IsFaceup() and (c:IsCode(210661444) or c:IsCode(210661445) or c:IsCode(210661446) or c:IsCode(210661447))
+end
+function s.cbacon(e)
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetMatchingGroupCount(s.grandonfilter,tp,LOCATION_ONFIELD,0,nil)>=e:GetLabel()
 end

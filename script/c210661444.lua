a3ce7b8f9313185fb7d30dc97d713369e6c53984
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-27 19:17:05 +0300

    prepare summoning material

---
+++ script/c210661444.lua
@@ -0,0 +1,16 @@
+--Neo Piledriver Cat
+--Scripted By Konstak
+--Effect:
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon
+	c:EnableReviveLimit()
+    Link.AddProcedure(c,s.machinefilter,2,2,s.lcheck)
+end
+--lcheck
+function s.machinefilter(c,scard,sumtype,tp)
+	return c:IsRace(RACE_MACHINE,scard,sumtype,tp) and c:IsAttribute(ATTRIBUTE_EARTH,scard,sumtype,tp) and not c:IsType(TYPE_TOKEN)
+end
+function s.lcheck(g,lc,sumtype,tp)
+	return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+end

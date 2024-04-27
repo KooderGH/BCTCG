a705a79cb7ba9fa2e0887cb98c31d163e360b53d
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-28 02:53:17 +0300

    script Saw Cat

--- script/c210661447.lua
+++ script/c210661447.lua
@@ -15,6 +15,45 @@ function s.initial_effect(c)
     e0:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
     e0:SetValue(1)
     c:RegisterEffect(e0)
+    --during damage calculation WATER lose half of their ATK/DEF
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetTarget(s.atktg)
+    e1:SetOperation(s.atkop)
+    c:RegisterEffect(e1)
+    --Targetable Warp Mechanic
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.warptg)
+    e2:SetOperation(s.warpop)
+    c:RegisterEffect(e2)
+    --"Grandon Corps" monsters you control cannot be tributed.
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTargetRange(1,1)
+    e3:SetTarget(s.rmlimit)
+    c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+    c:RegisterEffect(e4)
+    --"Grandon Corps" monsters you control cannot be returned to hand.
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_FIELD)
+    e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e5:SetCode(EFFECT_CANNOT_TO_HAND)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetTargetRange(1,1)
+    e5:SetTarget(s.rmlimit)
+    c:RegisterEffect(e5)
 end
 --lcheck
 function s.machinefilter(c,scard,sumtype,tp)
@@ -22,4 +61,66 @@ function s.machinefilter(c,scard,sumtype,tp)
 end
 function s.lcheck(g,lc,sumtype,tp)
     return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+end
+--during damage calculation function
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_WATER)) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-bc:GetAttack()/2)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-bc:GetDefense()/2)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e2)
+    end
+end
+--Banish until next turn
+function s.warpfilter(c)
+    return c:IsAbleToRemove() and c:IsMonster() and not c:IsCode(id)
+end
+function s.warptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    local g=Duel.GetMatchingGroup(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.warpop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.HintSelection(g)
+        if Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+            e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetReset(RESET_PHASE+PHASE_MAIN1,3)
+            e1:SetLabelObject(tc)
+            e1:SetCountLimit(1)
+            e1:SetOperation(s.returnop)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetTurnCount()~=e:GetLabel() then
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.ReturnToField(e:GetLabelObject())
+    end
+end
+--Cannot tribute/return
+function s.rmlimit(e,c,tp,r)
+    return (c:IsCode(210661443) or c:IsCode(210661444) or c:IsCode(210661445) or c:IsCode(210661446) or c:IsCode(210661447)) and c:IsFaceup()
+        and c:IsControler(e:GetHandlerPlayer()) and not c:IsImmuneToEffect(e) and r&REASON_EFFECT>0
 end

b53345d3f102a7bb5d3c993a1e7bf265047307bc
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-28 18:08:34 +0300

    SnS fully and script boulder

--- script/c210661581.lua
+++ script/c210661581.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
     --fusion material
     c:EnableReviveLimit()
-    Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
+    Fusion.AddProcMixRep(c,true,true,s.fil,1,1)
     Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
     --cannot be fusion material
     local e1=Effect.CreateEffect(c)
@@ -39,6 +39,21 @@ function s.initial_effect(c)
     local e5=e4:Clone()
     e5:SetCode(EFFECT_CANNOT_REMOVE)
     c:RegisterEffect(e5)
+    --self destroy
+    local e6=Effect.CreateEffect(c)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCode(EFFECT_SELF_DESTROY)
+    c:RegisterEffect(e6)
+    --Death send
+    local e7=Effect.CreateEffect(c)
+    e7:SetCategory(CATEGORY_TOGRAVE)
+    e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e7:SetCode(EVENT_TO_GRAVE)
+    e7:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e7:SetOperation(s.gyop)
+    c:RegisterEffect(e7)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
@@ -56,4 +71,39 @@ function s.cfilter(c,tp)
 end
 function s.contactop(g,tp,c)
 	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--GY send function
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+    if #g>0 then
+        Duel.HintSelection(g)
+        Duel.SendtoGrave(g,REASON_EFFECT)
+    end
+    --Return To Hand
+    local e2=Effect.CreateEffect(e:GetHandler())
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+    e2:SetLabel(Duel.GetTurnCount())
+    e2:SetReset(RESET_PHASE+PHASE_MAIN1,4)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetLabelObject(c)
+    e2:SetCountLimit(1)
+    e2:SetOperation(s.returnop)
+    Duel.RegisterEffect(e2,tp)
+end
+function s.disop(e,tp)
+    return 0x1<<e:GetLabel()
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    if ct==3 then
+        ct=0
+        c:SetTurnCounter(ct)
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.SendtoDeck(c,nil,tp,REASON_EFFECT)
+    end
 end

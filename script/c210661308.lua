c5d4b981720c138679ae3b21a8ea59a5501155ef
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 01:48:11 +0300

    SnS and script Weedwacker Cat

--- script/c210661308.lua
+++ script/c210661308.lua
@@ -7,5 +7,92 @@
 -- (4) When this card declares an attack on a monster: Negate that attack, and if you do, negate its effects until the 2nd next Main Phase.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --self destroy
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --No Battle damage
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+    --once normal summoned, Special summon as many weedwackers as possible
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_SUMMON_SUCCESS)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    --disable on Battle Ability
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetCategory(CATEGORY_DISABLE)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1)
+    e5:SetCondition(s.disablecon)
+    e5:SetTarget(s.disabletg)
+    e5:SetOperation(s.disableop)
+    c:RegisterEffect(e5)
+end
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and not (c:IsAttribute(ATTRIBUTE_EARTH) or c:IsRace(RACE_MACHINE))
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--Special summon
+function s.spfilter(c,e,tp)
+    return c:IsCode(210661308) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+--Disable on Battle Function
+function s.disablecon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
+end
+function s.disabletg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.disableop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+        tc:RegisterEffect(e1)
+        Duel.NegateAttack()
+    end
 end

9e6e610f1e44d7ed6605e10250d5453f85a835b9
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 01:34:59 +0300

    preparing script template

---
+++ script/c210661308.lua
@@ -0,0 +1,11 @@
+--Weedwacker Cat
+--Scripted by Konstak
+--Effect
+-- (1) If you control a monster that is not a EARTH Machine monster, destroy this card.
+-- (2) You take no battle damage from attacks involving this card.
+-- (3) When this card is Normal Summoned; Special Summon as many "Weedwacker Cat" from your Deck, Hand, and/or GY as possible.
+-- (4) When this card declares an attack on a monster: Negate that attack, and if you do, negate its effects until the 2nd next Main Phase.
+local s,id=GetID()
+function s.initial_effect(c)
+
+end

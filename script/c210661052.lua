637f0aff01678ba0bf56b566742544e216d6b07e
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 02:46:14 +0300

    progress on Necrodancer

--- script/c210661052.lua
+++ script/c210661052.lua
@@ -1,83 +1,82 @@
 --Necro-Dancer Cat
---Link Monster (LD,RD)
+--Scripted By Konstak.
+--Effect:
 --2 monsters with different names, except tokens.
---(1) Cannot be used as Link material.
 local s,id=GetID()
 function s.initial_effect(c)
     --Link Summon
     c:EnableReviveLimit()
     Link.AddProcedure(c,aux.NOT(aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOKEN)),2,2,s.lcheck)
-    --cannot link material
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
-    e1:SetValue(1)
-    c:RegisterEffect(e1)
     --equip
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_EQUIP)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.eqcon)
-    e2:SetTarget(s.eqtg)
-    e2:SetOperation(s.eqop)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_EQUIP)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.eqcon)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
     c:RegisterEffect(e1)
-    aux.AddEREquipLimit(c,s.eqcon,function(ec,_,tp) return ec:IsControler(tp,1-tp) end,s.equipop,e2)
+    aux.AddEREquipLimit(c,s.eqcon,function(ec,_,tp) return ec:IsControler(tp,1-tp) end,s.equipop,e1)
+    --atk/def
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetCondition(s.adcon)
+    e2:SetValue(s.atkval)
+    c:RegisterEffect(e2)
 end
 --lcheck
 function s.lcheck(g,lc,sumtype,tp)
-	return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+    return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
 end
 --Equip
-function s.eqfilter(c)
-	return c:GetFlagEffect(id)~=0
-end
 function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetHandler():GetEquipGroup():Filter(s.eqfilter,nil)
-	return #g==0
+    local g=e:GetHandler():GetEquipGroup():Filter(s.eqfilter,nil)
+    return #g==0
+end
+function s.eqfilter(c)
+    return c:GetFlagEffect(id)~=0 
+end
+function s.eqfilter2(c,e,tp,lg)
+    return c:IsFaceup() and (c:IsControler(tp) or c:IsAbleToChangeControler()) and lg:IsContains(c)
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToChangeControler() end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+    local lg=e:GetHandler():GetLinkedGroup()
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.eqfilter2(chkc,e,tp,lg) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingTarget(s.eqfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp,lg) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local g=Duel.SelectTarget(tp,s.eqfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp,lg)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
 function s.equipop(c,e,tp,tc)
-    local atk=tc:GetTextAttack()/2
-    if tc:IsFacedown() then atk=0 end
-    if atk<0 then atk=0 end
-    if not c:EquipByEffectAndLimitRegister(e,tp,tc,id) then return end
-    if atk>0 then
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_EQUIP)
-        e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE)
-        e2:SetCode(EFFECT_UPDATE_ATTACK)
-        e2:SetReset(RESET_EVENT|RESETS_STANDARD)
-        e2:SetValue(atk)
-        tc:RegisterEffect(e2)
-    end
-    if tc:IsFaceup() then
-        local e3=Effect.CreateEffect(c)
-        e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e3:SetCode(EVENT_ADJUST)
-        e3:SetRange(0x7f)
-        e3:SetOperation(s.op)
-        tc:RegisterEffect(e3)
-    end
+    c:EquipByEffectAndLimitRegister(e,tp,tc,id)  
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        if c:IsFaceup() and c:IsRelateToEffect(e) then
-            s.equipop(c,e,tp,tc)
-        else Duel.SendtoGrave(tc,REASON_RULE) end
+    if tc:IsFacedown() then return end 
+    if tc and tc:IsRelateToEffect(e) and tc:IsMonster() and s.eqcon(e,tp,eg,ep,ev,re,r,rp) then
+		s.equipop(c,e,tp,tc)
+	end
+end
+function s.adcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
+    return #g>0
+end
+function s.atkval(e,c)
+    local c=e:GetHandler()
+    local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
+    local atk=g:GetFirst():GetTextAttack()
+    if g:GetFirst():GetOriginalType()&TYPE_MONSTER==0 or atk<0 then
+        return 0
+    else
+        return atk/2
     end
 end

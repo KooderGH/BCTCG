f4f09a47267c9ca328a317a1427c79b73ebecb2e
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-28 21:17:43 +0300

    SnS and script True Love Neneko

---
+++ script/c210661589.lua
@@ -0,0 +1,105 @@
+--True Love Neneko
+--Scripted by Konstak
+--Effect
+-- (1) Cannot be attacked or targeted by effects.
+-- (2) While this card is in your hand, If a monster is targeted by a effect: Reveal this card; Negate the effect that targeted that monster and destroy it. You can only use this effect of "True Love Neneko" once per Duel.
+-- (3) Once per turn, you can activate this effect: Gain 750 LP for each card on the field.
+-- (4) You can banish this card from your GY (Quick); Gain 750 LP for each card in either GY.
+local s,id=GetID()
+function s.initial_effect(c)
+    --Cannot be attacked
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+    --Cannot be targeted by card effects
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+    --While in card, reveal card, negate effect, destroy that monster
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_CHAINING)
+    e3:SetRange(LOCATION_HAND)
+    e3:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    e3:SetCondition(s.negcon)
+    e3:SetTarget(s.distg)
+    e3:SetOperation(s.disop)
+    c:RegisterEffect(e3)
+    --Once per turn 750 recovery
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e4:SetCategory(CATEGORY_RECOVER)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.rtarget)
+    e4:SetOperation(s.roperation)
+    c:RegisterEffect(e4)
+    --Banish recovery
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,2))
+    e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e5:SetCategory(CATEGORY_RECOVER)
+    e5:SetType(EFFECT_TYPE_QUICK_O)
+    e5:SetCode(EVENT_FREE_CHAIN)
+    e5:SetRange(LOCATION_GRAVE)
+    e5:SetCost(aux.selfbanishcost)
+    e5:SetTarget(s.gravetarget)
+    e5:SetOperation(s.graveoperation)
+    c:RegisterEffect(e5)
+end
+--(2)
+function s.tgfilter(c,tp)
+	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+    local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+    return tg and tg:IsExists(s.tgfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp,chk)
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+--e3
+function s.rtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)>0 end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,0)
+end
+function s.roperation(e,tp,eg,ep,ev,re,r,rp)
+    local rt=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)*750
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    Duel.Recover(p,rt,REASON_EFFECT)
+end
+--Banish from GY
+function s.gravetarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroup(tp,LOCATION_GRAVE,LOCATION_GRAVE):FilterCount(aux.TRUE,e:GetHandler())>0 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,0)
+end
+function s.graveoperation(e,tp,eg,ep,ev,re,r,rp)
+	local rt=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,LOCATION_GRAVE)*750
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Recover(p,rt,REASON_EFFECT)
+end

abddaebbd0c815cf2773670a35cf09d601afd7ff
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 22:49:15 +0300

    prepare for actual script

--- script/c210661522.lua
+++ script/c210661522.lua
@@ -1,17 +1,23 @@
---Fusion Template
---Scripted by " "
---Effects:
+--Fiend Cat
+--Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
     --fusion material
     c:EnableReviveLimit()
-    Fusion.AddProcMixRep(c,true,true,s.fil,2,99)
+    Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
     Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --cannot be fusion material
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
     if contact then sumtype=0 end
-    return c:IsFaceup() and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp)) --c:IsRace(RACE_MACHINE,fc,sumtype,tp) and c:IsType(ATTRIBUTE_EARTH,fc,sumtype,tp) 
+    return c:IsFaceup() and c:IsType(ATTRIBUTE_DARK,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
 end
 function s.splimit(e,se,sp,st)
     return e:GetHandler():GetLocation()~=LOCATION_EXTRA

67376b72136a604f7ebe65c8e066e7a23d03915f
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 02:58:54 +0300

    Kai scripted

--- script/c210660106.lua
+++ script/c210660106.lua
@@ -13,6 +13,18 @@ function s.initial_effect(c)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+    --control
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_CONTROL)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetLabel(3)
+    e2:SetCondition(s.ctcon)
+    e2:SetTarget(s.cttg)
+    e2:SetOperation(s.ctop)
+    c:RegisterEffect(e2)
     --If this card would be destroyed; gain 500 ATK instead.
     local e3=Effect.CreateEffect(c)
     e3:SetCode(EFFECT_DESTROY_REPLACE)
@@ -22,10 +34,20 @@ function s.initial_effect(c)
     e3:SetCountLimit(1)
     e3:SetTarget(s.desreptg)
     c:RegisterEffect(e3)
+    --tohand
+    local e4=Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_RELEASE)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_NO_TURN_RESET)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.thtg)
+    e4:SetOperation(s.thop)
+    c:RegisterEffect(e4)
 end
 --Special Summon Function
 function s.trfilter(c)
-	return c:IsLevelAbove(7) and c:IsType(TYPE_MONSTER)
+	return c:IsLevelAbove(7) and c:IsMonster()
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.trfilter,1,false,aux.ReleaseCheckMMZ,nil) end
@@ -44,6 +66,27 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
     end
 end
+--Control Function
+function s.crabfilter(c)
+    return c:IsFaceup() and c:IsLevelAbove(7) and c:IsMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_SPELLCASTER) and not c:IsCode(id)
+end
+function s.ctcon(e)
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetMatchingGroupCount(s.crabfilter,tp,LOCATION_ONFIELD,0,nil)>=e:GetLabel()
+end
+function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsControlerCanBeChanged() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+    local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.GetControl(tc,tp,PHASE_END,1)
+    end
+end
 --Replace destroy function
 function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
@@ -55,4 +98,20 @@ function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
     e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
     c:RegisterEffect(e1)
     return true
+end
+--If tributed function
+function s.thfilter(c)
+	return c:IsLevelBelow(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_SPELLCASTER) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end

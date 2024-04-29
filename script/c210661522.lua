13c28f2a3e3cfbb426816dd777a95f327666cd81
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 22:57:28 +0300

    Script a bitty Fiend Cat

--- script/c210661522.lua
+++ script/c210661522.lua
@@ -13,11 +13,24 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
     e1:SetValue(1)
     c:RegisterEffect(e1)
+    --dis field
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetOperation(s.disop)
+    c:RegisterEffect(e2)
+    local e3=e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    local e4=e2:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
     if contact then sumtype=0 end
-    return c:IsFaceup() and c:IsType(ATTRIBUTE_DARK,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
+    return c:IsFaceup() and c:IsType(ATTRIBUTE_DARK,fc,sumtype,tp) and c:IsRace(RACE_FIEND,fc,sumtype,tp) and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
 end
 function s.splimit(e,se,sp,st)
     return e:GetHandler():GetLocation()~=LOCATION_EXTRA
@@ -30,4 +43,20 @@ function s.cfilter(c,tp)
 end
 function s.contactop(g,tp,c)
     Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--Disable Zone Function
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE,PLAYER_NONE,0)<=0 and Duel.GetLocationCount(1-tp,LOCATION_MZONE,PLAYER_NONE,0)<=0 then return end
+    --disable field
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_DISABLE_FIELD)
+    e1:SetOperation(s.disop)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+    e:GetHandler():RegisterEffect(e1)
+end
+function s.disop(e,tp)
+    local dis1=Duel.SelectDisableField(tp,1,LOCATION_MZONE,LOCATION_MZONE,0)
+    return dis1
 end

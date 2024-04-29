714e80e09c8b876f09db9c8ad2030d37dd447ef4
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 03:52:43 +0300

    delete fishman's script

--- script/c210661036.lua
+++ script/c210661036.lua
@@ -1,57 +1,5 @@
---Cameraman Cat
---Scripted By Konstak
---Link Monster (LD,RD)
--- 2 monsters with different names, except tokens.
--- (1) Cannot be used as Link material.
--- (2) If this card battles a WIND type monster, double this card's ATK until the end of the damage step.
--- (3) WIND monsters cannot target this card for an attack.
+--Fishman
 local s,id=GetID()
 function s.initial_effect(c)
-    --Link Summon
-    c:EnableReviveLimit()
-    Link.AddProcedure(c,aux.NOT(aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOKEN)),2,2,s.lcheck)
-    --cannot link material
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
-    e1:SetValue(1)
-    c:RegisterEffect(e1)
-    --during damage calculation gain double atk
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_BATTLE_START)
-    e2:SetTarget(s.atktg)
-    e2:SetOperation(s.atkop)
-    c:RegisterEffect(e2)
-    --Cannot be selected as attack target
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetValue(function(e,c) return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND) end)
-    c:RegisterEffect(e3)
-end
---lcheck
-function s.lcheck(g,lc,sumtype,tp)
-    return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
-end
---during damage calculation function
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and bc:IsAttribute(ATTRIBUTE_WIND) end
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToBattle() then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(c:GetAttack())
-        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-        c:RegisterEffect(e1)
-    end
+
 end

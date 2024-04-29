7d2ba30ac8ac1c8007da99e3af17a0c67ec62547
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 04:49:30 +0300

    script Cameraman

--- script/c210661149.lua
+++ script/c210661149.lua
@@ -3,7 +3,7 @@
 --Link Monster (LD,RD)
 -- 2 monsters with different names, except tokens.
 -- (1) If this card battles a WIND type monster, double this card's ATK until the end of the damage step.
--- (2) If this card would be destroyed; Special Summon 1 EARTH Warrior monster from your GY instead. You can only use this effect of "Cameraman Cat" once per turn.
+-- (2) If this card would be destroyed; Special Summon 1 EARTH Warrior monster from your GY. You can only use this effect of "Cameraman Cat" once per turn.
 local s,id=GetID()
 function s.initial_effect(c)
     --Link Summon
@@ -18,6 +18,17 @@ function s.initial_effect(c)
     e1:SetTarget(s.atktg)
     e1:SetOperation(s.atkop)
     c:RegisterEffect(e1)
+    --Destroy and Special Summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
+    e2:SetTarget(s.sstg)
+    e2:SetOperation(s.ssop)
+    c:RegisterEffect(e2)
 end
 --lcheck
 function s.lcheck(g,lc,sumtype,tp)
@@ -37,5 +48,21 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetValue(c:GetAttack())
         e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
         c:RegisterEffect(e1)
+    end
+end
+--SS from GY
+function s.specialfilter(c)
+    return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR)
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end

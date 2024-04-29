0571e833f87c88fc3ec1b3a3c1a20d52e71396a5
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 00:31:49 +0300

    script Li'l Gau

---
+++ script/c210661067.lua
@@ -0,0 +1,88 @@
+--Li'l Gau
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --special summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --once normal summoned, Special summon as many grateful cranes as possible
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
+    c:RegisterEffect(e2)
+    local e3=e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    local e4=e2:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    --Can Attack While Defense
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetCode(EFFECT_DEFENSE_ATTACK)
+    c:RegisterEffect(e5)
+    local e6=Effect.CreateEffect(c)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e6:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetValue(function(e,c) return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK) end)
+    c:RegisterEffect(e6)
+    --Li'l Gau gains 500 ATK
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_SINGLE)
+    e7:SetCode(EFFECT_UPDATE_ATTACK)
+    e7:SetRange(LOCATION_MZONE)
+    e7:SetTargetRange(LOCATION_MZONE,0)
+    e7:SetValue(s.adval)
+    c:RegisterEffect(e7)
+end
+--Special Summon Function
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+    end
+end
+--Special summon
+function s.spfilter(c,e,tp)
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+    end
+end
+--Atk update function
+function s.atkfilter(c)
+    return c:IsFaceup() and c:IsCode(id)
+end
+function s.adval(e,c)
+    return Duel.GetMatchingGroupCount(s.atkfilter,c:GetControler(),LOCATION_MZONE,0,nil)*500
+end

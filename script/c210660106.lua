10a0613ef8e12b917fd6a321914fbf71574156e4
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 02:35:38 +0300

    Love how i need more than half an hour just for 1 effect

--- script/c210660106.lua
+++ script/c210660106.lua
@@ -1,6 +1,37 @@
 --Kai
---Scripted By ""
+--Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --special summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1)
+    e1:SetCost(s.spcost)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+end
+--Special Summon Function
+function s.trfilter(c)
+	return c:IsLevelAbove(7) and c:IsType(TYPE_MONSTER)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.trfilter,1,false,aux.ReleaseCheckMMZ,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local cg=Duel.SelectReleaseGroupCost(tp,s.trfilter,1,1,false,aux.ReleaseCheckMMZ,nil)
+    Duel.Release(cg,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    end
 end

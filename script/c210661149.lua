63f177fab2f3f0da05b8d3ef4046e49f93ae4324
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-29 03:04:15 +0300

    progress on Cameraman

--- script/c210661149.lua
+++ script/c210661149.lua
@@ -15,8 +15,33 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
     e1:SetValue(1)
     c:RegisterEffect(e1)
+    --during damage calculation gain 2900 atk
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_BATTLE_START)
+    e2:SetTarget(s.atktg)
+    e2:SetOperation(s.atkop)
+    c:RegisterEffect(e2)
 end
 --lcheck
 function s.lcheck(g,lc,sumtype,tp)
     return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
+end
+--during damage calculation function
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsAttribute(ATTRIBUTE_WIND) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(c:GetAttack())
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        c:RegisterEffect(e1)
+    end
 end

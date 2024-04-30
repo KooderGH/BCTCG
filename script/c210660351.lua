4a1b503098139557d7e5b5e07ad44385041d2b09
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 04:07:11 +0300

    SnS Twinstars

--- script/c210660351.lua
+++ script/c210660351.lua
@@ -33,6 +33,18 @@ function s.initial_effect(c)
     e4:SetTarget(s.destg)
     e4:SetOperation(s.desop)
     c:RegisterEffect(e4)
+    --Tribute
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,0))
+    e5:SetType(EFFECT_TYPE_IGNITION)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+    e5:SetCountLimit(1)
+    e5:SetCondition(s.tributecondition)
+    e5:SetCost(s.trcost)
+    e5:SetTarget(s.trtg)
+    e5:SetOperation(s.trop)
+    c:RegisterEffect(e5)
 end
 --e1
 function s.spfilter(c)
@@ -61,5 +73,33 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local tg=Duel.GetTargetCards(e)
     if #tg>0 then
         Duel.Destroy(tg,REASON_EFFECT)
+    end
+end
+--e4
+function s.spellfilter(c)
+    return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_SPELLCASTER) and (c:IsLevel(6) or c:IsLevel(7) or c:IsLevel(8)) and c:IsAbleToHand()
+end
+function s.spellfilter2(c)
+    return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
+end
+function s.tributecondition(e)
+    return Duel.IsExistingMatchingCard(s.spellfilter2,e:GetHandlerPlayer(),LOCATION_MZONE,0,4,nil)
+end
+function s.trcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsRace,1,false,nil,nil,RACE_SPELLCASTER) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local g=Duel.SelectReleaseGroupCost(tp,Card.IsRace,1,1,false,nil,nil,RACE_SPELLCASTER)
+    Duel.Release(g,REASON_COST)
+end
+function s.trtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.spellfilter,tp,LOCATION_DECK,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.trop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.spellfilter,tp,LOCATION_DECK,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end

aa929e325ffc180e45be586853648be49dcca54b
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 03:16:42 +0300

    windy effect 2

--- script/c210660075.lua
+++ script/c210660075.lua
@@ -11,6 +11,17 @@ function s.initial_effect(c)
     e1:SetCountLimit(1)
     e1:SetCondition(s.spcon)
     c:RegisterEffect(e1)
+    --during damage calculation gain 700 atk
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+    e2:SetCode(EVENT_BATTLE_START)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.atktg)
+    e2:SetOperation(s.atkop)
+    c:RegisterEffect(e2)
 end
 --e1
 function s.spfilter(c)
@@ -21,4 +32,25 @@ function s.spcon(e,c)
     local tp=c:GetControler()
     return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+--e2
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(700)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        c:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        c:RegisterEffect(e2)
+    end
 end

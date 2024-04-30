6f013fc2e1af2a6a84b152665789ffd347f48106
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-01 01:07:06 +0300

    SnS fully and script Summer Vigler

--- script/c210663001.lua
+++ script/c210663001.lua
@@ -1,1 +1,147 @@
-
+--Summer Vacation Vigler
+--Scripted by Konstak
+--Effect
+-- 2 "Detective Vigler" monsters (Fusion Monster)
+-- (1) Cannot be used as Fusion Material.
+-- (2) Must be Special Summoned only (from your Extra Deck) by sending 2 "Detective Vigler" from your field to the GY. (You do not use "Polymerization")
+-- (3) When your opponent activates a monster effect: You can discard this card and a level 4 or higher DARK Warrior monster from your hand; negate that effect and destroy it.
+-- (4) When this card is Special Summoned: Target 2 cards on the field; Banish them and deal 2000 damage to your opponent.
+-- (5) When this card on the field is sent to the GY; You can add 1 card from your GY to your hand except "Summer Vacation Vigler".
+local s,id=GetID()
+function s.initial_effect(c)
+    --Can only control one
+    c:SetUniqueOnField(1,0,id)
+    --fusion material
+    c:EnableReviveLimit()
+    Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
+    Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --cannot be fusion material
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+    e0:SetValue(1)
+    c:RegisterEffect(e0)
+    --Negate
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.discon)
+    e1:SetCost(s.discost)
+    e1:SetTarget(s.distg)
+    e1:SetOperation(s.disop)
+    c:RegisterEffect(e1)
+    --Special banish damage
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_REMOVE+CATEGORY_DAMAGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetTarget(s.tbtarget)
+    e2:SetOperation(s.tboperation)
+    c:RegisterEffect(e2)
+    --Recover from grave
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetCondition(s.thcon)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
+    c:RegisterEffect(e3)
+end
+--Special Summon Functions
+function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
+	if contact then sumtype=0 end
+	return c:IsFaceup() and c:IsCode(210660431)
+end
+function s.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil,tp)
+end
+function s.cfilter(c,tp)
+	return c:IsAbleToGraveAsCost() and (c:IsControler(tp) or c:IsFaceup())
+end
+function s.contactop(g,tp,c)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--e1
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+function s.costfilter(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsAbleToGraveAsCost() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevelAbove(4)
+end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() and 
+		Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,c)
+	g:AddCard(c)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+--e2
+function s.tbtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and tc:IsAbleToRemove() then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+		if tc:IsFaceup() and tc:IsAttribute(ATTRIBUTE_DARK) then
+			Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+		end
+	end
+end
+function s.tboperation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+		if tc:IsLocation(LOCATION_REMOVED) and tc:IsMonster() and tc:IsAttribute(ATTRIBUTE_DARK) then
+			Duel.Damage(1-tp,2000,REASON_EFFECT)
+		end
+	end
+end
+--e3
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousPosition(POS_FACEUP)
+		and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.thfilter(c)
+	return c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

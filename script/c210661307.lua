f620c99a544ad7bf28154b111d29404cba0c49dd
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-01 02:23:41 +0300

    SnS Housewife

---
+++ script/c210661307.lua
@@ -0,0 +1,26 @@
+--Housewife
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --fusion material
+    c:EnableReviveLimit()
+    Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
+    Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+end
+--Special Summon Functions
+function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
+    if contact then sumtype=0 end
+    return c:IsFaceup() and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
+end
+function s.splimit(e,se,sp,st)
+    return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function s.contactfil(tp)
+    return Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil,tp)
+end
+function s.cfilter(c,tp)
+    return c:IsAbleToGraveAsCost() and (c:IsControler(tp) or c:IsFaceup())
+end
+function s.contactop(g,tp,c)
+    Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end

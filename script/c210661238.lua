e88e5718e6d7b71ba9a24346babdfff1039e0f03
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 22:48:36 +0300

    hmmm

--- script/c210661238.lua
+++ script/c210661238.lua
@@ -6,6 +6,47 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
     Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --Summon and destroy
+    c:EnableCounterPermit(0x4003)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_COUNTER)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.addct)
+    e1:SetOperation(s.addc)
+    e1:SetCountLimit(1)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --add one wind monster from your deck or GY to hand
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1,id)
+    e4:SetCost(s.srcost)
+    e4:SetTarget(s.srtg)
+    e4:SetOperation(s.srop)
+    c:RegisterEffect(e4)
+    --End phase add 1 counter
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,2))
+    e5:SetCategory(CATEGORY_COUNTER)
+    e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCode(EVENT_PHASE+PHASE_END)
+    e5:SetCountLimit(1)
+    e5:SetOperation(s.fcoperation)
+    c:RegisterEffect(e5)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
@@ -23,4 +64,47 @@ function s.cfilter(c,tp)
 end
 function s.contactop(g,tp,c)
     Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--addcounter
+s.counter_list={0x4003}
+function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x4003)
+end
+function s.addc(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        e:GetHandler():AddCounter(0x4003,1)
+    end
+end
+--Removecounter
+function s.srcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x4003,1,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x4003,1,REASON_COST)
+end
+function s.srfilter(c)
+    return c:IsAbleToHand()
+end
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--Add counter to everyone
+function s.levelfilter(c)
+	return c:IsFaceup()
+end
+function s.fcoperation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.levelfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		tc:AddCounter(0x4003,1)
+	end
 end

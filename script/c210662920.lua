7f870ec0c8afcdac2ae284e757fdb3631cd0057e
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-01 02:05:38 +0300

    The 12th Angel in both bctcgs

---
+++ script/c210662920.lua
@@ -0,0 +1,6 @@
+-- The 12th Angel
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
---
+++ script/c210663011.lua
@@ -0,0 +1,7 @@
+--The 12th Angel
+--Scripted by " "
+--Effects:
+local s,id=GetID()
+function s.initial_effect(c)
+
+end

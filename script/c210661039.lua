142e716ca330882b178d1ebda173986c72eff4db
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 18:16:47 +0300

    SnS and script Juliet/Icat

--- script/c210661039.lua
+++ script/c210661039.lua
@@ -6,6 +6,22 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
     Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --cannot attack
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_ATTACK)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+    e1:SetTarget(s.atktarget)
+    c:RegisterEffect(e1)
+    --Cannot Target
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(function(e,c) return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND) end)
+    c:RegisterEffect(e2)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
@@ -23,4 +39,8 @@ function s.cfilter(c,tp)
 end
 function s.contactop(g,tp,c)
     Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--Cannot Attack
+function s.atktarget(e,c)
+    return c:IsAttribute(ATTRIBUTE_DARK) or c:IsAttribute(ATTRIBUTE_FIRE)
 end

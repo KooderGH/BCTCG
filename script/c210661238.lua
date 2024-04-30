059bc718825c501e8fde51d5e8caafdb3e8e8998
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-04-30 23:35:26 +0300

    more experimenting on richest cat

--- script/c210661238.lua
+++ script/c210661238.lua
@@ -2,6 +2,8 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
+    --Can only control one
+    c:SetUniqueOnField(1,0,id)
     --fusion material
     c:EnableReviveLimit()
     Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
@@ -24,28 +26,38 @@ function s.initial_effect(c)
     local e3=e1:Clone()
     e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e3)
-    --Counter remove add monster
+    --Counter remove defense
     local e4=Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id,1))
     e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e4:SetType(EFFECT_TYPE_IGNITION)
     e4:SetRange(LOCATION_MZONE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetCountLimit(1,id)
+    e4:SetCountLimit(2)
     e4:SetCost(s.srcost)
-    e4:SetTarget(s.srtg)
-    e4:SetOperation(s.srop)
+    e4:SetTarget(s.crtg)
+    e4:SetOperation(s.crop)
     c:RegisterEffect(e4)
-    --End phase add 1 counter
+    --Counter remove attack
     local e5=Effect.CreateEffect(c)
     e5:SetDescription(aux.Stringid(id,2))
-    e5:SetCategory(CATEGORY_COUNTER)
-    e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e5:SetType(EFFECT_TYPE_IGNITION)
     e5:SetRange(LOCATION_MZONE)
-    e5:SetCode(EVENT_PHASE+PHASE_END)
     e5:SetCountLimit(1)
-    e5:SetOperation(s.coperation)
+    e5:SetCost(s.srcost2)
+    e5:SetTarget(s.crtg2)
+    e5:SetOperation(s.crop2)
     c:RegisterEffect(e5)
+    --End phase add 1 counter based on the number of cards on the field.
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,3))
+    e6:SetCategory(CATEGORY_COUNTER)
+    e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCode(EVENT_PHASE+PHASE_END)
+    e6:SetCountLimit(1)
+    e6:SetOperation(s.coperation)
+    c:RegisterEffect(e6)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
@@ -72,27 +84,49 @@ function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.addc(e,tp,eg,ep,ev,re,r,rp)
     if e:GetHandler():IsRelateToEffect(e) then
-        e:GetHandler():AddCounter(0x4003,1)
+        e:GetHandler():AddCounter(0x4003,2)
     end
 end
---Removecounter
+--Counter for Defense
 function s.srcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x4003,1,REASON_COST) end
 	e:GetHandler():RemoveCounter(tp,0x4003,1,REASON_COST)
 end
-function s.srfilter(c)
-    return c:IsAbleToHand()
+function s.crtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+function s.crop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_DEFENSE)
+		e1:SetValue(500)
+		tc:RegisterEffect(e1)
+	end
 end
-function s.srop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+--Counter for Attack
+function s.srcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x4003,2,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x4003,1,REASON_COST)
+end
+function s.crtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.crop2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		tc:RegisterEffect(e1)
 	end
 end
 --Add counter to everyone

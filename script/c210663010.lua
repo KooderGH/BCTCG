2f8126cab45ad85991d5192b87a964d614954a66
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-01 00:31:39 +0300

    Progress on Jizo's Wedding Cake

--- script/c210663010.lua
+++ script/c210663010.lua
@@ -1,7 +1,107 @@
 --Jizo's Wedding Cake
---Scripted by " "
---Effects:
+--Scripted by Konstak
+--Effect
+-- (1) If you control a monster that is not a WIND Attribute monster, destroy this card.
+-- (2) If you control a WIND Attribute monster, you can Special Summon this card from your hand.
+-- (3) If an attack is declared involving this card and a Warrior monster: Banish that Warrior monster.
+-- (4) You can only use 1 of these effects of "Jizo's Wedding Cake" per turn, and only once that turn.
+-- * You can Target 1 card on the field (Ignition); Negate its effects.
+-- * If this card is sent to the GY: You can target 1 card on the field; Negate its effects. You cannot activate this effect if you control a non WIND monster.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --self destroy (1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_SELF_DESTROY)
+    e1:SetCondition(s.sdcon)
+    c:RegisterEffect(e1)
+    --Special Summon this card (2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e2:SetCode(EFFECT_SPSUMMON_PROC)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetCondition(s.spcon)
+    c:RegisterEffect(e2)
+    --Can banish Warrior monsters (3)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_START)
+    e3:SetTarget(s.bntg)
+    e3:SetOperation(s.bnop)
+    c:RegisterEffect(e3)
+    --Can target 1 card on the field. destroy that target (Ignition) (4)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1,id)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
+    --Once sent to GY target 1 card on the field. destroy that target (5)
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetCategory(CATEGORY_DESTROY)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e5:SetCode(EVENT_TO_GRAVE)
+    e5:SetCountLimit(1,id)
+    e5:SetCondition(s.descon)
+    e5:SetTarget(s.destg)
+    e5:SetOperation(s.desop)
+    c:RegisterEffect(e5)
+end
+--Self Destroy Function
+function s.sdfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.sdcon(e)
+    return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+--Filter and Special summon function
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+--Banish Zombies function
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_WARRIOR) end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() then
+    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+--Target 1 card on the field; destroy that target.
+function s.desfilter(c)
+    return c:IsMonster() and c:IsAttributeExcept(ATTRIBUTE_WIND)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    return not Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end

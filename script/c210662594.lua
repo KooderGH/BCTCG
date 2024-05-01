430f09d7198a8612b6c3f06fd4a50dcbf7188422
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-02 01:12:20 +0300

    Big Peng Z on BCETCG (Wip)

---
+++ script/c210662594.lua
@@ -0,0 +1,70 @@
+-- Big Peng Z
+--Scripted by Konstak.
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e1=Effect.CreateEffect(c)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --Revive
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCondition(s.sumcon)
+    e2:SetTarget(s.sumtg)
+    e2:SetOperation(s.sumop)
+    c:RegisterEffect(e2)
+end
+function s.zombiefilter(c)
+	return c:IsRace(RACE_ZOMBIE) and c:IsFaceup()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.zombiefilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.zombiefilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
+end
+function s.sumcon(e,tp,c)
+    if c==nil then return true end
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.CheckLPCost(tp,1000)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and ct<1 end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    if e:GetHandler():IsRelateToEffect(e) then
+        ct=ct+1
+        c:SetTurnCounter(ct)
+        Duel.PayLPCost(tp,1000)
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+    end
+end

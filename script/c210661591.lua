f707574d4cd158c5f0bb320b3ecda61581397148
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-02 19:39:58 +0300

    SnS fully and script Kaito

--- script/c210661591.lua
+++ script/c210661591.lua
@@ -13,6 +13,35 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
     e1:SetValue(1)
     c:RegisterEffect(e1)
+    --Can banish zombie monsters
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_BATTLE_START)
+    e2:SetTarget(s.bntg)
+    e2:SetOperation(s.bnop)
+    c:RegisterEffect(e2)
+    --to defense
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_POSITION)
+    e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e3:SetCode(EVENT_SUMMON_SUCCESS)
+    e3:SetTarget(s.deftg)
+    e3:SetOperation(s.defop)
+    c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    local e5=e3:Clone()
+    e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e5)
+    --Can Attack While Defense
+    local e6=Effect.CreateEffect(c)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetCode(EFFECT_DEFENSE_ATTACK)
+    c:RegisterEffect(e6)
     --Once destroyed Special Summon 1 EARTH Machine Monster
     local e7=Effect.CreateEffect(c)
     e7:SetDescription(aux.Stringid(id,1))
@@ -20,6 +49,8 @@ function s.initial_effect(c)
     e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e7:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
     e7:SetCode(EVENT_TO_GRAVE)
+    e7:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    e7:SetCondition(s.spcon)
     e7:SetTarget(s.sptg)
     e7:SetOperation(s.spop)
     c:RegisterEffect(e7)
@@ -41,9 +72,38 @@ end
 function s.contactop(g,tp,c)
     Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
 end
+--Banish Zombies function
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() then
+        Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+--To defense
+function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return e:GetHandler():IsAttackPos() end
+    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
+end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
+        Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+    end
+end
 --Once destroyed function
 function s.filter(c,e,tp)
     return c:IsCode(210663002) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.countfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE)
+end
+function s.spcon(e)
+	return Duel.IsExistingMatchingCard(s.countfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0

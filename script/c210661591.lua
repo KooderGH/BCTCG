70c78cb80785f82df1b15492095446a4c0256ab3
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-02 19:09:50 +0300

    prepare for Kaito & Cat

---
+++ script/c210661591.lua
@@ -0,0 +1,33 @@
+--Kaito & Cat
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --fusion material
+    c:EnableReviveLimit()
+    Fusion.AddProcMixRep(c,true,true,s.fil,3,3)
+    Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --cannot be fusion material
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+end
+--Special Summon Functions
+function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
+    if contact then sumtype=0 end
+    return c:IsFaceup() and (not contact or c:IsType(TYPE_MONSTER,fc,sumtype,tp))
+end
+function s.splimit(e,se,sp,st)
+    return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function s.contactfil(tp)
+    return Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil,tp)
+end
+function s.cfilter(c,tp)
+    return c:IsAbleToGraveAsCost() and (c:IsControler(tp) or c:IsFaceup())
+end
+function s.contactop(g,tp,c)
+    Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end

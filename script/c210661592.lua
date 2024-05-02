bd0c55e62fbe830fca69c676e369cdf85c8ef1e8
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-02 18:51:22 +0300

    blank scripts for both

---
+++ script/c210661592.lua
@@ -0,0 +1,6 @@
+--Neko Rin
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
---
+++ script/c210661593.lua
@@ -0,0 +1,6 @@
+--Neko Len
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+
+end

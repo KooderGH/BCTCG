b49f4d5a058545d4ad64fbdaf9ff43763a9b8574
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-03 23:38:40 +0300

    Script Richest Cat

--- script/c210661238.lua
+++ script/c210661238.lua
@@ -8,6 +8,38 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixRep(c,true,true,s.fil,2,2)
     Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+    --Summon and dmg
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DAMAGE)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetOperation(s.smop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --damage controller
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DAMAGE)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_CONTROL_CHANGED)
+    e4:SetOperation(s.ccop)
+    c:RegisterEffect(e4)
+    --Once destroyed
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,2))
+    e5:SetCategory(CATEGORY_RECOVER)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetOperation(s.drop)
+    c:RegisterEffect(e5)
 end
 --Special Summon Functions
 function s.fil(c,fc,sumtype,tp,sub,mg,sg,contact)
@@ -25,4 +57,22 @@ function s.cfilter(c,tp)
 end
 function s.contactop(g,tp,c)
     Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+--Damage
+function s.smop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.Damage(tp,4000,REASON_EFFECT)
+    end
+end
+--Control Changed
+function s.ccop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.Damage(tp,12000,REASON_EFFECT)
+    end
+end
+--Recover
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.Recover(tp,7000,REASON_EFFECT)
+    end
 end

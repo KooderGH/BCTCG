46df6c3edbdf32ef98f6eef504f560e192ea922c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-03 22:47:57 +0300

    Rework Manic Mohawk Cat

--- script/c210661091.lua
+++ script/c210661091.lua
@@ -4,10 +4,8 @@
 -- 1 monster, except tokens.
 -- (1) Cannot be used as Link material.
 -- (2) You can only control one "Manic Mohawk Cat".
--- (3) Your opponent can only target "Manic Mohawk Cat" for attacks.
--- (4) You take no battle damage from attacks involving this card.
--- (5) While this card is face-up on the field, other monsters you control cannot be destroyed by card effects.
--- (6) Once while this card is face-up on the field: If it would be destroyed; it is not destroyed.
+-- (3) When an opponent's monster declares an attack: You can Tribute this card; change that opponent's monster's ATK to 0, until the end of this turn.
+-- (4) If this card is in your GY (Quick Effect): You can Tribute 1 Level 3 or lower monster; Special Summon this card.
 local s,id=GetID()
 function s.initial_effect(c)
     --Can only control one
@@ -22,54 +20,69 @@ function s.initial_effect(c)
     e0:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
     e0:SetValue(1)
     c:RegisterEffect(e0)
-    --Only this monster can be attack target
+    --Change ATK to 0
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+    e1:SetCategory(CATEGORY_ATKCHANGE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(0,LOCATION_MZONE)
-    e1:SetValue(s.atlimit)
+    e1:SetCondition(s.atkcon)
+    e1:SetCost(s.atkcost)
+    e1:SetOperation(s.atkop)
     c:RegisterEffect(e1)
-    --No Battle damage
+    --Special Summon itself from the GY
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-    e2:SetValue(1)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetHintTiming(0,TIMING_BATTLE_START+TIMING_END_PHASE)
+    e2:SetCost(s.spcost)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
-    --other cards cannot be destroyed by card effects.
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD)
-    e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetTargetRange(LOCATION_MZONE,0)
-    e3:SetValue(1)
-    e3:SetTarget(s.monsterfilter)
-    c:RegisterEffect(e3)
-    --Once while this card is face-up on the field: If it would be destroyed; It isn't.
-    local e4=Effect.CreateEffect(c)
-    e4:SetCode(EFFECT_DESTROY_REPLACE)
-    e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetCountLimit(1)
-    e4:SetTarget(s.desreptg)
-    c:RegisterEffect(e4)
 end
 --lcheck
 function s.lcheck(g,lc,sumtype,tp)
     return g:CheckDifferentProperty(Card.GetCode,lc,sumtype,tp)
 end
---Only this monster can be attack target function
-function s.atlimit(e,c)
-    return c:IsFacedown() or not c:IsCode(id)
+--e1
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	return at and Duel.IsTurnPlayer(1-tp) and at:HasNonZeroAttack()
 end
---Monster filter
-function s.monsterfilter(e,c,tp,r)
-    return c:IsFaceup() and c:IsType(TYPE_MONSTER) and not c:IsCode(id)
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
 end
---once per turn cannot be destroyed
-function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	if tc:IsRelateToBattle() and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+--e2
+function s.cfilter(c,ft,tp)
+	return c:IsLevelBelow(3) and c:IsFaceup()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,ft,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,ft,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) end
-	return true
+	if c:IsRelateToEffect(e) then 
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end

b48d87557cd7e261896e00852db6b06d01c0bf71
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-04 05:35:35 +0300

    fix lucia again

--- script/c210663007.lua
+++ script/c210663007.lua
@@ -98,16 +98,15 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 end
 --Cannot Attack
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
-    if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-    local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chck:IsFaceup() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+    Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=Duel.GetFirstTarget()
-    if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+    if tc:IsRelateToEffect(e) then
         local e1=Effect.CreateEffect(c)
         e1:SetDescription(3206)
         e1:SetType(EFFECT_TYPE_SINGLE)

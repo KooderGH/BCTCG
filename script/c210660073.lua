c0f2b3163c6f45ff2d25a026e7484bd67474e0ab
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-05 18:05:44 +0300

    fix Oda Spacing

--- script/c210660073.lua
+++ script/c210660073.lua
@@ -86,19 +86,19 @@ function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.srop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.srfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
 --You can Special Summon this card from your hand if you control 4 FIRE monsters.
 function s.spfilter(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_FIRE)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_FIRE)
 end
 function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=e:GetHandlerPlayer()
-	return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,4,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    if c==nil then return true end
+    local tp=e:GetHandlerPlayer()
+    return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,4,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end

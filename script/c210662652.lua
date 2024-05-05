9f7103ab1d8872bac264d35f608ba2d6709f923c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-05 19:20:23 +0300

    Zenry on BCETCG

---
+++ script/c210662652.lua
@@ -0,0 +1,44 @@
+-- Formicidaean Ariant
+--Scripted by Konstak.
+local s,id=GetID()
+function s.initial_effect(c)
+    --Revive
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCondition(s.sumcon)
+    e1:SetTarget(s.sumtg)
+    e1:SetOperation(s.sumop)
+    c:RegisterEffect(e1)
+    --Also treated as a Beast monster on the field
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_ADD_RACE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(RACE_BEAST)
+    c:RegisterEffect(e2)
+end
+function s.sumcon(e,tp,c)
+    if c==nil then return true end
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and ct<1 end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    if e:GetHandler():IsRelateToEffect(e) then
+        ct=ct+1
+        c:SetTurnCounter(ct)
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+    end
+end

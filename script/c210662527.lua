e29c9368e7dffe6978443b681d69d18fa616ee40
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-05 23:15:04 +0300

    Zollow on BCETCG

---
+++ script/c210662527.lua
@@ -0,0 +1,68 @@
+-- Zollow
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --once normal summoned, SS as many brollows as possible
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    --destroy both
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_START)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
+    --During end phase, SS as many brollows as possible
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.sptg)
+    e4:SetOperation(s.spop)
+    c:RegisterEffect(e4)
+end
+--Special summon
+function s.spfilter(c,e,tp)
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 and Duel.TossCoin(tp,1)==COIN_HEADS then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+    end
+end
+--Destroy both Function
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local bc=e:GetHandler():GetBattleTarget()
+    if bc:IsRelateToBattle() and Duel.TossCoin(tp,1)==COIN_HEADS then
+        Duel.Destroy(bc,REASON_EFFECT)
+        Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+    end
+end

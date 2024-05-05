f03b53cd7d2bd86392f0c183865f0054f874f9e7
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-05 18:08:54 +0300

    Zuche on BCETCG

---
+++ script/c210662468.lua
@@ -0,0 +1,70 @@
+-- Zuche
+--Scripted by Konstak.
+local s,id=GetID()
+function s.initial_effect(c)
+    --Place as Spell
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,2))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.spelltarget)
+    e1:SetOperation(s.spellop)
+    c:RegisterEffect(e1)
+    --Then SS to opp
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,3))
+    e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e2:SetCondition(s.destcondition)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
+end
+--e1
+function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.spellop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
+end
+--e2
+function s.destcondition(e,tp,eg,ep,ev,re,r,rp)
+    return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
+end
+function s.desfilter(c,atk)
+    return c:IsFaceup()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local c=e:GetHandler()
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
+    if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,c:GetAttack())
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) then
+            Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+        end
+    end
+end

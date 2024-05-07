c611922013eb1365c217de6f5977aec59de89127
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 23:21:57 +0300

    SnS fully and script Alpacky

--- script/c210662209.lua
+++ script/c210662209.lua
@@ -12,6 +12,15 @@ function s.initial_effect(c)
     e0:SetTarget(s.sptg)
     e0:SetOperation(s.spop)
     c:RegisterEffect(e0)
+    --Base Destroyer Ability
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,1))
+    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_CONFIRM)
+    e1:SetCondition(function() return Duel.GetAttackTarget()==nil end)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
     --Strengthen
     local e2=Effect.CreateEffect(c)
     e2:SetCode(EFFECT_DESTROY_REPLACE)
@@ -44,27 +53,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Release(g,REASON_COST)
 	g:DeleteGroup()
 end
---Strong function
-function s.strongtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
-end
-function s.strongop(e,tp,eg,ep,ev,re,r,rp)
+--Base Destroyer Function
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local bc=e:GetHandler():GetBattleTarget()
-    if c:IsRelateToBattle() then
+    if not c:IsRelateToEffect(e) then return end
+    if c:IsFaceup() then
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(-bc:GetAttack()/2)
+        e1:SetValue(c:GetAttack())
         e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-        bc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_UPDATE_DEFENSE)
-        e2:SetValue(-bc:GetDefense()/2)
-        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-        bc:RegisterEffect(e2)
+        c:RegisterEffect(e1)
     end
 end
 --Strengthen function

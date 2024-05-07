f0713d5f6903c6002a7396224dbae8f64184e889
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-08 01:49:31 +0300

    SnS fully and script Metal Cyclone

--- script/c210662071.lua
+++ script/c210662071.lua
@@ -25,16 +25,32 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_ATTACK_ALL)
     e2:SetValue(1)
     c:RegisterEffect(e2)
+    --Metal Mechanic
+    local e3=Effect.CreateEffect(c)
+    e3:SetCode(EFFECT_DESTROY_REPLACE)
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTarget(s.desatktg)
+    c:RegisterEffect(e3)
+    --self destroy
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EFFECT_SELF_DESTROY)
+    e4:SetCondition(s.sdcon)
+    c:RegisterEffect(e4)
 end
-function s.alienfilter(c)
+function s.filter(c)
     return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsFaceup()
 end
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.filter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.alienfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    local g=Duel.SelectReleaseGroup(tp,s.filter,2,2,false,true,true,c,nil,nil,false,nil,nil)
     if g then
         g:KeepAlive()
         e:SetLabelObject(g)
@@ -47,4 +63,20 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     if not g then return end
     Duel.Release(g,REASON_COST)
     g:DeleteGroup()
+end
+--Metal Ability Function
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsFaceup() end
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_DEFENSE)
+        e1:SetValue(-50)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+        c:RegisterEffect(e1)
+    return true
+end
+function s.sdcon(e)
+    local c=e:GetHandler()
+    return c:GetDefense()<=0
 end

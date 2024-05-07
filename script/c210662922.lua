1af47d3dc26feada75b989add73537670fc3054c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 20:28:09 +0300

    Church Base on BCETCG

---
+++ script/c210662922.lua
@@ -0,0 +1,10 @@
+-- Church Base
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+end

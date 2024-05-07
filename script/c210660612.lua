dceb7933a138367899cbf76b8535d9f0e6485690
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-05-06 19:13:56 -0700

    Princess Cat Rough Draft

---
+++ script/c210660612.lua
@@ -0,0 +1,138 @@
+--Princess Cat
+--Scripted by Gideon
+-- (1) You can Special Summon this card by discarding 1 monster from your hand that is level 7 or higher.
+-- (2) While this card is face-up on the field; Your opponent cannot attack with level 6 or lower monsters. This effect becomes negated if your opponent controls 5 or more monsters.
+-- (3) During your opponent's turn, when a Spell Card is activated: You can negate the activation, and if you do, destroy it, then, draw 1 card. You can only use this effect of "Princess Cat" once per turn.
+-- (4) If you activate a Trap Card (except during the Damage Step): You can target 1 monster in your GY; Special Summon that target. You can only use this effect of "Princess Cat" once per turn.
+-- (5) If you control 5 or more monsters: This card become uneffected by other monster effects.
+-- (6) If this card would leave the field; Banish it 
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Level 6 monster's cannot attack.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.atktarget)
+	c:RegisterEffect(e2)
+	--Opp turn spell negate draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.scondition)
+	e3:SetTarget(s.starget)
+	e3:SetOperation(s.soperation)
+	e3:SetCountLimit(1,id)
+	c:RegisterEffect(e3)
+	--trap resolve recovery
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetOperation(aux.chainreg)
+	c:RegisterEffect(e4)
+	--special summon procedure
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetRange(LOCATION_FIELD)
+	e5:SetCode(EVENT_CHAIN_SOLVED)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCondition(s.trapreborncon)
+	e5:SetTarget(s.trapreborntarget)
+	e5:SetOperation(s.traprebornopp)
+	e5:SetCountLimit(1,{id,1})
+	c:RegisterEffect(e5)
+	--If you control 5 or more monsters: This card become uneffected by other monster effects.
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_IMMUNE_EFFECT)
+	e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetValue(s.immval)
+	c:RegisterEffect(e6)
+	--Banish on remove field
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e7:SetValue(LOCATION_REMOVED)
+	c:RegisterEffect(e7)
+end
+--E1
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0 and
+		Duel.IsExistingMatchingCard(Card.IsLevelAbove,c:GetControler(),LOCATION_HAND,0,1,c,7)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,Card.IsLevelAbove,tp,LOCATION_HAND,0,1,1,c,7)
+	Duel.SendtoGrave(g,REASON_DISCARD+REASON_COST)
+end
+--e2
+function s.atktarget(e,c)
+	return c:GetLevel()<=6 and not Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>=5
+end
+--e3
+function s.scondition(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep~=tp
+		and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and Duel.IsChainNegatable(ev)
+end
+function s.starget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.soperation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end
+--e5
+function s.sgfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.trapreborncon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP)
+		and rp==tp 
+end
+function s.trapreborntarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.sgfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.sgfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.sgfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.traprebornopp(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--e6
+function s.immval(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,MONSTER),tp,LOCATION_MZONE,0,5,nil)
+end

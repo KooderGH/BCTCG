9ccda29262ae2b2783eea28fd6910f9b94b49bf1
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 20:32:28 +0300

    SnS Fully and script Church Base

--- script/c210662922.lua
+++ script/c210662922.lua
@@ -7,4 +7,16 @@ function s.initial_effect(c)
     e0:SetType(EFFECT_TYPE_ACTIVATE)
     e0:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e0)
+    --boost
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetValue(s.atkval)
+    c:RegisterEffect(e1)
+end
+function s.atkval(e,c)
+    local g=Duel.GetMatchingGroup(Card.IsMonster,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil)
+    return g:GetClassCount(Card.GetAttribute)*50
 end

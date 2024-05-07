918ad5b648941ce58cbd22aa7308710f1781f18e
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 23:35:24 +0300

    SnS fully and script Cosmic Cyclone

--- script/c210662179.lua
+++ script/c210662179.lua
@@ -25,6 +25,15 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_ATTACK_ALL)
     e2:SetValue(1)
     c:RegisterEffect(e2)
+    --Strengthen
+    local e3=Effect.CreateEffect(c)
+    e3:SetCode(EFFECT_DESTROY_REPLACE)
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.strengthentg)
+    c:RegisterEffect(e3)
 end
 function s.alienfilter(c)
     return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
@@ -47,4 +56,16 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     if not g then return end
     Duel.Release(g,REASON_COST)
     g:DeleteGroup()
+end
+--Strengthen function
+function s.strengthentg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return not c:IsReason(REASON_REPLACE) end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+    c:RegisterEffect(e1)
+    return true
 end

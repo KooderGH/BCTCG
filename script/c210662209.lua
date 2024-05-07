986a69f140a7e901f04fe50030c8c153fe61ee74
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 23:19:49 +0300

    Alpacky on BCETCG

---
+++ script/c210662209.lua
@@ -0,0 +1,81 @@
+-- Alpacky
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+    --Strengthen
+    local e2=Effect.CreateEffect(c)
+    e2:SetCode(EFFECT_DESTROY_REPLACE)
+    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.strengthentg)
+    c:RegisterEffect(e2)
+end
+function s.alienfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+--Strong function
+function s.strongtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
+end
+function s.strongop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-bc:GetAttack()/2)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-bc:GetDefense()/2)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e2)
+    end
+end
+--Strengthen function
+function s.strengthentg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return not c:IsReason(REASON_REPLACE) end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+    c:RegisterEffect(e1)
+    return true
+end

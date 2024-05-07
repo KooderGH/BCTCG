d81d6a39e4329cdb78b460d098c580d646b9e6bb
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 22:39:38 +0300

    SnS fully and script Celeboodle

--- script/c210662184.lua
+++ script/c210662184.lua
@@ -1,53 +1,40 @@
 -- Mistress Celeboodle
 local s,id=GetID()
 function s.initial_effect(c)
-    --during damage calculation FIRE lose half of their ATK/DEF (1)
+    --Strong Against
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_REMOVE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_BATTLE_START)
-    e1:SetTarget(s.atktg)
-    e1:SetOperation(s.atkop)
+    e1:SetTarget(s.strongtg)
+    e1:SetOperation(s.strongop)
     c:RegisterEffect(e1)
-    --Once while this card is face-up on the field: If it would be destroyed; gain 500 ATK instead.
+    --Strengthen
     local e2=Effect.CreateEffect(c)
     e2:SetCode(EFFECT_DESTROY_REPLACE)
     e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_NO_TURN_RESET)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1)
-    e2:SetTarget(s.desreptg)
+    e2:SetTarget(s.strengthentg)
     c:RegisterEffect(e2)
+    --draw
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_DRAW)	
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetOperation(s.drop)
+    c:RegisterEffect(e3)
 end
-function s.alienfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
-end
-function s.spcon(e,c)
-	if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
-	if g then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-	return true
-	end
-	return false
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
-end
---during damage calculation function
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+--Strong function
+function s.strongtg(e,tp,eg,ep,ev,re,r,rp,chk)
     local bc=e:GetHandler():GetBattleTarget()
     if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
 end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.strongop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local bc=e:GetHandler():GetBattleTarget()
     if c:IsRelateToBattle() then
@@ -65,8 +52,8 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
         bc:RegisterEffect(e2)
     end
 end
---Replace destroy function
-function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+--Strengthen function
+function s.strengthentg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return not c:IsReason(REASON_REPLACE) end
     local e1=Effect.CreateEffect(c)
@@ -76,4 +63,21 @@ function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
     e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
     c:RegisterEffect(e1)
     return true
+end
+--Draw Function
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.IsPlayerCanDraw(1-tp,1) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+        e1:SetRange(LOCATION_GRAVE)
+        e1:SetCode(EVENT_PHASE+PHASE_DRAW)
+        e1:SetReset(RESET_PHASE+PHASE_END,2)
+        e1:SetCountLimit(1)
+        e1:SetOperation(s.drawop)
+        c:RegisterEffect(e1)
+    end
+end
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(1-tp,1,REASON_EFFECT)
 end

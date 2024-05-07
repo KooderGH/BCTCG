c9d6a5fb621972881203bfe9ab832c12cd913f96
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-07 20:01:18 +0300

    fix Dogenstein

--- script/c210662679.lua
+++ script/c210662679.lua
@@ -2,6 +2,11 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
+    --activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
     --Reveal
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))

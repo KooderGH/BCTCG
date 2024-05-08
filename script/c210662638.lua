ec1e2822e92e5afd44134c7a86fab6b09065a329
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-05-08 03:17:54 +0300

    Mini White Cyclone scripted

--- script/c210662638.lua
+++ script/c210662638.lua
@@ -1,11 +1,17 @@
--- Mini Angel Cyclone
+-- Mini White Cyclone
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Attack all each time
+    --Opponent No Battle Damage
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_ATTACK_ALL)
+    e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
     e1:SetValue(1)
     c:RegisterEffect(e1)
+    --Attack all each time
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_ATTACK_ALL)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 end

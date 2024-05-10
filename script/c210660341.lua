cc86088b1d027c2ae86e467b2ffe429872a82cd5
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-05-10 01:04:06 -0700

    Finalize and script Lost World Yuki

---
+++ script/c210660341.lua
@@ -0,0 +1,178 @@
+--Lost World Yuki
+--Scripted by Gideon
+-- (1) The Maximum number of cards that your opponent can have on the field becomes 4.
+-- (2) During your opponent's end phase: You can discard this card from your hand; Add 1 Psychic monster from your deck to your hand.
+-- (3) When this card is sent to the GY; You can add up to 2 cards from your banished zone to your hand.
+-- (4) During your standby phase: You can Special Summon this card from your hand.
+local s,id=GetID()
+function s.initial_effect(c)
+	--(1) Start (it's a pain)
+	--adjust
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.adjustop)
+	c:RegisterEffect(e2)
+	--And the rest T_T
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_MAX_MZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(0,1)
+	e3:SetValue(s.mvalue)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_MAX_SZONE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(0,1)
+	e4:SetValue(s.svalue)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetTargetRange(0,1)
+	e5:SetValue(s.aclimit)
+	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EFFECT_CANNOT_SSET)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetTargetRange(0,1)
+	e6:SetTarget(s.setlimit)
+	c:RegisterEffect(e6)
+	--(1) end
+	--(2) Start
+	--Search
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,0))
+	e7:SetCategory(CATEGORY_TOHAND)
+	e7:SetType(EFFECT_TYPE_QUICK_O)
+	e7:SetCode(EVENT_FREE_CHAIN)
+	e7:SetRange(LOCATION_HAND)
+	e7:SetCondition(function(_,tp) return Duel.IsTurnPlayer(1-tp) end)
+	e7:SetCost(s.thcost)
+	e7:SetTarget(s.thtg)
+	e7:SetOperation(s.thop)
+	--(2) end
+	--(3) Start
+	--Recovery
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,1))
+	e8:SetCategory(CATEGORY_TOHAND)
+	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e8:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e8:SetCode(EVENT_TO_GRAVE)
+	e8:SetTarget(s.gthtg)
+	e8:SetOperation(s.gthop)
+	c:RegisterEffect(e8)
+	--(3) end
+	--(4) Start
+	--special summon
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,2))
+	e9:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e9:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e9:SetRange(LOCATION_HAND)
+	e9:SetCountLimit(1)
+	e9:SetTarget(s.sptg)
+	e9:SetOperation(s.spop)
+	c:RegisterEffect(e9)
+	--(4) end
+end
+--All (1) effect
+function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
+	local phase=Duel.GetCurrentPhase()
+	if (phase==PHASE_DAMAGE and not Duel.IsDamageCalculated()) or phase==PHASE_DAMAGE_CAL then return end
+	local c1=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+	local c2=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	if c1>4 or c2>4 then
+		local g=Group.CreateGroup()
+		if c1>4 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local g1=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,0,c1-4,c1-4,nil)
+			g:Merge(g1)
+		end
+		if c2>4 then
+			Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_TOGRAVE)
+			local g2=Duel.SelectMatchingCard(1-tp,nil,1-tp,LOCATION_ONFIELD,0,c2-4,c2-4,nil)
+			g:Merge(g2)
+		end
+		Duel.SendtoGrave(g,REASON_EFFECT)
+		Duel.Readjust()
+	end
+end
+function s.mvalue(e,fp,rp,r)
+	return 4-Duel.GetFieldGroupCount(fp,LOCATION_SZONE,0)
+end
+function s.svalue(e,fp,rp,r)
+	return 4-Duel.GetFieldGroupCount(fp,LOCATION_MZONE|LOCATION_FZONE,0)
+end
+function s.aclimit(e,re,tp)
+	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	if re:IsActiveType(TYPE_FIELD) then
+		return not Duel.GetFieldCard(tp,LOCATION_FZONE,0) and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>3
+	elseif re:IsActiveType(TYPE_PENDULUM) then
+		return Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>3
+	end
+	return false
+end
+function s.setlimit(e,c,tp)
+	return c:IsFieldSpell() and not Duel.GetFieldCard(tp,LOCATION_FZONE,0) and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>3
+end
+--e7
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_PSYCHIC) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.GetFirstMatchingCard(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if tc then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+--e8
+function s.filter(c)
+	return c:IsAbleToHand()
+end
+function s.gthtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetControler()==tp and chkc:IsLocation(LOCATION_REMOVED) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_REMOVED,0,1,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+end
+function s.gthop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	Duel.SendtoHand(sg,nil,REASON_EFFECT)
+end
+--e9
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

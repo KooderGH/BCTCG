bbb8cb96d3efae2ec2cea481155e286ab66cec4a
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-06-02 16:49:58 +0300

    Crustaceous Scissorex on BCETCG

---
+++ script/c210662606.lua
@@ -0,0 +1,116 @@
+-- Crustaceous Scissorex
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+    --Strong Against
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetTarget(s.strongtg)
+    e1:SetOperation(s.strongop)
+    c:RegisterEffect(e1)
+    --Can make a second attack
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+    --Opponent No Battle Damage
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+    --Slow Ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_BATTLE_START)
+    e4:SetCondition(s.slowcon)
+    e4:SetOperation(s.slowop)
+    c:RegisterEffect(e4)
+end
+function s.alienfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+--Strong function
+function s.strongtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttribute(ATTRIBUTE_FIRE)) end
+end
+function s.strongop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    if c:IsRelateToBattle() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-bc:GetAttack()/2)
+        e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        e2:SetValue(-bc:GetDefense()/2)
+        e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+        bc:RegisterEffect(e2)
+    end
+end
+--Slow Ability Function
+function s.slowcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle()
+end
+function s.slowop(e,tp,eg,ep,ev,re,r,rp)
+    local effp=e:GetHandler():GetControler()
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_CANNOT_BP)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(0,1)
+        if Duel.GetTurnPlayer()==effp then
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetCondition(s.skipcon)
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+        else
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+        end
+        Duel.RegisterEffect(e1,effp)
+        Duel.NegateAttack()
+    end
+end
+function s.skipcon(e)
+    return Duel.GetTurnCount()~=e:GetLabel()
+end

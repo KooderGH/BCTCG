672a6a0cc7cbf84ec27c1a6bd1d938e6841ed9e7
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-06-19 09:12:07 -0700

    Coppermine Finished

--- script/c210660107.lua
+++ script/c210660107.lua
@@ -2,11 +2,160 @@
 --Scripted By Gideon
 -- (1) When this card is Summoned; Add 2 Level 8 or lower LIGHT Spellcaster from your deck to your hand, then, raise it's level by 2.
 -- (2) During your Main Phase, you can Normal Summon 1 Spellcaster monster in addition to your Normal Summon/Set. (You can only gain this effect once per turn.)
--- (3) If this card is targeted for an attack: You can target 1 Spellcaster-Type monster in your GY except "Coppermine"; Special Summon it, then change the attack target ot it, and if you do, the attacking monster's ATK becomes half its current ATK for this battle calulation only.
+-- (3) If this card is targeted for an attack: You can target 1 Spellcaster-Type monster in your GY except "Coppermine"; Special Summon it, then change the attack target ot it, and if you do, the attacking monster's ATK becomes half its current ATK.
 -- (4) When this card is Tributed; Target 1 LIGHT monster you control; It gains ATK equal to the difference between LPs.
 -- (5) When this card is sent to the GY; Special Summon 1 LIGHT Spellcaster from your hand.
 -- (6) You can only use each effect of "Coppermine" once per turn and used only once while it is face-up on the field.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --(1)
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:Registerffect(e3)
+    --(2)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e4:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+    e4:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+    e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_SPELLCASTER))
+	c:RegisterEffect(e4)
+    --(3)
+    --change battle target
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
+	e5:SetCode(EVENT_BE_BATTLE_TARGET)
+	e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1,{id,2})
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+    --(4)
+    local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCategory(CATEGORY_ATKCHANGE)
+	e6:SetCode(EVENT_RELEASE)
+	e6:SetCountLimit(1,{id,3})
+	e6:SetTarget(s.reltg)
+	e6:SetOperation(s.relop)
+	e6:SetLabel(1)
+	c:RegisterEffect(e6)
+    --(5)
+    local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,4))
+	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e7:SetCode(EVENT_TO_GRAVE)
+	e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e7:SetCountLimit(1,{id,4})
+	e7:SetTarget(s.ssptg)
+	e7:SetOperation(s.sspop)
+	c:RegisterEffect(e7)
+end
+--e1
+function s.filter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_SPELLCASTER) and c:IsLevelBelow(8) and c:IsAbleToHand()
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,2,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(2)
+		tc:RegisterEffect(e1)
+	end
+end
+--e5
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local a=Duel.GetAttacker()
+        if a:IsRelateToEffect(e) and a:IsFaceup() then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_SET_BASE_ATTACK)
+            e1:SetValue(a:GetBaseAttack()/2)
+            a:RegisterEffect(e1)
+        end
+		if a:CanAttack() and not a:IsImmuneToEffect(e) then
+			Duel.CalculateDamage(a,tc)
+		end
+	end
+end
+--e6
+function s.spellfilter(c)
+	return c:IsRace(RACE_SPELLCASTER)
+end
+function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(s.spellfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.spellfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.relop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        local atk=math.abs(Duel.GetLP(tp)-Duel.GetLP(1-tp))
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+        e1:SetValue(atk)
+        c:RegisterEffect(e1)
+	end
+end
+--e7
+function s.sgfilter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.sgfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.sgfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end

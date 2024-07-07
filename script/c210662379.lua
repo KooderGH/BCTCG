f28a0448f751f9e2cd3b30a5d87e46d5c7accd69
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-07-07 23:05:03 +0300

    nerf winged pigge and give her 200 more DEF

--- script/c210662379.lua
+++ script/c210662379.lua
@@ -21,15 +21,6 @@ function s.initial_effect(c)
     e2:SetCondition(s.weakencon)
     e2:SetOperation(s.weakenop)
     c:RegisterEffect(e2)
-    --Can banish zombie monsters
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetCategory(CATEGORY_REMOVE)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_BATTLE_START)
-    e3:SetTarget(s.bntg)
-    e3:SetOperation(s.bnop)
-    c:RegisterEffect(e3)
 end
 function s.angelfilter(c)
     return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
@@ -75,16 +66,4 @@ function s.weakenop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkval(e,c)
 	return -c:GetBaseAttack()/4
-end
---Banish Zombies function
-function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
-end
-function s.bnop(e,tp,eg,ep,ev,re,r,rp)
-    local bc=e:GetHandler():GetBattleTarget()
-    if bc:IsRelateToBattle() then
-    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
-    end
 end

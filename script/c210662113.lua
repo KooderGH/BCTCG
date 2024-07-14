62355b6f0691c0ba0d18df446697a830d3538a02
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-07-14 20:25:43 +0300

    fix Draw message on Hippoe.Gabriel,Gory

--- script/c210662113.lua
+++ script/c210662113.lua
@@ -13,6 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
     --draw
     local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_DRAW)	
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_TO_GRAVE)

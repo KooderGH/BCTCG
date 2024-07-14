5fe8f22fb551ab91b35d9c3219334c7413adb137
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-07-15 00:40:17 +0300

    Buff Generic Strong against

--- script/c210662005.lua
+++ script/c210662005.lua
@@ -23,7 +23,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(400)
+        e1:SetValue(c:GetAttack())
         e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
         c:RegisterEffect(e1)
     end

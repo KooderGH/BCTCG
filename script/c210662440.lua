ee5d1be8524ff521d012a7a5dc8d35b70cd03ca1
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-01 00:07:58 +0300

    Introduce variety bears for future SnS

---
+++ script/c210662440.lua
@@ -0,0 +1,11 @@
+-- Variety Bears
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0,TIMING_END_PHASE)
+    c:RegisterEffect(e1)
+end

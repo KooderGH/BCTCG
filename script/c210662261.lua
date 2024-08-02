60a6d2aaa1248187f2e0cd1e6aa9a9f0d3d9073f
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-02 06:57:59 +0300

    rework Boraphim

--- script/c210662261.lua
+++ script/c210662261.lua
@@ -22,16 +22,6 @@ function s.initial_effect(c)
     e2:SetTarget(s.bntg)
     e2:SetOperation(s.bnop)
     c:RegisterEffect(e2)
-    --Knockback ability
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetCategory(CATEGORY_DISABLE)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e3:SetCondition(s.knockbackcon)
-    e3:SetTarget(s.knockbacktg)
-    e3:SetOperation(s.knockbackop)
-    c:RegisterEffect(e3)
 end
 function s.angelfilter(c)
     return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
@@ -65,28 +55,5 @@ function s.bnop(e,tp,eg,ep,ev,re,r,rp)
     local bc=e:GetHandler():GetBattleTarget()
     if bc:IsRelateToBattle() then
         Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
-    end
-end
---Knockback function
-function s.knockbackcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()==tp
-end
-function s.knockbacktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() end
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
-end
-function s.knockbackop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local tc=e:GetHandler():GetBattleTarget()
-    if tc:IsRelateToBattle() and tc and tc:IsFaceup() and Duel.TossCoin(tp,1)==COIN_HEADS and not tc:IsImmuneToEffect(e) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetDescription(3206)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CANNOT_ATTACK)
-        e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-        tc:RegisterEffect(e1)
-        Duel.NegateAttack()
     end
 end

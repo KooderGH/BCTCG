dbc19eb0acc1e42b70e47fc3984e26493b04c562
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-02 06:17:57 +0300

    rework Dead Donny Dash

--- script/c210662425.lua
+++ script/c210662425.lua
@@ -2,16 +2,81 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Draw lv4 Monster
+    --Burrow down
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e1:SetCode(EVENT_DESTROYED)
-    e1:SetTarget(s.drtg)
-    e1:SetOperation(s.drop)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    e1:SetTarget(s.burrowdowntg)
+    e1:SetOperation(s.burrowdownop)
     c:RegisterEffect(e1)
+    --Burrow up
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e2:SetCondition(s.burrowupcon)
+    e2:SetTarget(s.burrowuptg)
+    e2:SetOperation(s.burrowupop)
+    c:RegisterEffect(e2)
+    --Draw lv4 Monster
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetTarget(s.drtg)
+    e3:SetOperation(s.drop)
+    c:RegisterEffect(e3)
+end
+--burrow down function
+function s.burrowdowntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.burrowdownop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
+end
+--burrow up function
+function s.burrowupcon(e,tp,eg,ep,ev,re,r,rp)
+    return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
+end
+function s.burrowupfilter(c,atk)
+    return c:IsFaceup()
+end
+function s.burrowuptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local c=e:GetHandler()
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+    if chk==0 then return Duel.IsExistingTarget(s.burrowupfilter,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectTarget(tp,s.burrowupfilter,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+end
+function s.burrowupop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) then
+            Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+        end
+    end
 end
 --Destroy and draw function
 function s.drfilter(c)

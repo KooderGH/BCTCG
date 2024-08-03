b1ae7520eed7522f44b069e94e35722c780463be
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-08-02 18:15:00 -0700

    Megidora Scripted (May need tests)

--- script/c210660084.lua
+++ script/c210660084.lua
@@ -1,5 +1,5 @@
 --Megidora
---Scripted by Konstak
+--Scripted by Gideon, 6th effect by Konstak
 --Effect
 --(1) Any monster that is Normal Summoned, Flip Summoned, or Special Summoned is changed to Defense Position except Dragon type monsters.
 --(2) Once per turn (Igntion): You can target one monster on your side of the field except "Megidora" and one monster on your opponent side; Destroy both targets.
@@ -9,10 +9,150 @@
 --(6) Can be treated as 2 Tributes for the Tribute Summon of a Dragon monster.
 local s,id=GetID()
 function s.initial_effect(c)
+    --Stumbling but dragons fly so
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTarget(s.dtarget)
+	e1:SetOperation(s.doperation)
+	c:RegisterEffect(e2)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+    --Target 2 cards, destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCountLimit(1)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+    --Kill yourself the Effect
+    local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EFFECT_SELF_DESTROY)
+	e5:SetCondition(s.descon)
+	c:RegisterEffect(e5)
+    --destroyed by a card effect, target 1 Dragon monster in your GY except "Megidora"; Special Summon it
+    local e1=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,2))
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetCondition(s.spcon)
+	e6:SetTarget(s.sptg)
+	e6:SetOperation(s.spop)
+	c:RegisterEffect(e6)
+    --Destroyed by battle, You can target 1 monster you control; It gains 500 ATK.
+    local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,3))
+	e7:SetCategory(CATEGORY_ATKCHANGE)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e7:SetCode(EVENT_BATTLE_DESTROYED)
+	e7:SetCondition(s.atkcon)
+	e7:SetTarget(s.atktg)
+	e7:SetOperation(s.atkop)
+	c:RegisterEffect(e7)
     --Can be treated as 2 Tributes for the Tribute Summon of a Dragon monster.(6)
-    local e6=Effect.CreateEffect(c)
-    e6:SetType(EFFECT_TYPE_SINGLE)
-    e6:SetCode(EFFECT_DOUBLE_TRIBUTE)
-    e6:SetValue(function (e,c) return c:IsRace(RACE_DRAGON) end)
-    c:RegisterEffect(e6)
+    local e8=Effect.CreateEffect(c)
+    e8:SetType(EFFECT_TYPE_SINGLE)
+    e8:SetCode(EFFECT_DOUBLE_TRIBUTE)
+    e8:SetValue(function (e,c) return c:IsRace(RACE_DRAGON) end)
+    c:RegisterEffect(e8)
+end
+--e1 (1)
+function s.dtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,eg,#eg,0,0)
+end
+function s.filter(c,e)
+	return c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) and not c:IsRace(RACE_DRAGON)
+end
+function ds.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=eg:Filter(s.filter,nil,e)
+	Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+end
+--e4 (2)
+function s.dfilter(c,e)
+    return c:IsMonster() and not c:IsCode(id)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,0,1,nil())
+		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,0,1,1,nil())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
+	end
+end
+--e5 (3)
+function s.descon(e)
+	return e:GetHandler():IsDefensePos()
+end
+--e6 (4)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT) and c:IsReason(REASON_DESTROY)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:GetCode()~=id and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--e7 (5)
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(500)
+		tc:RegisterEffect(e1)
+	end
 end

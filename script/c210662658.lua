5caf5a82c79bc71ca57313b126077e910a57805c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-05 21:26:03 +0300

    rework Mini angel cyclone

--- script/c210662658.lua
+++ script/c210662658.lua
@@ -1,15 +1,13 @@
 -- Mini Angel Cyclone
 local s,id=GetID()
 function s.initial_effect(c)
-    --Wave on Battle
+    --Surge Attack on Battle
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,3))
-    e1:SetCategory(CATEGORY_DISABLE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCondition(s.wavecon)
-    e1:SetTarget(s.wavetg)
-    e1:SetOperation(s.waveop)
+    e1:SetCondition(s.surgecon)
+    e1:SetTarget(s.surgetg)
+    e1:SetOperation(s.surgeop)
     c:RegisterEffect(e1)
     --Attack all each time
     local e2=Effect.CreateEffect(c)
@@ -18,23 +16,51 @@ function s.initial_effect(c)
     e2:SetValue(1)
     c:RegisterEffect(e2)
 end
---Wave on Battle Function
-function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+--Surge on Battle Function
+function s.surgecon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.GetTurnPlayer()==tp
 end
-function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
-function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
     if not e:GetHandler():IsRelateToEffect(e) then return end
+    Duel.NegateAttack()
     local d1=6
     while d1>3 do
         d1=Duel.TossDice(tp,1)
     end
     local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
-    Duel.NegateAttack()
+    local Zone=d1
     if tc then
+        local seq=tc:GetSequence()
+        local nseq=seq
         Duel.Destroy(tc,REASON_EFFECT)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE_FIELD)
+        e1:SetRange(LOCATION_SZONE)
+        e1:SetLabel(nseq+16)
+        e1:SetOperation(s.disop)
+        e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+        Duel.RegisterEffect(e1,tp)
+    else
+        local seq=Zone
+        local nseq=seq
+        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD)
+            e1:SetCode(EFFECT_DISABLE_FIELD)
+            e1:SetRange(LOCATION_SZONE)
+            e1:SetLabel(nseq+16)
+            e1:SetOperation(s.disop)
+            e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
+            Duel.RegisterEffect(e1,tp)
+        end
     end
+end
+function s.disop(e,tp)
+    return 0x1<<e:GetLabel()
 end

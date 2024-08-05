4e472658c96640429d57f4cb0fca8163872254a8
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-05 21:29:24 +0300

    rework Sleipnir and Lutrinae

--- script/c210662649.lua
+++ script/c210662649.lua
@@ -2,23 +2,22 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableUnsummonable()
-	-- Special Summon this card
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.spcon)
-	c:RegisterEffect(e1)
-    --Can banish zombie monsters
+    c:EnableUnsummonable()
+    -- Special Summon this card
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    c:RegisterEffect(e1)
+    --Immune to Wave, Surge, Single Attack
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_BATTLE_START)
-    e2:SetTarget(s.bntg)
-    e2:SetOperation(s.bnop)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2:SetValue(1)
     c:RegisterEffect(e2)
     --Knockback ability
     local e3=Effect.CreateEffect(c)
@@ -36,18 +35,6 @@ function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_LIGHT),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-end
---Banish Zombies function
-function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_ZOMBIE) end
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
-end
-function s.bnop(e,tp,eg,ep,ev,re,r,rp)
-    local bc=e:GetHandler():GetBattleTarget()
-    if bc:IsRelateToBattle() then
-    Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
-    end
 end
 --Knockback function
 function s.knockbacktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)

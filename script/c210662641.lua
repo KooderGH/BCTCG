433ebe64a4893b457795355d5e1359fb026cd9c8
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-05 07:34:26 +0300

    Elephantidae Papaou buff

--- script/c210662641.lua
+++ script/c210662641.lua
@@ -33,10 +33,10 @@ function s.papaoufilter(c)
 end
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.papaoufilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.papaoufilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.papaoufilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    local g=Duel.SelectReleaseGroup(tp,s.papaoufilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
     if g then
         g:KeepAlive()
         e:SetLabelObject(g)

1ab22e66f6b6e17d51f6a4d7c594dd64a5bbfce1
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-05 07:03:16 +0300

    buff Angel Gory

--- script/c210662115.lua
+++ script/c210662115.lua
@@ -11,14 +11,15 @@ function s.initial_effect(c)
     e1:SetRange(LOCATION_HAND)
     e1:SetCondition(s.spcon)
     c:RegisterEffect(e1)
-    --draw
+    --Add Monster
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_DRAW)	
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetOperation(s.drop)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetTarget(s.addtg)
+    e2:SetOperation(s.addop)
     c:RegisterEffect(e2)
     --Can banish zombie monsters
     local e3=Effect.CreateEffect(c)
@@ -35,21 +36,21 @@ function s.spcon(e,c)
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_LIGHT),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if Duel.IsPlayerCanDraw(1-tp,1) then
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_GRAVE)
-    e1:SetCode(EVENT_PHASE+PHASE_DRAW)
-	e1:SetReset(RESET_PHASE+PHASE_END,2)
-    e1:SetCountLimit(1)
-    e1:SetOperation(s.drawop)
-    c:RegisterEffect(e1)
-    end
+--Destroy and add function
+function s.addfilter(c)
+    return c:IsLevelAbove(6) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
 end
-function s.drawop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Draw(1-tp,1,REASON_EFFECT)
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
 --Banish Zombies function
 function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk)

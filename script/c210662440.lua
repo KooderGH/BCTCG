84f1a3734bb7515e1ab8ea9d26d1b7750d7bedc7
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-07 01:58:59 +0300

    rework variety bears

--- script/c210662440.lua
+++ script/c210662440.lua
@@ -5,36 +5,23 @@ function s.initial_effect(c)
     --Activate
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetHintTiming(0,TIMING_END_PHASE)
+    e1:SetCondition(s.condition)
     c:RegisterEffect(e1)
-    --Can target 1 S/T (Long Distance Attack)
+    --Increase ATK
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetCategory(CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e2:SetTarget(s.destg)
-    e2:SetOperation(s.desop)
+    e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+    e2:SetTarget(s.target)
+    e2:SetValue(250)
     c:RegisterEffect(e2)
 end
---Can target 1 S/T (Long Distance Attack) Function
-function s.filter(c)
-    return c:IsType(TYPE_SPELL+TYPE_TRAP)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and s.filter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_ONFIELD,1,e:GetHandler()) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,e:GetHandler())
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        Duel.Destroy(tc,REASON_EFFECT)
-    end
+function s.target(e,c)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,c:GetCode()),0,LOCATION_MZONE,LOCATION_MZONE,1,c)
 end

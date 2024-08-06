71d01b6837b91f6e442358a426f52fdebd05ed22
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-06 19:24:43 +0300

    fix certain Angel enemies

--- script/c210662119.lua
+++ script/c210662119.lua
@@ -31,7 +31,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 function s.angelfilter(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsFaceup()
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
 end
 function s.spcon(e,c)
 	if c==nil then return true end

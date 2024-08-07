90b9069a8500539d247dae52dd9984a5d2506bde
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-07 03:24:50 +0300

    rework Ms Sign

--- script/c210662021.lua
+++ script/c210662021.lua
@@ -4,43 +4,28 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Activate
     local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetHintTiming(0,TIMING_END_PHASE)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
-    --summon count limit
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetCode(EFFECT_CANNOT_SUMMON)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetTargetRange(1,1)
-    e2:SetTarget(s.limittg)
-    c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
-    c:RegisterEffect(e3)
-    local e4=e2:Clone()
-    e4:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    c:RegisterEffect(e4)
-    local e5=e2:Clone()
-    e5:SetCode(EFFECT_CANNOT_MSET)
-    c:RegisterEffect(e5)
-    --counter
-    local et=Effect.CreateEffect(c)
-    et:SetType(EFFECT_TYPE_FIELD)
-    et:SetCode(EFFECT_LEFT_SPSUMMON_COUNT)
-    et:SetRange(LOCATION_SZONE)
-    et:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    et:SetTargetRange(1,1)
-    et:SetValue(s.countval)
-    c:RegisterEffect(et)
 end
-function s.limittg(e,c,tp)
-	local t1,t2,t3=Duel.GetActivityCount(tp,ACTIVITY_SUMMON,ACTIVITY_FLIPSUMMON,ACTIVITY_SPSUMMON)
-	return t1+t2+t3>=1
+function s.filter(c)
+	return c:IsFaceup()
 end
-function s.countval(e,re,tp)
-	local t1,t2,t3=Duel.GetActivityCount(tp,ACTIVITY_SUMMON,ACTIVITY_FLIPSUMMON,ACTIVITY_SPSUMMON)
-	if t1+t2+t3>=1 then return 0 else return 1-t1-t2-t3 end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end

5a295087e09fd25db88a0005f770bf380f931951
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-10 04:15:34 +0300

    fix the O to F

--- script/c210662443.lua
+++ script/c210662443.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
     --Battle RNG Warp Mechanic
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(s.warcon)

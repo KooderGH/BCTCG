950c061be1cee7844e0ae4adaec079738cdcd662
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-08-13 20:05:18 -0700

    Merge local

--- script/c210660502.lua
+++ script/c210660502.lua
@@ -1,6 +1,99 @@
 --Myrcia
---Scripted By " "
+--Scripted By Gideon
+-- (1) When this card is Normal Summoned; you can Special Summon one Level 4 or lower Spellcaster from your hand.
+-- (2) When this card is Tributed: You can Target 1 card per 2 levels this card had on the field; Negate their effects.
+-- (3) When an Spellcaster is Tributed except "Myrcia"; You can draw 1 card.
+-- (4) When a monster your opponent control's is Tribute summoned: You can reduce it's level to 1; Add the difference to this card.
+-- (5) You can only use each above effect of "Myrcia" once per turn and used only once while it is face-up on the field.
+-- (6) If this card is in your GY: You can banish 3 Spell cards from your GY; Special Summon this card. You can only use this effect of "Myrcia" once per duel.
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+    --Tribute
+    local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCategory(CATEGORY_NEGATE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_RELEASE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.reltg)
+	e3:SetOperation(s.relop)
+	e3:SetLabel(1)
+	c:RegisterEffect(e3)
+end
+--e1
+function s.filter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--e2
+function s.opfilter(c)
+	return c:IsFaceup() and not c:IsDisabled()
+end
+function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+    local ct=e:GetHandler():GetPreviousLevelOnField()//2
+	if chk==0 then return Duel.IsExistingTarget(s.opfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g1=Duel.SelectTarget(tp,s.sefilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,nil,e:GetHandler())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g1,ct,0,0)
+end
+function s.refilter(c,e)
+	return ((c:IsFaceup() and not c:IsDisabled()) or c:IsType(TYPE_TRAPMONSTER)) and c:IsRelateToEffect(e)
+end
+function s.relop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg then return end
+	local tc=tg:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e3)
+		end
+		tc=tg:GetNext()
+	end
 end

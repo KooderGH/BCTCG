a94af97502e53639cb013d77211e91f229cd0414
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-08-19 23:51:02 -0700

    Minor edits

--- script/c210660619.lua
+++ script/c210660619.lua
@@ -36,7 +36,6 @@ function s.initial_effect(c)
 	e4:SetCategory(CATEGORY_DESTROY)
 	e4:SetCode(EVENT_RELEASE)
 	e4:SetCountLimit(1,{id,1})
-    e4.SetCondition(s.tcondition)
 	e4:SetTarget(s.reltg)
 	e4:SetOperation(s.relop)
 	c:RegisterEffect(e4)
@@ -87,16 +86,6 @@ function s.levelop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 --e4
-function s.tcondition(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_ATTACK)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
-	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
 function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)

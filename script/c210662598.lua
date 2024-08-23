900e4abbb86f49efb1a623f54a68478861722653
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-23 13:24:13 +0300

    Rework Firecracker

--- script/c210662598.lua
+++ script/c210662598.lua
@@ -10,57 +10,49 @@ function s.initial_effect(c)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCode(EFFECT_SELF_DESTROY)
     c:RegisterEffect(e1)
-    --Death Knockback
+    --Death Special Summon
     local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetTarget(s.weakentg)
-    e2:SetOperation(s.weakenop)
+    e2:SetTarget(s.sstg)
+    e2:SetOperation(s.ssop)
     c:RegisterEffect(e2)
 end
-function s.weakentg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+function s.specialfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_PYRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.weakenop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    local d1=6
-    while d1>3 do
-        d1=Duel.TossDice(tp,1)
-    end
-    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
-    if tc then
-        local e1=Effect.CreateEffect(c)
-        e1:SetDescription(aux.Stringid(id,0))
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CANNOT_ATTACK)
-        e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-        tc:RegisterEffect(e1)
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.specialfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.specialfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
     end
     --Return To Hand
-    local e2=Effect.CreateEffect(e:GetHandler())
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
-    e2:SetLabel(Duel.GetTurnCount())
-    e2:SetReset(RESET_PHASE+PHASE_MAIN1,4)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetLabelObject(c)
-    e2:SetCountLimit(1)
-    e2:SetOperation(s.returnop)
-    Duel.RegisterEffect(e2,tp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+    e1:SetLabel(Duel.GetTurnCount())
+    e1:SetReset(RESET_PHASE+PHASE_MAIN1,5)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetOperation(s.returnop)
+    Duel.RegisterEffect(e1,tp)
 end
 function s.returnop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local ct=c:GetTurnCounter()
     ct=ct+1
     c:SetTurnCounter(ct)
-    if ct==3 then
+    if ct==4 then
         ct=0
         c:SetTurnCounter(ct)
         Duel.Hint(HINT_CARD,0,id)
-        Duel.SendtoHand(e:GetLabelObject(),tp,REASON_EFFECT)
+        Duel.SendtoHand(e:GetHandler(),tp,REASON_EFFECT)
     end
 end

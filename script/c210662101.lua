64e1cfbe9d335e327bbaade6aab0135f7c631325
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-24 17:28:05 +0300

    Lord Enma rework

--- script/c210662101.lua
+++ script/c210662101.lua
@@ -13,6 +13,26 @@ function s.initial_effect(c)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+    --Attack Up Ability
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE,0)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetCondition(s.con)
+    e2:SetTarget(s.tg)
+    e2:SetValue(250)
+    c:RegisterEffect(e2)
+    --Damage LP
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,3))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetCountLimit(1)
+    e3:SetOperation(s.lpop)
+    c:RegisterEffect(e3)
 end
 function s.firefilter(c)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsFaceup()
@@ -35,4 +55,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	if not g then return end
 	Duel.Release(g,REASON_COST)
 	g:DeleteGroup()
+end
+--Attack Up function
+function s.con(e)
+	return e:GetHandler():IsAttackPos()
+end
+function s.tg(e,c)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_PYRO)
+end
+--Damage LP Function
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.Damage(1-tp,250,REASON_EFFECT)
+    end
 end

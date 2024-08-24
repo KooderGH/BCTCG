180b4b8336c17c73c679dd04649b059b23dd961c
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-08-23 18:40:19 -0700

    Terun Unfinished

--- script/c210660647.lua
+++ script/c210660647.lua
@@ -1,6 +1,81 @@
 --Huntress Terun
---Scripted By " "
+--Scripted By Gideon
+-- (1) You can Normal Summon/Set this card without Tribute.
+-- (2) When this card is Tribute Summoned; You can Special Summon the monsters that were used for this card Tribute and set their levels to 7.
+-- (3) If you control 4 or more Level 7 LIGHT Spellcaster monsters: You can target 1 card on your opponent side of the field, 1 card in their hand randomly, and 1 card in their GY; Banish those targets.
+-- (4) If this card is Tributed by a Spellcaster; Negate all current effect's on the field until the end phase.
+-- (5) You can only use each effect of "Huntress Terun" once per turn and used only once while it is face-up on the field.
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--summon & set with no tribute (1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCondition(s.ntcon)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_PROC)
+	c:RegisterEffect(e2)
+    --Tribute Summoned; You can Special Summon the monsters that were used for this card Tribute and set their levels to 7. (2)
+    local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetCondition(s.regcon)
+	e3:SetOperation(s.regop)
+	c:RegisterEffect(e3)
+end
+--E1
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+--e3
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_TRIBUTE)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.spfilter(c,e,tp,rc)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_SUMMON) and c:GetReasonCard()==rc and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=e:GetHandler():GetMaterial():Filter(s.spfilter,nil,e,tp,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local c=e:GetHandler()
+	local g=c:GetMaterial():Filter(s.spfilter,nil,e,tp,c)
+	if #g>0 then
+		if #g>ft then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			g=g:Select(tp,ft,ft,nil)
+		end
+		local tc=g:GetFirst()
+		for tc in aux.Next(g) do
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_CHANGE_LEVEL)
+            e1:SetValue(7)
+            g:RegisterEffect(e1)
+		end
+		Duel.SpecialSummonComplete()
+	end
 end

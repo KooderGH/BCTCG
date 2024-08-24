c225f1b112e00ccbeea22beb81a52a74d8376bf5
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-24 16:46:20 +0300

    rework Pigge victorious

--- script/c210662389.lua
+++ script/c210662389.lua
@@ -2,5 +2,46 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --When Normal Summoned (Search Ability)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.srtg)
+    e1:SetOperation(s.srop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    --Change battle damage (LP drain ability)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetTargetRange(0,1)
+    e3:SetCondition(s.con)
+    e3:SetValue(1000)
+    c:RegisterEffect(e3)
+end
+--When SP add function
+function s.ssfilter(c)
+    return c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_PYRO) and c:IsAbleToHand()
+end
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.ssfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.ssfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Change Battle Damage Ability
+function s.con(e)
+	return e:GetHandler():IsAttackPos()
 end

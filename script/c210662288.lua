7d40d019fd67badca3f25d06144ad9bf109d611c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-25 21:22:44 +0300

    rework Zackie Peng

--- script/c210662288.lua
+++ script/c210662288.lua
@@ -2,106 +2,78 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-    --Burrow down
+    --Attack Up Ability
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
-    e1:SetTarget(s.burrowdowntg)
-    e1:SetOperation(s.burrowdownop)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetValue(s.atkval)
     c:RegisterEffect(e1)
-    --Burrow up
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetCode(EVENT_PHASE+PHASE_END)
-    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e2:SetCondition(s.burrowupcon)
-    e2:SetTarget(s.burrowuptg)
-    e2:SetOperation(s.burrowupop)
-    c:RegisterEffect(e2)
     --Revive
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCost(s.sumcost)
+    e2:SetCondition(s.sumcon)
+    e2:SetTarget(s.sumtg)
+    e2:SetOperation(s.sumop)
+    c:RegisterEffect(e2)
+    --Add lv4 Monster
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e3:SetCode(EVENT_PHASE+PHASE_END)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCost(s.sumcost)
-    e3:SetCondition(s.sumcon)
-    e3:SetTarget(s.sumtg)
-    e3:SetOperation(s.sumop)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetTarget(s.drtg)
+    e3:SetOperation(s.drop)
     c:RegisterEffect(e3)
 end
---burrow down function
-function s.burrowdowntg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+--GY Attack Up function
+function s.gyfilter(c)
+    return c:IsRace(RACE_ZOMBIE)
 end
-function s.burrowdownop(e,tp,eg,ep,ev,re,r,rp)
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    local c=e:GetHandler()
-    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-    local e1=Effect.CreateEffect(c)
-    e1:SetCode(EFFECT_CHANGE_TYPE)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
-    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-    c:RegisterEffect(e1)
+function s.atkval(e,c)
+    local g=Duel.GetMatchingGroupCount(s.gyfilter,e:GetHandlerPlayer(),LOCATION_GRAVE,LOCATION_GRAVE,nil)
+    return g*150
 end
---burrow up function
-function s.burrowupcon(e,tp,eg,ep,ev,re,r,rp)
-    return tp==Duel.GetTurnPlayer() and e:GetHandler():IsContinuousSpell()
-end
-function s.burrowupfilter(c,atk)
-    return c:IsFaceup()
-end
-function s.burrowuptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local c=e:GetHandler()
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-    if chk==0 then return Duel.IsExistingTarget(s.burrowupfilter,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectTarget(tp,s.burrowupfilter,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
-end
-function s.burrowupop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
-        local c=e:GetHandler()
-        if c:IsRelateToEffect(e) then
-            Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
-        end
-    end
-end
---Zombie Revive Function
+--Revive Function
 function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.CheckLPCost(tp,500) end
-    Duel.PayLPCost(tp,500)
+    if chk==0 then return Duel.CheckLPCost(tp,250) end
+    Duel.PayLPCost(tp,250)
 end
 function s.sumcon(e,tp,c)
-    if c==nil then return true end
-    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if c==nil then return true end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    local ct=c:GetTurnCounter()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and ct<1 end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local ct=c:GetTurnCounter()
-    if e:GetHandler():IsRelateToEffect(e) then
-        ct=ct+1
-        c:SetTurnCounter(ct)
+	if e:GetHandler():IsRelateToEffect(e) then
         Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+	end
+end
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevelBelow(3) and c:IsRace(RACE_ZOMBIE) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end

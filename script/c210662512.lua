23925a60e991a79337dfdbe3f9cbb52405a9bd96
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-25 16:55:06 +0300

    rework Li'l Zyclone

--- script/c210662512.lua
+++ script/c210662512.lua
@@ -2,5 +2,88 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --To Defense
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_POSITION)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.deftg)
+    e1:SetOperation(s.defop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Revive
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCost(s.sumcost)
+    e4:SetCondition(s.sumcon)
+    e4:SetTarget(s.sumtg)
+    e4:SetOperation(s.sumop)
+    c:RegisterEffect(e4)
+    --Add lv3 or lower Monster
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,2))
+    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetTarget(s.drtg)
+    e5:SetOperation(s.drop)
+    c:RegisterEffect(e5)
+end
+--to defense function
+function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return e:GetHandler():IsAttackPos() end
+    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
+end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
+        Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+    end
+end
+--Revive Function
+function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckLPCost(tp,250) end
+    Duel.PayLPCost(tp,250)
+end
+function s.sumcon(e,tp,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+	end
+end
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevelBelow(3) and c:IsRace(RACE_ZOMBIE) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end

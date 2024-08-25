729ce4a627d19897fa863abbe2fdaac65be797fc
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-25 16:15:43 +0300

    revert something

--- script/c210662921.lua
+++ script/c210662921.lua
@@ -2,102 +2,43 @@
 --Scripted by Konstak.
 local s,id=GetID()
 function s.initial_effect(c)
-    --Revive
+    --Attack Up Ability
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetRange(LOCATION_GRAVE)
-    e1:SetCost(s.sumcost)
-    e1:SetCondition(s.sumcon)
-    e1:SetTarget(s.sumtg)
-    e1:SetOperation(s.sumop)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetTarget(s.tg)
+    e1:SetValue(250)
     c:RegisterEffect(e1)
-    --Surge Attack on Battle
+    --Add lv4 Monster
     local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e2:SetCondition(s.surgecon)
-    e2:SetTarget(s.surgetg)
-    e2:SetOperation(s.surgeop)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetTarget(s.drtg)
+    e2:SetOperation(s.drop)
     c:RegisterEffect(e2)
-    --Attack all each time
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_ATTACK_ALL)
-    e3:SetValue(1)
-    c:RegisterEffect(e3)
 end
-function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.CheckLPCost(tp,500) end
-    Duel.PayLPCost(tp,500)
+--Attack Up function
+function s.tg(e,c)
+	return c:IsRace(RACE_ZOMBIE)
 end
-function s.sumcon(e,tp,c)
-    if c==nil then return true end
-    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevel(4) and c:IsRace(RACE_ZOMBIE) and c:IsAbleToHand()
 end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    local ct=c:GetTurnCounter()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and ct<1 end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local ct=c:GetTurnCounter()
-    if e:GetHandler():IsRelateToEffect(e) then
-        ct=ct+1
-        c:SetTurnCounter(ct)
-        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
-end
---Surge on Battle Function
-function s.surgecon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()==tp
-end
-function s.surgetg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
-end
-function s.surgeop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    Duel.NegateAttack()
-    local d1=6
-    while d1>3 do
-        d1=Duel.TossDice(tp,1)
-    end
-    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
-    local Zone=d1
-    if tc then
-        local seq=tc:GetSequence()
-        local nseq=seq
-        Duel.Destroy(tc,REASON_EFFECT)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_DISABLE_FIELD)
-        e1:SetRange(LOCATION_SZONE)
-        e1:SetLabel(nseq+16)
-        e1:SetOperation(s.disop)
-        e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
-        Duel.RegisterEffect(e1,tp)
-    else
-        local seq=Zone
-        local nseq=seq
-        if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_FIELD)
-            e1:SetCode(EFFECT_DISABLE_FIELD)
-            e1:SetRange(LOCATION_SZONE)
-            e1:SetLabel(nseq+16)
-            e1:SetOperation(s.disop)
-            e1:SetReset(RESET_PHASE+PHASE_STANDBY,1)
-            Duel.RegisterEffect(e1,tp)
-        end
-    end
-end
-function s.disop(e,tp)
-    return 0x1<<e:GetLabel()
 end

7f2ee73ed6792c9f01f1191a01a1a4e06e29bad5
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-08-25 21:32:56 +0300

    remove freeze from Zir Zeal

--- script/c210662290.lua
+++ script/c210662290.lua
@@ -26,13 +26,6 @@ function s.initial_effect(c)
     e2:SetTarget(s.burrowuptg)
     e2:SetOperation(s.burrowupop)
     c:RegisterEffect(e2)
-    --Freeze Ability
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e3:SetCondition(s.freezecon)
-    e3:SetOperation(s.freezeop)
-    c:RegisterEffect(e3)
 end
 --burrow down function
 function s.burrowdowntg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -74,48 +67,4 @@ function s.burrowupop(e,tp,eg,ep,ev,re,r,rp)
             Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
         end
     end
-end
---Freeze Function
-function s.freezecon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
-end
-function s.freezeop(e,tp,eg,ep,ev,re,r,rp)
-    local effp=e:GetHandler():GetControler()
-    local c=e:GetHandler()
-    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_SKIP_DP)
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e1:SetTargetRange(0,1)
-        if Duel.GetTurnPlayer()==effp then
-            e1:SetLabel(Duel.GetTurnCount())
-            e1:SetCondition(s.skipcon)
-            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
-        else
-            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
-        end
-        Duel.RegisterEffect(e1,effp)
-        local e2=Effect.CreateEffect(e:GetHandler())
-        e2:SetCategory(CATEGORY_DRAW)
-        e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e2:SetCode(EVENT_PHASE+PHASE_END)
-        e2:SetCondition(s.retcon)
-        e2:SetLabel(Duel.GetTurnCount())
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-        e2:SetCountLimit(1)
-        e2:SetOperation(s.droperation)
-        Duel.RegisterEffect(e2,effp)
-        Duel.NegateAttack()
-    end
-end
-function s.skipcon(e)
-    return Duel.GetTurnCount()~=e:GetLabel()
-end
-function s.retcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-function s.droperation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_CARD,0,id)
-	Duel.Draw(1-tp,2,REASON_EFFECT)
 end

0e2cbb8ddbc92b577e55bca5bb139511cac1f861
DESKTOP-DIVJ3KT\Gid-Linux <gideon_gallagher@zoho.com>
2024-08-26 17:58:50 -0700

    Dioramos Unfinished. (E4 is not done)

--- script/c210660177.lua
+++ script/c210660177.lua
@@ -1,10 +1,109 @@
 --Dioramos
---Scripted by " "
+--Scripted by Gideon
 --Effect
---
+-- (1) You can reveal this card in your hand; Draw 2 cards, then, destroy this card and 1 level 8 monster in your hand. If you cannot, banish your hand face-down. You can only use this effect of "Dioramos" once per turn.
+-- (2) When this card is Tribute Summoned: Gain the following effect: 
+-- After damage calculation, when this card battles an opponent's monster: You can banish that monster, also banish this card.
+-- (3) Cannot be destroyed by battle.
+-- (4) If this card is destroyed except by its own effect: You can banish this card from your GY and target 1 card your opponent controls; Draw 2 cards then destroy 1 card in your hand, then banish that target.
 local s,id=GetID()
 function s.initial_effect(c)
-
-
-
+    --Darkness on crack(1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.rcost)
+	e1:SetTarget(s.rtarget)
+	e1:SetOperation(s.ropp)
+	c:RegisterEffect(e1)
+    --DD war lady tribute (2)
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetOperation(s.regop)
+	c:RegisterEffect(e2)
+    --Cannot be destroyed by battle (3)
+    local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+    --(4) banish this card from your GY and target 1 card your opponent controls; Draw 2 cards then destroy 1 card in your hand, then banish that target.
+end
+--e1
+function s.filter(c)
+	return c:IsMonster() and c:IsLevel(8) and not c:IsCode(id)
+end
+function s.rcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsPublic() end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_PUBLIC)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e:GetHandler():RegisterEffect(e1)
+end
+function s.rtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,30459350) and Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.ropp(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	Duel.ShuffleHand(p)
+	Duel.BreakEffect()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(p,s.filter,p,LOCATION_HAND,0,1,1,nil)
+	local tg=g:GetFirst()
+	if tg then
+        Duel.Destroy(c,REASON_EFFECT)
+		if Duel.Destroy(tg,POS_FACEUP,REASON_EFFECT)==0 then
+			Duel.ConfirmCards(1-p,tg)
+			Duel.ShuffleHand(p)
+		end
+	else
+		local sg=Duel.GetFieldGroup(p,LOCATION_HAND,0)
+		Duel.Remove(sg,POS_FACEDOWN,REASON_EFFECT)
+	end
+end
+--e2
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsSummonType(SUMMON_TYPE_TRIBUTE) then return end
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetTarget(s.btarget)
+	e1:SetOperation(s.boperation)
+	c:RegisterEffect(e1)
+end
+function s.btarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local a=Duel.GetAttacker()
+	local t=Duel.GetAttackTarget()
+	if chk==0 then
+		return (t==c and a:IsAbleToRemove())
+			or (a==c and t~=nil and t:IsAbleToRemove())
+	end
+	local g=Group.CreateGroup()
+	if a:IsRelateToBattle() then g:AddCard(a) end
+	if t~=nil and t:IsRelateToBattle() then g:AddCard(t) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+function s.boperation(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	local g=Group.FromCards(a,d)
+	local rg=g:Filter(Card.IsRelateToBattle,nil)
+	Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
 end

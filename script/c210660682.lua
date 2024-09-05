465d08825746b743ccf816bbd0b027dac14a61be
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-09-05 14:12:17 +0300

    Goth Vega scripted

--- script/c210660682.lua
+++ script/c210660682.lua
@@ -1,39 +1,62 @@
---By Tungnon
+--By Tungnon (effect 2*1 and 2*2 by Konstak)
 --Spectral Goth Vega
 local s,id=GetID()
 function s.initial_effect(c)
-	--Banish 1 target from the GY
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1)
+    --Banish 1 target from the GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    c:RegisterEffect(e1)
+    --act limit
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetTargetRange(1,1)
+    e2:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_TRIBUTE) end)
+    e2:SetValue(s.aclimit)
+    c:RegisterEffect(e2)
+    --remove
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+    e3:SetTargetRange(1,1)
+    e3:SetValue(LOCATION_REMOVED)
+    e3:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_TRIBUTE) end)
+    c:RegisterEffect(e3)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsDiscardable() end
-	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToGraveAsCost() and c:IsDiscardable() end
+    Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
 function s.rmfilter(c)
-	return c:IsAbleToRemove() and aux.SpElimFilter(c)
+    return c:IsAbleToRemove() and aux.SpElimFilter(c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and s.rmfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and s.rmfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
-	end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+function s.aclimit(e,re,tp)
+    return re:IsActiveType(TYPE_MONSTER)
 end

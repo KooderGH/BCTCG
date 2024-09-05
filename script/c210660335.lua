80226e4c250178e2c209da5f12434f87a9f566c6
poka-poka <regaalfiansyah@gmail.com>
2024-09-05 14:08:48 +0700

    Adventure Kanna + Sea Maiden Ruri done~

--- script/c210660335.lua
+++ script/c210660335.lua
@@ -1,47 +1,124 @@
 --Sea Maiden Ruri
---Scripted by senorpizza
---Effect:
---(1) During your opponent's turn (Quick Effect): You can Tribute this card from your hand or face-up field; neither player can banish cards for the rest of this turn.
+--Old effect Scripted by senorpizza
+--Previous effect(1) During your opponent's turn (Quick Effect): You can Tribute this card from your hand or face-up field; neither player can banish cards for the rest of this turn.
+--New effect Scripted by poka-poka
 
-local s,id=GetID()
+local s, id = GetID()
 function s.initial_effect(c)
-	--During your opponent's turn (Quick Effect): You can Tribute this card from your hand or face-up field; neither player can banish cards for the rest of this turn.
-    	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
-	e1:SetCondition(s.rmcon)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(s.rmcost)
-	e1:SetOperation(s.rmop)
-	c:RegisterEffect(e1)
+    -- Effect 1: Send from hand to GY and banish cards sent to GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.nograycon)
+    e1:SetCost(s.handcost)
+    e1:SetOperation(s.banishop)
+    c:RegisterEffect(e1)
+    -- Effect 2: Return from banished zone to GY, draw 2 cards (Once per Duel)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_REMOVED)
+    e2:SetCost(s.banishcost)
+    e2:SetOperation(s.drawop)
+    e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+    c:RegisterEffect(e2) 
+    -- Effect 3: Special Summon from hand if you control no monsters
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_SPSUMMON_PROC)
+    e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e3:SetRange(LOCATION_HAND)
+    e3:SetCondition(s.spcon)
+    c:RegisterEffect(e3)
+    -- Effect 4: Equip and take control during End Phase if destroyed by opponent card
+    local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1)
+    e4:SetCondition(s.eqcon)
+    e4:SetTarget(s.eqtg)
+    e4:SetOperation(s.eqop)
+    c:RegisterEffect(e4)
 end
 
-function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==1-tp
+-- Condition for Effect 1: No cards in GY
+function s.nograycon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetFieldGroupCount(tp, LOCATION_GRAVE, 0)==0
 end
-function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsReleasable() and Duel.GetFlagEffect(0,id)==0 end
-	Duel.Release(e:GetHandler(),REASON_COST)
+-- Cost for Effect 1: Send this card from hand to GY
+function s.handcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk == 0 then return e:GetHandler():IsAbleToGraveAsCost() end
+    Duel.SendtoGrave(e:GetHandler(), REASON_COST)
 end
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_REMOVE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,1)
-	e1:SetValue(1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	--30459350 chk
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(30459350)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetTargetRange(1,1)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,1,aux.Stringid(id,2),nil)
-	Duel.RegisterFlagEffect(0,id,RESET_PHASE+PHASE_END,0,1)
+-- Operation for Effect 1: Cards sent to GY are banished instead
+function s.banishop(e,tp,eg,ep,ev,re,r,rp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e1:SetTargetRange(0xff, 0xff)
+    e1:SetValue(LOCATION_REMOVED)
+    e1:SetReset(RESET_PHASE+PHASE_END + RESET_OPPO_TURN)
+    Duel.RegisterEffect(e1,tp)
+end
+-- Cost for Effect 2: Return this card from banished zone to GY
+function s.banishcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToGrave() end
+    Duel.SendtoGrave(e:GetHandler(), REASON_COST + REASON_RETURN)
+end
+-- Operation for Effect 2: Draw 2 cards
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(tp,2,REASON_EFFECT)
+end
+-- Condition for Effect 3: Control no monsters
+function s.spcon(e,c)
+    if c == nil then return true end
+    return Duel.GetFieldGroupCount(c:GetControler(), LOCATION_MZONE,0)==0
+end
+-- Condition for Effect 4: Sent to GY because destroyed by opponent's card
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+    local c = e:GetHandler()
+    return c:IsReason(REASON_DESTROY) and rp ~= tp and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+-- Target for Effect 4: Equip to an opponent's monster
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and aux.CheckStealEquip(chkc, e, tp) end
+    if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,aux.CheckStealEquip,tp,0,LOCATION_MZONE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+end
+-- Equip limit function
+function s.eqlimit(e,c)
+    return e:GetOwner()==c
+end
+-- Operation for Effect 4: Equip this card and take control of target
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and c:IsRelateToEffect(e) and aux.CheckStealEquip(tc, e, tp) and tc:IsRelateToEffect(e) and Duel.Equip(tp, c, tc) then
+        -- Add Equip limit
+        local e1 = Effect.CreateEffect(tc)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_EQUIP_LIMIT)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        e1:SetValue(s.eqlimit)
+        c:RegisterEffect(e1)
+        -- Control of equipped monster
+        local e2 = Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_EQUIP)
+        e2:SetCode(EFFECT_SET_CONTROL)
+        e2:SetValue(tp)
+        e2:SetReset(RESET_EVENT + RESETS_STANDARD - RESET_TURN_SET)
+        c:RegisterEffect(e2)
+    end
 end
--- script/c210660607.lua
+++ script/c210660607.lua
@@ -1,4 +1,4 @@
---By Tungnon
+--By Tungnon e1 and poka-poka e2
 --Adventurer Kanna
 local s,id=GetID()
 function s.initial_effect(c)
@@ -14,14 +14,19 @@ function s.initial_effect(c)
 	e1:SetTarget(s.distg)
 	e1:SetOperation(s.disop)
 	c:RegisterEffect(e1)
-	--Double Snare
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_SINGLE)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
-	e6:SetRange(LOCATION_HAND)
-	e6:SetCode(3682106)
-	c:RegisterEffect(e6)
+	-- Tribute this card, draw and reveal 1 card, if its a monster draw again.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.drawcost)
+	e2:SetTarget(s.drawtg)
+	e2:SetOperation(s.drawop)
+	c:RegisterEffect(e2)	
 end
+
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
 	return ep~=tp and Duel.IsChainDisablable(ev) and loc==LOCATION_GRAVE
@@ -36,4 +41,26 @@ function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
+end
+-- Effect 2 Cost: Tribute this card
+function s.drawcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsReleasable() end
+    Duel.Release(e:GetHandler(),REASON_COST)
+end
+-- Effect 2 Target: Draw 1 card
+function s.drawtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+-- Effect 2 Operation: Draw 1 card, reveal it, and draw 1 more if it's a Monster Card
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+    local dc=Duel.Draw(tp,1,REASON_EFFECT)
+    local tc=Duel.GetOperatedGroup():GetFirst()
+    if tc then
+        Duel.ConfirmCards(1-tp,tc)
+        if tc:IsType(TYPE_MONSTER) then
+            Duel.Draw(tp,1,REASON_EFFECT)
+        end
+        Duel.ShuffleHand(tp)
+    end
 end

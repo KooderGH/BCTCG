d6ad07f1916b7e0cbd66776eb16271be30e293de
Gideon <gideon_gallagher@zoho.com>
2024-09-04 22:31:30 -0700

    Push local

--- script/c210660732.lua
+++ script/c210660732.lua
@@ -1,40 +1,54 @@
---By Tungnon
 --Ninja Girl Tomoe
+--By Gideon
+-- (1) When a monster effect is activated in your opponent's hand (Quick Effect): You can send this card from your hand to the GY; negate the activation, and if you do, return that card to their hand (if possible). For the rest of this turn after this card resolves, your opponent cannot activate cards, or the effects of cards, with the same name. You can only use this effect of "Ninja Girl Tomoe" once per turn.
+-- (2) FLIP: You can draw 2 cards, then discard 1 card.
+-- (3) If this card is destroyed by battle; Add 1 monster from your deck to your hand.
 local s,id=GetID()
 function s.initial_effect(c)
-	--Inactivate
+	--Negate Handtrap (1)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.discon)
-	e1:SetCost(s.discost)
-	e1:SetTarget(s.distg)
-	e1:SetOperation(s.disop)
+	e1:SetCondition(s.condition)
+    e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.opp)
 	c:RegisterEffect(e1)
-	--Double Snare
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_SINGLE)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
-	e6:SetRange(LOCATION_HAND)
-	e6:SetCode(3682106)
-	c:RegisterEffect(e6)
 end
-function s.discon(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	return ep~=tp and Duel.IsChainDisablable(ev) and loc==LOCATION_HAND
+	return (loc==LOCATION_HAND) and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
 end
-function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rc=re:GetHandler()
+	local relation=rc:IsRelateToEffect(re)
+	if chk==0 then return rc:IsAbleToHand(tp)
+		or (not relation and Duel.IsPlayerCanSendtoHand(tp)) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if relation then
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,rc,1,rc:GetControler(),rc:GetLocation())
+	else
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,0,0,rc:GetPreviousLocation())
+	end
 end
-function s.disop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateEffect(ev)
+function s.opp(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SendtoHand(tc,REASON_EFFECT)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetValue(s.aclimit)
+		Duel.RegisterEffect(e1,tp)
+	end
 end
-
+function s.aclimit(e,re,tp)
+	return re:GetHandler():IsCode(e:GetLabel()) and re:IsActiveType(TYPE_MONSTER)
+end

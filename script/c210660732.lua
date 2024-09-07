1d6c83a9262592442dc8ee6cd982a82749c72d95
Gideon <gideon_gallagher@zoho.com>
2024-09-07 03:06:22 -0700

    Ninja Girl Tomoe done

--- script/c210660732.lua
+++ script/c210660732.lua
@@ -17,38 +17,102 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.opp)
 	c:RegisterEffect(e1)
+    --Flip (2)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.fcost)
+	e2:SetTarget(s.ftarget)
+	e2:SetOperation(s.foperation)
+	c:RegisterEffect(e2)
+    --Search (3)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_BATTLE_DESTROYED)
+	e3:SetCondition(s.scondition)
+	e3:SetTarget(s.starget)
+	e3:SetOperation(s.soperation)
+	c:RegisterEffect(e3)
+end
+--e1
+function s.cfilter(c,tp)
+	return c:IsControler(1-tp)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	return (loc==LOCATION_HAND) and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+	return (loc==LOCATION_HAND) and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev) and eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local rc=re:GetHandler()
-	local relation=rc:IsRelateToEffect(re)
-	if chk==0 then return rc:IsAbleToHand(tp)
-		or (not relation and Duel.IsPlayerCanSendtoHand(tp)) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if relation then
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,rc,1,rc:GetControler(),rc:GetLocation())
-	else
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,0,0,rc:GetPreviousLocation())
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,eg,1,0,0)
 	end
 end
 function s.opp(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SendtoHand(tc,REASON_EFFECT)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    local ec=re:GetHandler()
+	if Duel.NegateActivation(ev) then
+		ec:CancelToGrave()
+        Duel.SendtoHand(ec,nil,2,REASON_EFFECT)
+		local e1=Effect.CreateEffect(ec)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(0,1)
 		e1:SetValue(s.aclimit)
+		e1:SetLabel(ec:GetCode())
+		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-	end
+    end
 end
 function s.aclimit(e,re,tp)
-	return re:GetHandler():IsCode(e:GetLabel()) and re:IsActiveType(TYPE_MONSTER)
+	return re:GetHandler():IsCode(e:GetLabel())
+end
+--e2
+function s.fcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
+function s.ftarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function s.foperation(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	if Duel.Draw(p,2,REASON_EFFECT)==2 then
+		Duel.ShuffleHand(tp)
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+end
+--e3
+function s.scondition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
+end
+function s.starget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.sfilter(c)
+	return c:IsAbleToHand()
+end
+function s.soperation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end

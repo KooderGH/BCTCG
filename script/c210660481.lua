4142f4fde72909197664aa78a4945375686a93fc
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-09-24 18:50:41 +0300

    fix doktor heaven's GY recovery effect

--- script/c210660481.lua
+++ script/c210660481.lua
@@ -233,6 +233,19 @@ function s.graverecoveryop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
 		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,e:GetHandler())
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+        e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+        e1:SetReset(RESET_PHASE+PHASE_END)
+        e1:SetTargetRange(1,0)
+        Duel.RegisterEffect(e1,tp)
+        local e2=Effect.CreateEffect(e:GetHandler())
+        e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+        e2:SetDescription(aux.Stringid(id,1))
+        e2:SetReset(RESET_PHASE+PHASE_END)
+        e2:SetTargetRange(1,0)
+        Duel.RegisterEffect(e2,tp)
 	end
 end
 --(7) functions core

6a389a9a237280549715668d586fb89e5931adb1
Gideon9212 <gideon_gallagher@zoho.com>
2024-11-12 19:36:20 -0800

    Fix Yuki (D)

--- script/c210660180.lua
+++ script/c210660180.lua
@@ -88,7 +88,7 @@ function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.dop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,ds.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

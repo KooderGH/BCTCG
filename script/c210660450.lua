d878b89df0f0a7ae5eebdef46e4827f1b6938773
Gideon9212 <gideon_gallagher@zoho.com>
2024-11-28 09:50:08 -0800

    Emergency effect change Babel

--- script/c210660450.lua
+++ script/c210660450.lua
@@ -165,6 +165,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
         and Duel.GetFieldGroupCount(tp,LOCATION_EMZONE,0)==0
         and (Duel.CheckLocation(tp,LOCATION_EMZONE,0) or Duel.CheckLocation(tp,LOCATION_EMZONE,1))end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetChainLimit(aux.FALSE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()

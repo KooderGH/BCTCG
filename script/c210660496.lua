c331c455d42baf5be0886d10ae7dd98f43eeb0f4
poka-poka <regaalfiansyah@gmail.com>
2024-11-30 13:42:15 +0700

    Kaihime Fix 1+3
    effect 1 Now the effect applies to self instead to opponent card
    effect 3 :
    -effect is optional
    -only trigger when kaihime is attacked (attack a monster during a battle phase) not when this card was attacked
    -only trigger if it still in the Mzone

--- script/c210660496.lua
+++ script/c210660496.lua
@@ -12,22 +12,14 @@ function s.initial_effect(c)
     e1:SetValue(s.immval)
     c:RegisterEffect(e1)
     -- Cannot be targeted for attacks if equipped with Equip Spell
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
-    e2:SetCondition(s.atkcon)
-    e2:SetValue(1)
-    c:RegisterEffect(e2)
-    -- Allow enemies to attack directly if only this card or its copies are controlled and all are equipped with Equip Spells
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD)
-    e3:SetCode(EFFECT_DIRECT_ATTACK)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetTargetRange(0,LOCATION_MZONE)
-    e3:SetCondition(s.condiratk)
-    c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1)
+	e2:SetCondition(s.atkcon)
+	c:RegisterEffect(e2)
     -- Equip an Equip Spell from GY
     local e4=Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id,0))
@@ -45,22 +37,12 @@ function s.initial_effect(c)
     e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e5:SetCode(EVENT_PHASE+PHASE_BATTLE)
     e5:SetRange(LOCATION_MZONE)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
     e5:SetCountLimit(1)
     e5:SetCondition(s.battlesendcon)
+	e5:SetTarget(s.battlesendtg)
     e5:SetOperation(s.battlesendop)
     c:RegisterEffect(e5)
-    -- Track if the card was attacked during the Battle Phase
-    local e6=Effect.CreateEffect(c)
-    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e6:SetCode(EVENT_BE_BATTLE_TARGET)
-    e6:SetOperation(s.attacked_op)
-    c:RegisterEffect(e6)
-    -- Reset the flag at the end of the turn
-    local e7=Effect.CreateEffect(c)
-    e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e7:SetCode(EVENT_TURN_END)
-    e7:SetOperation(s.reset_op)
-    Duel.RegisterEffect(e7,0)
     -- Add 2 FIRE Warrior Monsters to hand if this monster destroys an opponent's monster in battle
     local e8=Effect.CreateEffect(c)
     e8:SetDescription(aux.Stringid(id,2))
@@ -90,22 +72,7 @@ end
 function s.atkcon(e)
     local c=e:GetHandler()
     local eqg=c:GetEquipGroup()
-    return eqg and eqg:GetCount() > 0
-end
--- Allow enemies to attack directly if only this card or its copies are controlled and all are equipped with Equip Spells
-function s.condiratk(e)
-    local c=e:GetHandler()
-    local tp=c:GetControler()
-    local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_MZONE,0,nil,c:GetCode())
-    local allEquipped=true
-    for tc in aux.Next(g) do
-        local eqg = tc:GetEquipGroup()
-        if not (eqg and eqg:GetCount()>0) then
-            allEquipped=false
-            break
-        end
-    end
-    return g:GetCount()>0 and g:GetCount()==Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0) and allEquipped
+    return eqg and eqg:GetCount()>0
 end
 -- Equip an Equip Spell from GY
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -142,8 +109,11 @@ function s.eqlimit(e,c)
 end
 -- Add 1 FIRE Warrior Monster from Deck to hand if this card is attacked during the Battle Phase
 function s.battlesendcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:GetFlagEffect(id)~=0
+	return e:GetHandler():GetAttackAnnouncedCount()~=0
+end
+function s.battlesendtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.battlesendop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) then
@@ -153,18 +123,6 @@ function s.battlesendop(e,tp,eg,ep,ev,re,r,rp)
             Duel.SendtoHand(g,nil,REASON_EFFECT)
             Duel.ConfirmCards(1-tp,g)
         end
-    end
-end
--- Flag to track if the card was attacked
-function s.attacked_op(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE,0,1)
-end
--- Reset the flag at the end of the turn
-function s.reset_op(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-    for tc in aux.Next(g) do
-        tc:ResetFlagEffect(id)
     end
 end
 -- Add 2 FIRE Warrior Monsters to hand if this monster destroys an opponent's monster in battle

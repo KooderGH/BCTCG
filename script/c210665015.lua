f0bb526a84825f218b056bb88ae0f1582b7bed58
Señor Pizza <168953777+senorpizza@users.noreply.github.com>
2024-12-10 18:02:53 -0800

    Manifestation Of A Dying Star At The End Of The Universe part 2 (reupload)
    please work this time 🥺

--- script/c210665015.lua
+++ script/c210665015.lua
@@ -1,4 +1,29 @@
+--Add 1 “Lumina” from your deck or GY to your hand. You can only activate “Manifestation Of A Dying Star At The End Of The Universe” once per turn.
+--Scripted by senorpizza
 local s,id=GetID()
 function s.initial_effect(c)
-
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+--Check if a card is able to be added to hand and is Lumina
+function s.filter(c)
+	return c:IsCode(210660478) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end

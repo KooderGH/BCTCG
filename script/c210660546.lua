a9d3aca92a9c820647789336512e787b491944a6
poka-poka <regaalfiansyah@gmail.com>
2024-12-13 14:31:09 +0700

    papaluga

--- script/c210660546.lua
+++ script/c210660546.lua
@@ -12,6 +12,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
+	--2
 	--The ATK of non-Fiend monsters on the field becomes 200.
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -19,10 +20,23 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_SET_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.no_non_fiend_con)
 	e2:SetTarget(s.atktarget)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e2:SetValue(400)
 	c:RegisterEffect(e2)
+	-- Effect that applies only to the owner's field if they control a non-Fiend monster
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_ATKCHANGE)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_SET_ATTACK)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.has_non_fiend_con)
+	e5:SetTarget(s.atktarget)
+	e5:SetTargetRange(LOCATION_MZONE,0)
+	e5:SetValue(400)
+	c:RegisterEffect(e5)
 	--3
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -43,6 +57,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 --E2
+--No non-Fiend monsters are controlled
+function s.no_non_fiend_con(e)
+    return not Duel.IsExistingMatchingCard(aux.NOT(Card.IsRace),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,RACE_FIEND)
+end
+--non-Fiend monster is controlled
+function s.has_non_fiend_con(e)
+    return Duel.IsExistingMatchingCard(aux.NOT(Card.IsRace),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,RACE_FIEND)
+end
 function s.atktarget(e,c)
 	return not c:IsRace(RACE_FIEND)
 end

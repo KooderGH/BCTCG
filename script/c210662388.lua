b148a5a317a6b96f1ce12f067db90fb4209c4493
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-05 22:56:24 +0200

    UltraBaaBaa on BCETCG

--- script/c210662388.lua
+++ script/c210662388.lua
@@ -2,5 +2,63 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --To Defense
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_POSITION)
+    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.deftg)
+    e1:SetOperation(s.defop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Can target 1 card on the field. destroy that target (Ignition)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
+    --Immune to Wave, Surge, Single Attack
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e5:SetValue(1)
+    c:RegisterEffect(e5)
+end
+--to defense function
+function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return e:GetHandler():IsAttackPos() end
+    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
+end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
+        Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+    end
+end
+--Destroy That Target Function
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end

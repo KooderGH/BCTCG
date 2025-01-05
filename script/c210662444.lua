a740c17beb32e8084f8549e1f6faeb20a48f44ea
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-05 22:30:34 +0200

    Gardeneel bros on BCETCG

---
+++ script/c210662444.lua
@@ -0,0 +1,53 @@
+-- MesocosmoCyclone
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Battle RNG Warp Mechanic
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.warpcon)
+	e1:SetOperation(s.warpop)
+	c:RegisterEffect(e1)
+    --Attack all each time
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_ATTACK_ALL)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+end
+function s.warpcon(e,tp,eg,ep,ev,re,r,rp)
+	return (e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or e:GetHandler()==Duel.GetAttackTarget()
+end
+function s.warpop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+	if tc:IsRelateToBattle() and Duel.TossCoin(tp,1)==COIN_HEADS then
+		if Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+            e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+            e1:SetLabelObject(tc)
+            e1:SetCountLimit(1)
+            e1:SetOperation(s.returnop)
+            Duel.RegisterEffect(e1,tp)
+            Duel.NegateAttack()
+		end
+	end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    if ct==1 then
+        ct=0
+        c:SetTurnCounter(ct)
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.ReturnToField(e:GetLabelObject())
+    end
+end

f132e9d9583dfa8f5c0846ce0f3f1fd9d1a79cba
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-05 23:05:05 +0200

    Update Shibalien Elite

--- script/c210662360.lua
+++ script/c210662360.lua
@@ -2,17 +2,55 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Targetable Warp Mechanic
+    --When Normal Summoned (Search Ability)
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetTarget(s.warptg)
-    e1:SetOperation(s.warpop)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.srtg)
+    e1:SetOperation(s.srop)
     c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    --Targetable Warp Mechanic
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE+LOCATION_SZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.warptg)
+    e3:SetOperation(s.warpop)
+    c:RegisterEffect(e3)
+    --return hand (Peon Ability)
+    local e4=Effect.CreateEffect(c)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
 end
+--When NS add function
+function s.dfilter(c)
+    return c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsAbleToHand()
+end
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_DECK,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_DECK,0,2,2,nil)
+    if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Warp Function
 function s.warpfilter(c)
 	return c:IsAbleToRemove() and c:IsMonster() and c:IsCanBeEffectTarget() and not c:IsCode(id)
 end
@@ -50,5 +88,16 @@ function s.returnop(e,tp,eg,ep,ev,re,r,rp)
         c:SetTurnCounter(ct)
         Duel.Hint(HINT_CARD,0,id)
         Duel.ReturnToField(e:GetLabelObject())
+    end
+end
+--Peon Ability
+function s.destg(e,tp,eg,ev,ep,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.desop(e,tp,eg,ev,ep,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
     end
 end

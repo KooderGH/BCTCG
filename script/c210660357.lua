7e628e12af485306de24c762bbf6aff2a6be7b9c
Gideon <gideon_gallagher@zoho.com>
2025-01-07 21:17:52 -0800

    The wolfchild update

--- script/c210660357.lua
+++ script/c210660357.lua
@@ -1,5 +1,12 @@
 --Wolfchild Deale
---By Tungnon
+--Effect 1 Scripted By Tungnon, rest Gideon.
+-- (1) When a card or effect is activated that includes any of these effects (Quick Effect): You can discard this card; negate that activation.
+-- ● Add a card(s) from the GY to the hand, Deck, and/or Extra Deck.
+-- ● Special Summon a Monster Card(s) from the GY.
+-- ● Banish a card(s) from the GY.
+-- You can only use this effect of "Wolfchild Deale" once per turn. 
+-- (2) You can Special Summon from your GY by banishing 2 spell cards from your GY (Ignition). If you do, banish this card when it leaves the field. 
+-- (3) If this card is banished; draw 1 card. You can only use this effect of "Wolfchild Deale" once per turn. 
 local s,id=GetID()
 function s.initial_effect(c)
 	--Negate
@@ -16,7 +23,30 @@ function s.initial_effect(c)
 	e1:SetTarget(s.distg)
 	e1:SetOperation(s.disop)
 	c:RegisterEffect(e1)
+    --Special summon itself from GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+    --When banished, draw 1 card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetTarget(s.drtg)
+	e3:SetOperation(s.drop)
+	e3:SetCountLimit(1,{id,1})
+	c:RegisterEffect(e3)
 end
+--e1
 function s.check(ev,category)
 	local ex1,g1,gc1,dp1,loc1=Duel.GetOperationInfo(ev,category)
 	local ex2,g2,gc2,dp2,loc2=Duel.GetPossibleOperationInfo(ev,category)
@@ -47,4 +77,42 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SendtoGrave(eg,REASON_EFFECT)
 	end
+end
+--e2
+function s.cfilter2(c,tp)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_GRAVE,0,2,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter2,tp,LOCATION_GRAVE,0,2,2,c)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		--Banish it if it leaves the field
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3300)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		c:RegisterEffect(e1,true)
+	end
+end
+--e3
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
 end

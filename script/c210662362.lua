ee3c44e95644449249a60d1c3eb84061dc9384c6
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 18:03:06 +0200

    progress on general greg

--- script/c210662362.lua
+++ script/c210662362.lua
@@ -2,5 +2,12 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --Immune to Wave, Surge, Single Attack
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
 end

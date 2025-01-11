81f10cc7426eee5dde487914a54ee2325f5ea410
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 21:25:09 +0200

    script Kid Tappa

--- script/c210662417.lua
+++ script/c210662417.lua
@@ -25,6 +25,16 @@ function s.initial_effect(c)
     e5:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e5:SetValue(1)
     c:RegisterEffect(e5)
+    --When destroyed (Search Ability)
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,0))
+    e6:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e6:SetCode(EVENT_DESTROYED)
+    e6:SetTarget(s.drtg)
+    e6:SetOperation(s.drop)
+    c:RegisterEffect(e6)
 end
 --to defense function
 function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -37,17 +47,19 @@ function s.defop(e,tp,eg,ep,ev,re,r,rp)
         Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
     end
 end
---Destroy That Target Function
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() end
-    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevel(5) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsAbleToHand()
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.Destroy(tc,REASON_EFFECT)
-	end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end

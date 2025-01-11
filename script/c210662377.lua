b4c501155b6460ed201da1e0384cbbc5a0929d42
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 20:12:29 +0200

    Fully script SCC

--- script/c210662377.lua
+++ script/c210662377.lua
@@ -25,6 +25,17 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_ATTACK_ALL)
     e2:SetValue(1)
     c:RegisterEffect(e2)
+    --Battle Warp Mechanic
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_START)
+    e3:SetCondition(s.warpcon)
+    e3:SetTarget(s.warptg)
+    e3:SetOperation(s.warpop)
+    c:RegisterEffect(e3)
 end
 function s.alienfilter(c)
     return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
@@ -47,4 +58,44 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     if not g then return end
     Duel.Release(g,REASON_COST)
     g:DeleteGroup()
+end
+--Warp Function
+function s.warpcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.warptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
+end
+function s.warpop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=e:GetHandler():GetBattleTarget()
+    local d1=Duel.TossDice(tp,1)
+    if bc:IsRelateToBattle() and d1<=2 then
+        if Duel.Remove(bc,bc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+            e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetReset(RESET_PHASE+PHASE_MAIN1,2)
+            e1:SetLabelObject(bc)
+            e1:SetCountLimit(1)
+            e1:SetOperation(s.returnop)
+            Duel.RegisterEffect(e1,tp)
+            Duel.NegateAttack()
+        end
+    end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    if ct==1 then
+        ct=0
+        c:SetTurnCounter(ct)
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.ReturnToField(e:GetLabelObject())
+    end
 end

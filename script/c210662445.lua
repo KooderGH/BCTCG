fff1b8e4c60bff7c96063d0f2250e68822fd4432
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-12 01:42:55 +0200

    Axoloty on BCETCG

---
+++ script/c210662445.lua
@@ -0,0 +1,69 @@
+-- Axoloty
+--Scripted by Konstak.
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e1=Effect.CreateEffect(c)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --Opponent No Battle Damage
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+    --Base Destroyer Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_CONFIRM)
+    e3:SetCondition(function() return Duel.GetAttackTarget()==nil end)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
+    --This card's Position cannot be changed.
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_SET_POSITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetValue(POS_FACEUP_ATTACK)
+    e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    c:RegisterEffect(e4)
+end
+function s.zombiefilter(c)
+    return c:IsRace(RACE_ZOMBIE) and c:IsFaceup()
+end
+function s.spcon(e,c)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.zombiefilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.SelectReleaseGroup(tp,s.zombiefilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
+end
+--Base Destroyer Function
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    if c:IsFaceup() then
+        Duel.Damage(1-tp,c:GetDefense(),REASON_EFFECT)
+    end
+end

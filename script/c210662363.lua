3a2ba90d26e2dcd6442ae4b08c2d029b702bc0b3
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 19:44:06 +0200

    Le'Solar on BCETCG

---
+++ script/c210662363.lua
@@ -0,0 +1,39 @@
+-- Le'Solar
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+    --special summon tribute
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+end
+--Alien SP Tribute Summon Function
+function s.alienfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end

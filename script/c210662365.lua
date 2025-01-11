0c9abc34ecc6ec72caf49ed1f9950a9bbce30f4a
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 19:09:32 +0200

    Corporal Weyland in BCETCG

---
+++ script/c210662365.lua
@@ -0,0 +1,96 @@
+-- Corporal Weyland
+--Scripted By Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Hide
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.spelltarget)
+    e1:SetOperation(s.spellop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Targetable Warp Mechanic
+    local e4=Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_REMOVE)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE+LOCATION_SZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.warptg)
+    e4:SetOperation(s.warpop)
+    c:RegisterEffect(e4)
+    --Immune to Wave, Surge, Single Attack
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e5:SetValue(1)
+    c:RegisterEffect(e5)
+end
+--Hide Function
+function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.spellop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
+end
+--Warp Function
+function s.warpfilter(c)
+	return c:IsAbleToRemove() and c:IsMonster() and c:IsCanBeEffectTarget() and not c:IsCode(id)
+end
+function s.warptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    local g=Duel.GetMatchingGroup(s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.warpop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.warpfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.HintSelection(g)
+        if Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+            e1:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetReset(RESET_PHASE+PHASE_MAIN1,3)
+            e1:SetLabelObject(tc)
+            e1:SetCountLimit(1)
+            e1:SetOperation(s.returnop)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local ct=c:GetTurnCounter()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    if ct==2 then
+        ct=0
+        c:SetTurnCounter(ct)
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.ReturnToField(e:GetLabelObject())
+    end
+end

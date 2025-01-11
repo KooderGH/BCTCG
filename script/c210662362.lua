63cc05c2190367c93466d798a7173e8ce223f701
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 19:26:46 +0200

    finish General Gregory

--- script/c210662362.lua
+++ script/c210662362.lua
@@ -2,12 +2,66 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Immune to Wave, Surge, Single Attack
+    --When Normal Summoned (Search Ability)
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    e1:SetValue(1)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(s.srtg)
+    e1:SetOperation(s.srop)
     c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    --Immune to Wave, Surge, Single Attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+    --When destroyed (Search Ability)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetTarget(s.drtg)
+    e4:SetOperation(s.drop)
+    c:RegisterEffect(e4)
+end
+--When NS add function
+function s.dfilter(c)
+    return c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsAbleToHand()
+end
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevelAbove(6) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end

914422dd0a1e7ac82ff6ebe60df69d8084b39ef3
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 18:10:13 +0200

    Update Mesocosmocyclone

--- script/c210662443.lua
+++ script/c210662443.lua
@@ -3,21 +3,32 @@
 local s,id=GetID()
 function s.initial_effect(c)
     --Battle RNG Warp Mechanic
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.warpcon)
-	e1:SetOperation(s.warpop)
-	c:RegisterEffect(e1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCondition(s.warpcon)
+    e1:SetOperation(s.warpop)
+    c:RegisterEffect(e1)
     --Attack all each time
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_ATTACK_ALL)
     e2:SetValue(1)
     c:RegisterEffect(e2)
+    --Add Monster
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetTarget(s.drtg)
+    e3:SetOperation(s.drop)
+    c:RegisterEffect(e3)
 end
+--RNG Warp Function
 function s.warpcon(e,tp,eg,ep,ev,re,r,rp)
 	return (e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or e:GetHandler()==Duel.GetAttackTarget()
 end
@@ -49,5 +60,21 @@ function s.returnop(e,tp,eg,ep,ev,re,r,rp)
         c:SetTurnCounter(ct)
         Duel.Hint(HINT_CARD,0,id)
         Duel.ReturnToField(e:GetLabelObject())
+    end
+end
+--Destroy and add function
+function s.drfilter(c)
+    return c:IsLevel(3) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_GRAVE,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end

5f3fba9074189416897c0bb23a6a58bdecf61fe4
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-11 19:59:00 +0200

    Raging Bahamut Cat in BCETCG

---
+++ script/c210662212.lua
@@ -0,0 +1,33 @@
+-- Raging Bahamut Cat
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_UPDATE_DEFENSE)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetLabel(1)
+    e1:SetCondition(s.ccon)
+    e1:SetValue(s.adval)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    c:RegisterEffect(e2)
+end
+function s.filter(c)
+    return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_CELESTIALWARRIOR) and c:IsFaceup()
+end
+function s.ccon(e)
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_ONFIELD,0,nil)>=e:GetLabel()
+end
+--Atk/Def gain
+function s.adval(e,c)
+	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,nil)*200
+end

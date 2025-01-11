c79660ca11d7f4c3c8bf8f7e66074248a30650a7
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-12 01:54:56 +0200

    fully script axoloty

--- script/c210662445.lua
+++ script/c210662445.lua
@@ -4,48 +4,52 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableUnsummonable()
     --special summon tribute
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+    --Wave on Battle
     local e1=Effect.CreateEffect(c)
-    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCode(EFFECT_SPSUMMON_PROC)
-    e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.wavecon)
+    e1:SetTarget(s.wavetg)
+    e1:SetOperation(s.waveop)
     c:RegisterEffect(e1)
-    --Opponent No Battle Damage
+    --to defense
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
-    e2:SetValue(1)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCondition(s.poscon)
+    e2:SetOperation(s.posop)
     c:RegisterEffect(e2)
-    --Base Destroyer Ability
+    --Immune to Wave, Surge, Single Attack
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_BATTLE_CONFIRM)
-    e3:SetCondition(function() return Duel.GetAttackTarget()==nil end)
-    e3:SetOperation(s.desop)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e3:SetValue(1)
     c:RegisterEffect(e3)
-    --This card's Position cannot be changed.
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetCode(EFFECT_SET_POSITION)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetValue(POS_FACEUP_ATTACK)
-    e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    c:RegisterEffect(e4)
 end
-function s.zombiefilter(c)
-    return c:IsRace(RACE_ZOMBIE) and c:IsFaceup()
+--Alien SP Tribute Summon Function
+function s.alienfilter(c)
+    return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
 end
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.zombiefilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.zombiefilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    local g=Duel.SelectReleaseGroup(tp,s.alienfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
     if g then
         g:KeepAlive()
         e:SetLabelObject(g)
@@ -59,11 +63,41 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     Duel.Release(g,REASON_COST)
     g:DeleteGroup()
 end
---Base Destroyer Function
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-    if c:IsFaceup() then
-        Duel.Damage(1-tp,c:GetDefense(),REASON_EFFECT)
+end
+--Wave on Battle Function
+function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp and e:GetHandler():GetBattleTarget()~=nil
+end
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
     end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    Duel.NegateAttack()
+    if tc then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--Once attack go to defense Position function
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetAttackedCount()>0
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	-- If attacks, then go to def
+	if c:IsAttackPos() then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+	c:RegisterEffect(e1)
 end

e478bb2ffab9c1be5a7cedc9a7150308bc6ae140
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-12 22:18:49 +0200

    finish filibuster

--- script/c210662447.lua
+++ script/c210662447.lua
@@ -13,51 +13,88 @@ function s.initial_effect(c)
     e0:SetTarget(s.sptg)
     e0:SetOperation(s.spop)
     c:RegisterEffect(e0)
-    --Filibuster timer Function
+    --Cannot Attack
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetOperation(s.addop)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_ATTACK)
     c:RegisterEffect(e1)
+    --Filibuster timer Function
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetOperation(s.addop)
+    c:RegisterEffect(e2)
+    --Immune to Wave, Surge, Single Attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+    --return hand (Peon Ability)
+    local e4=Effect.CreateEffect(c)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetTarget(s.destg)
+    e4:SetOperation(s.desop)
+    c:RegisterEffect(e4)
 end
 --Alien SP Tribute Summon Function
 function s.alienfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
+    return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
 end
 function s.spcon(e,c)
-	if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
-	if g then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-	return true
-	end
-	return false
+    local g=Duel.SelectReleaseGroup(tp,s.alienfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+    return true
+    end
+    return false
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g,REASON_COST)
+    g:DeleteGroup()
 end
 --Filibuster timer function
 function s.addop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-	local ct=e:GetValue()
-	ct=ct+1
-	c:SetTurnCounter(ct)
-	e:SetValue(ct)
-	if ct==5 then
+    local ct=e:GetValue()
+    ct=ct+1
+    c:SetTurnCounter(ct)
+    e:SetValue(ct)
+    if ct==10 then
         Duel.Win(tp,0x65)
         if re then re:Reset() end
         ct=0
         c:SetTurnCounter(ct)
         e:SetValue(ct)
-	end
+    end
+end
+--Peon Ability
+function s.destg(e,tp,eg,ev,ep,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.desop(e,tp,eg,ev,ep,re,r,rp)
+    local c=e:GetHandler()
+    local ct=e:GetValue()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
+        ct=0
+        c:SetTurnCounter(ct)
+        e:SetValue(ct)
+    end
 end

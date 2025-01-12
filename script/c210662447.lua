5e18ea5dc9ee206419d38bfe9d7202ff0751bfa2
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-12 22:19:49 +0200

    fix a value

--- script/c210662447.lua
+++ script/c210662447.lua
@@ -75,7 +75,7 @@ function s.addop(e,tp,eg,ep,ev,re,r,rp)
     ct=ct+1
     c:SetTurnCounter(ct)
     e:SetValue(ct)
-    if ct==10 then
+    if ct==15 then
         Duel.Win(tp,0x65)
         if re then re:Reset() end
         ct=0

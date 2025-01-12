f0625b6caf6bd83c1328d5e99b79dd8f6fd8e006
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-12 21:54:42 +0200

    time to start working on filibuster

--- script/c210662447.lua
+++ script/c210662447.lua
@@ -13,16 +13,26 @@ function s.initial_effect(c)
     e0:SetTarget(s.sptg)
     e0:SetOperation(s.spop)
     c:RegisterEffect(e0)
+    --Filibuster timer Function
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetOperation(s.addop)
+    c:RegisterEffect(e1)
 end
+--Alien SP Tribute Summon Function
 function s.alienfilter(c)
-	return c:IsFaceup() and c:IsCode(210662360)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsFaceup()
 end
 function s.spcon(e,c)
 	if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.alienfilter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+	local g=Duel.SelectReleaseGroup(tp,s.alienfilter,1,1,false,true,true,c,nil,nil,false,nil,nil)
 	if g then
 		g:KeepAlive()
 		e:SetLabelObject(g)
@@ -31,8 +41,23 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	return false
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=e:GetLabelObject()
-    if not g then return end
-    Duel.Release(g,REASON_COST)
-    g:DeleteGroup()
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+--Filibuster timer function
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	local ct=e:GetValue()
+	ct=ct+1
+	c:SetTurnCounter(ct)
+	e:SetValue(ct)
+	if ct==5 then
+        Duel.Win(tp,0x65)
+        if re then re:Reset() end
+        ct=0
+        c:SetTurnCounter(ct)
+        e:SetValue(ct)
+	end
 end

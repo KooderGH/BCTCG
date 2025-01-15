6c16dd802446ef83d44802b0e2e91dbdfe8136fa
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-16 00:26:57 +0200

    Rework Gideon de Sable

--- script/c210662123.lua
+++ script/c210662123.lua
@@ -2,9 +2,56 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddUnionProcedure(c,s.floatingfilter)
+    aux.AddUnionProcedure(c,s.floatingfilter)
+    --Wave on Ignition
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(aux.IsUnionState)
+    e1:SetTarget(s.wavetg)
+    e1:SetOperation(s.waveop)
+    c:RegisterEffect(e1)
+    --return hand (Peon Ability)
+    local e2=Effect.CreateEffect(c)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
 end
 --Union filter
 function s.crabfilter(c)
     return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+--Wave on Ignition Function
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    if tc then
+    Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--Peon Ability
+function s.destg(e,tp,eg,ev,ep,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.desop(e,tp,eg,ev,ep,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
+    end
 end

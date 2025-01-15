afb9a5f09428f42986798ef6bcfb08cc63920ffa
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-16 01:23:49 +0200

    Reworked Owlbrow

--- script/c210662037.lua
+++ script/c210662037.lua
@@ -2,46 +2,42 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --draw
+    aux.AddUnionProcedure(c,s.floatingfilter)
+    --Atk up
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,2))
-    e1:SetCategory(CATEGORY_DRAW)	
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetOperation(s.drop)
+    e1:SetType(EFFECT_TYPE_EQUIP)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(200)
+    e1:SetCondition(aux.IsUnionState)
     c:RegisterEffect(e1)
-    --No battle damage
+    --Add Monster
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetTarget(s.addtg)
+    e2:SetOperation(s.addop)
     c:RegisterEffect(e2)
-    --Avoid Battle damage
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-    e3:SetValue(1)
-    c:RegisterEffect(e3)
 end
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=e:GetHandlerPlayer()
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0 or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),c:GetControler(),LOCATION_MZONE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+--Union filter
+function s.floatingfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
 end
---Draw Function
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if Duel.IsPlayerCanDraw(1-tp,1) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-        e1:SetRange(LOCATION_GRAVE)
-        e1:SetCode(EVENT_PHASE+PHASE_DRAW)
-        e1:SetReset(RESET_PHASE+PHASE_END,2)
-        e1:SetCountLimit(1)
-        e1:SetOperation(s.drawop)
-        c:RegisterEffect(e1)
+--Destroy and add function
+function s.addfilter(c)
+    return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToHand()
+end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_GRAVE,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
-end
-function s.drawop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Draw(1-tp,1,REASON_EFFECT)
 end

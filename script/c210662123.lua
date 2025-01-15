2ab0f985f060a453d1435d3cb63cde49a11777d9
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-15 23:40:28 +0200

    Mini white cyclone finished

--- script/c210662123.lua
+++ script/c210662123.lua
@@ -2,28 +2,9 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Guaranteed Wave
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_DESTROY)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_BATTLE_START)
-    e1:SetTarget(s.wavetg)
-    e1:SetOperation(s.waveop)
-    c:RegisterEffect(e1)
+	aux.AddUnionProcedure(c,s.floatingfilter)
 end
-function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
-end
-function s.waveop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local d1=6
-	while d1>3 do
-		d1=Duel.TossDice(tp,1)
-	end
-	local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
-	if tc then
-		Duel.Destroy(tc,REASON_EFFECT)
-	end
+--Union filter
+function s.crabfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
 end
--- script/c210662638.lua
+++ script/c210662638.lua
@@ -2,6 +2,7 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
+    aux.AddUnionProcedure(c,s.floatingfilter)
     --Opponent No Battle Damage
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
@@ -14,4 +15,30 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_ATTACK_ALL)
     e2:SetValue(1)
     c:RegisterEffect(e2)
+    --pos change
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_POSITION)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCondition(aux.IsUnionState)
+    e3:SetTarget(s.postg)
+    e3:SetOperation(s.posop)
+    c:RegisterEffect(e3)
+end
+--Union filter
+function s.crabfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+--Pos Changer
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler():GetEquipTarget(),1,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.ChangePosition(c:GetEquipTarget(),POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
+    end
 end

c6d5f1af185bf798049be5bf224ea7af1114a88c
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-15 23:42:19 +0200

    stringID added too

--- script/c210662638.lua
+++ script/c210662638.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
     --pos change
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetDescription(aux.Stringid(id,0))
     e3:SetCategory(CATEGORY_POSITION)
     e3:SetType(EFFECT_TYPE_IGNITION)
     e3:SetRange(LOCATION_SZONE)

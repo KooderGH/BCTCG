3886d519e11d21f4aabcde9399400d3ffc437042
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-16 02:01:45 +0200

    W v1, W v2 and W v3 on BCETCG

---
+++ script/c210662299.lua
@@ -0,0 +1,10 @@
+-- W v1
+--Scripted by Konstak
+local s,id=GetID()
+function s.initial_effect(c)
+    aux.AddUnionProcedure(c,s.floatingfilter)
+end
+--Union filter
+function s.floatingfilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end

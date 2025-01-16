a96241c6f504c4fb3111efd6bf37359f60a17ed1
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-16 02:29:09 +0200

    Scripted W v2

--- script/c210662300.lua
+++ script/c210662300.lua
@@ -3,8 +3,44 @@
 local s,id=GetID()
 function s.initial_effect(c)
     aux.AddUnionProcedure(c,s.floatingfilter)
+    --once special summoned, SS as many as possible
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.spamtg)
+    e1:SetOperation(s.spamop)
+    c:RegisterEffect(e1)
+    --Atk up
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_EQUIP)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetValue(150)
+    e2:SetCondition(aux.IsUnionState)
+    c:RegisterEffect(e2)
 end
 --Union filter
 function s.floatingfilter(c)
     return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+--Special Summon as many Function
+function s.spfilter(c,e,tp)
+    return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spamtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spamop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
 end

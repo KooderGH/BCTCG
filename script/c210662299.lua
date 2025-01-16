0fca72a7b5bc6d53b88d335b26a1c5004a698cf7
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-16 02:21:27 +0200

    Scripted W v1

--- script/c210662299.lua
+++ script/c210662299.lua
@@ -2,9 +2,59 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    aux.AddUnionProcedure(c,s.floatingfilter)
+    aux.AddUnionProcedure(c,s.unfilter)
+    --Weaken Ability
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetCountLimit(2)
+    e1:SetCondition(aux.IsUnionState)
+    e1:SetTarget(s.weakentg)
+    e1:SetOperation(s.weakenop)
+    c:RegisterEffect(e1)
+    --return hand (Peon Ability)
+    local e2=Effect.CreateEffect(c)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
 end
 --Union filter
-function s.floatingfilter(c)
-    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+function s.unfilter(c)
+	return c:IsMonster() and c:IsFaceup()
+end
+--Weaken
+function s.weakentg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.weakenop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+        e1:SetValue(-tc:GetAttack()/4)
+        tc:RegisterEffect(e1)
+    end
+end
+--Peon Ability
+function s.destg(e,tp,eg,ev,ep,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.desop(e,tp,eg,ev,ep,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
+    end
 end

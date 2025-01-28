0907c38f0ce9c5785db0a77050253eac6bd802e7
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-28 16:57:13 +0200

    Revert brollow to its pre-tourney 1 balance

--- script/c210662207.lua
+++ script/c210662207.lua
@@ -49,7 +49,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if ft<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
-    if #g>0 and Duel.TossCoin(tp,1)==COIN_HEADS then
+    if #g>0 then
         Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end

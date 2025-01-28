8f7c140b8e6d4c24fd9a0f75783f42fd47ad72fe
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-28 18:00:49 +0200

    Doktor K.O scripted

--- script/c210662692.lua
+++ script/c210662692.lua
@@ -2,5 +2,21 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.lpdrainop)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer() and Duel.GetAttackTarget()==nil
+end
+function s.lpdrainop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.Damage(1-tp,250,REASON_EFFECT)
+		Duel.Damage(1-tp,250,REASON_EFFECT)
+		Duel.Damage(1-tp,250,REASON_EFFECT)
+	end
 end

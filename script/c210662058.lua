75266929320e954eee4f51012f6e7b036a3a336f
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-29 19:35:17 +0200

    Update metal enemies wave 4

--- script/c210662058.lua
+++ script/c210662058.lua
@@ -35,16 +35,16 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
     --Metal Mechanic
     local e4=Effect.CreateEffect(c)
-    e4:SetCode(EFFECT_DESTROY_REPLACE)
     e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
     e4:SetRange(LOCATION_MZONE)
+    e4:SetCode(EFFECT_DESTROY_REPLACE)
     e4:SetTarget(s.desatktg)
     c:RegisterEffect(e4)
     --self destroy
     local e5=Effect.CreateEffect(c)
     e5:SetType(EFFECT_TYPE_SINGLE)
-    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
     e5:SetRange(LOCATION_MZONE)
     e5:SetCode(EFFECT_SELF_DESTROY)
     e5:SetCondition(s.sdcon)
@@ -88,16 +88,17 @@ function s.waveop(e,tp,eg,ep,ev,re,r,rp)
         Duel.Destroy(tc,REASON_EFFECT)
     end
 end
---Metal Ability Function
+--Metal Ability
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return c:IsFaceup() end
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_DEFENSE)
-        e1:SetValue(-50)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
-        c:RegisterEffect(e1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetCode(EFFECT_UPDATE_DEFENSE)
+    e1:SetValue(-50)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
+    c:RegisterEffect(e1)
     return true
 end
 function s.sdcon(e)

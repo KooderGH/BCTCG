b4886804bad99dfc84f566d7234cfce104252441
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-29 21:57:38 +0200

    script Wecant

--- script/c210662925.lua
+++ script/c210662925.lua
@@ -2,15 +2,41 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Wave on Battle
+    --Shield Mechanic
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCondition(s.wavecon)
-    e1:SetTarget(s.wavetg)
-    e1:SetOperation(s.waveop)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCountLimit(1)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTarget(s.desatktg)
     c:RegisterEffect(e1)
+    --Wave on Battle
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCondition(s.wavecon)
+    e2:SetTarget(s.wavetg)
+    e2:SetOperation(s.waveop)
+    c:RegisterEffect(e2)
+    --Curse Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DISABLE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.cursetg)
+    e3:SetOperation(s.curseop)
+    c:RegisterEffect(e3)
+end
+--Shield Mechanic
+function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() end
+    return true
 end
 --Wave on Battle Function
 function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
@@ -30,5 +56,26 @@ function s.waveop(e,tp,eg,ep,ev,re,r,rp)
     Duel.NegateAttack()
     if tc then
         Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--Curse Ability Function
+function s.cursetg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsNegatable() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+    local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.curseop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and (tc:IsFaceup() and not tc:IsDisabled()) and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_PHASE+PHASE_END,2)
+        tc:RegisterEffect(e1)
     end
 end

5a43797aafb2f0361c0f65ca9de41c78b1a977b0
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-29 04:20:32 +0200

    Exiel Nerf and Mr. Angel rework

--- script/c210662254.lua
+++ script/c210662254.lua
@@ -14,17 +14,31 @@ function s.initial_effect(c)
     local e2=e1:Clone()
     e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e2)
-    --Weaken Ability
+    --once normal summoned, SS as many Zollows as possible
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e3:SetCategory(CATEGORY_DISABLE)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_MZONE)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_SUMMON_SUCCESS)
     e3:SetCountLimit(1)
-    e3:SetTarget(s.weakentg)
-    e3:SetOperation(s.weakenop)
+    e3:SetTarget(s.spamtg)
+    e3:SetOperation(s.spamop)
     c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+    --Weaken Ability
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e5:SetCategory(CATEGORY_DISABLE)
+    e5:SetType(EFFECT_TYPE_IGNITION)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1)
+    e5:SetTarget(s.weakentg)
+    e5:SetOperation(s.weakenop)
+    c:RegisterEffect(e5)
 end
 --When NS add function
 function s.dfilter(c)
@@ -38,8 +52,26 @@ function s.srop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_GRAVE,0,2,2,nil)
     if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+--Special summon
+function s.spfilter(c,e,tp)
+    return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spamtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spamop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 --Weaken
--- script/c210662697.lua
+++ script/c210662697.lua
@@ -10,8 +10,8 @@ function s.initial_effect(c)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCountLimit(1)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetTarget(s.spamtg)
+    e1:SetOperation(s.spamop)
     c:RegisterEffect(e1)
     local e2=e1:Clone()
     e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -32,20 +32,20 @@ function s.initial_effect(c)
     e4:SetRange(LOCATION_MZONE)
     e4:SetCode(EVENT_PHASE+PHASE_END)
     e4:SetCountLimit(1)
-    e4:SetTarget(s.sptg)
-    e4:SetOperation(s.spop)
+    e4:SetTarget(s.spamtg)
+    e4:SetOperation(s.spamop)
     c:RegisterEffect(e4)
 end
 --Special summon
 function s.spfilter(c,e,tp)
 	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spamtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spamop(e,tp,eg,ep,ev,re,r,rp)
     local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
     if ft<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)

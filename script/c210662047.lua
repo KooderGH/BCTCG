a24b7e07028c7ff8fccceda465db2fc65b3307bb
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-29 18:19:30 +0200

    metal enemy rework wave 1

--- script/c210662047.lua
+++ script/c210662047.lua
@@ -16,45 +16,59 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
     --Metal Mechanic
     local e3=Effect.CreateEffect(c)
-    e3:SetCode(EFFECT_DESTROY_REPLACE)
     e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
     e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_DESTROY_REPLACE)
     e3:SetTarget(s.desatktg)
     c:RegisterEffect(e3)
     --self destroy
     local e4=Effect.CreateEffect(c)
     e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
     e4:SetRange(LOCATION_MZONE)
     e4:SetCode(EFFECT_SELF_DESTROY)
     e4:SetCondition(s.sdcon)
     c:RegisterEffect(e4)
-    --return hand (Peon Ability)
+    --Cannot be targeted (Metal Coat)
     local e5=Effect.CreateEffect(c)
-    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
-    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e5:SetCode(EVENT_DESTROYED)
-    e5:SetTarget(s.destg)
-    e5:SetOperation(s.desop)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetValue(1)
     c:RegisterEffect(e5)
 end
---When NS add function
-function s.dfilter(c)
-    return c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
-end
+--Excavate Search Ability
 function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_DECK,0,2,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.filter(c)
+	return c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
 function s.srop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_DECK,0,2,2,nil)
-    if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-    end
+	--Effect
+	local c=e:GetHandler()
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<5 then return end
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	Duel.DisableShuffleCheck()
+	if g:IsExists(s.filter,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:FilterSelect(tp,s.filter,1,2,nil)
+		if #tg>0 then
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tg)
+			Duel.ShuffleHand(tp)
+			g:RemoveCard(tg)
+		end
+	end
+	local ct=#g
+	if ct>0 then
+		Duel.MoveToDeckTop(g,tp)
+		Duel.SortDecktop(tp,tp,ct)
+	end
 end
 --Metal Ability
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -62,6 +76,7 @@ function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return c:IsFaceup() end
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     e1:SetCode(EFFECT_UPDATE_DEFENSE)
     e1:SetValue(-50)
     e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
@@ -71,15 +86,4 @@ end
 function s.sdcon(e)
     local c=e:GetHandler()
     return c:GetDefense()<=0
-end
---Peon Ability
-function s.destg(e,tp,eg,ev,ep,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
-end
-function s.desop(e,tp,eg,ev,ep,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.SendtoHand(c,nil,REASON_EFFECT)
-    end
 end
--- script/c210662497.lua
+++ script/c210662497.lua
@@ -2,65 +2,49 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --to defense
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_POSITION)
-    e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetTarget(s.deftg)
-    e1:SetOperation(s.defop)
-    c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-    c:RegisterEffect(e2)
-    local e3=e1:Clone()
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    c:RegisterEffect(e3)
     --Metal Mechanic
-    local e4=Effect.CreateEffect(c)
-    e4:SetCode(EFFECT_DESTROY_REPLACE)
-    e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetTarget(s.desatktg)
-    c:RegisterEffect(e4)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTarget(s.desatktg)
+    c:RegisterEffect(e1)
     --self destroy
-    local e5=Effect.CreateEffect(c)
-    e5:SetType(EFFECT_TYPE_SINGLE)
-    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e5:SetRange(LOCATION_MZONE)
-    e5:SetCode(EFFECT_SELF_DESTROY)
-    e5:SetCondition(s.sdcon)
-    c:RegisterEffect(e5)
-    --Add lv4 Monster
-    local e6=Effect.CreateEffect(c)
-    e6:SetDescription(aux.Stringid(id,1))
-    e6:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e6:SetCode(EVENT_DESTROYED)
-    e6:SetTarget(s.drtg)
-    e6:SetOperation(s.drop)
-    c:RegisterEffect(e6)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCode(EFFECT_SELF_DESTROY)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCondition(s.sdcon)
+    c:RegisterEffect(e2)
+    --Cannot be targeted (Metal Coat)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+    --Excavate (Search Ability)
+    local e4=Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.srtg)
+    e4:SetOperation(s.srop)
+    c:RegisterEffect(e4)
 end
---to Defence
-function s.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chk==0 then return e:GetHandler():IsAttackPos() end
-    Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
-end
-function s.defop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
-        Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
-    end
-end
---Metal Ability function
+--Metal Ability
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return c:IsFaceup() end
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     e1:SetCode(EFFECT_UPDATE_DEFENSE)
     e1:SetValue(-50)
     e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE)
@@ -71,19 +55,34 @@ function s.sdcon(e)
     local c=e:GetHandler()
     return c:GetDefense()<=0
 end
---Destroy and add function
-function s.drfilter(c)
-    return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+--Excavate Search Ability
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,2,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.filter(c)
+	return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,2,2,nil)
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local c=e:GetHandler()
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<5 then return end
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	Duel.DisableShuffleCheck()
+	if g:IsExists(s.filter,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:FilterSelect(tp,s.filter,1,2,nil)
+		if #tg>0 then
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tg)
+			Duel.ShuffleHand(tp)
+			g:RemoveCard(tg)
+		end
+	end
+	local ct=#g
+	if ct>0 then
+		Duel.MoveToDeckTop(g,tp)
+		Duel.SortDecktop(tp,tp,ct)
+	end
 end

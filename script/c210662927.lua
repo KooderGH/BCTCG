d791d19c13c03599455be580014c0f4105c87e37
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-29 23:58:26 +0200

    Sir rel update

--- script/c210662927.lua
+++ script/c210662927.lua
@@ -18,7 +18,8 @@ function s.initial_effect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_BATTLE_START)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.wavecon)
     e1:SetTarget(s.wavetg)
     e1:SetOperation(s.waveop)
     c:RegisterEffect(e1)
@@ -79,18 +80,21 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
     g:DeleteGroup()
 end
 --Wave Ability Function
+function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
 function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
 function s.waveop(e,tp,eg,ep,ev,re,r,rp)
     if not e:GetHandler():IsRelateToEffect(e) then return end
-    Duel.NegateAttack()
     local d1=6
     while d1>3 do
         d1=Duel.TossDice(tp,1)
     end
     local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    Duel.NegateAttack()
     if tc then
         Duel.Destroy(tc,REASON_EFFECT)
     end

8b1aa07da17f3152daac154f7e495f39b5e7c5dd
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-30 05:44:39 +0200

    remove Virus control from Sir Metal Seal

--- script/c210662059.lua
+++ script/c210662059.lua
@@ -2,51 +2,30 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Can Take control machine monsters
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,1))
-    e1:SetCategory(CATEGORY_CONTROL)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_BATTLE_START)
-    e1:SetTarget(s.tctg)
-    e1:SetOperation(s.tcop)
-    c:RegisterEffect(e1)
     --Metal Mechanic
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_DESTROY_REPLACE)
+    e1:SetTarget(s.desatktg)
+    c:RegisterEffect(e1)
+    --self destroy
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EFFECT_DESTROY_REPLACE)
-    e2:SetTarget(s.desatktg)
+    e2:SetCode(EFFECT_SELF_DESTROY)
+    e2:SetCondition(s.sdcon)
     c:RegisterEffect(e2)
-    --self destroy
+    --Cannot be targeted (Metal Coat)
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetCode(EFFECT_SELF_DESTROY)
-    e3:SetCondition(s.sdcon)
+    e3:SetValue(1)
     c:RegisterEffect(e3)
-    --Cannot be targeted (Metal Coat)
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetValue(1)
-    c:RegisterEffect(e4)
-end
---Take control Machine function
-function s.tctg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() and bc:IsRace(RACE_MACHINE) and bc:IsControlerCanBeChanged() end
-    Duel.SetOperationInfo(0,CATEGORY_CONTROL,bc,1,0,0)
-end
-function s.tcop(e,tp,eg,ep,ev,re,r,rp)
-    local bc=e:GetHandler():GetBattleTarget()
-    if bc:IsRelateToBattle() then
-        Duel.GetControl(bc,tp)
-    end
 end
 --Metal Ability
 function s.desatktg(e,tp,eg,ep,ev,re,r,rp,chk)

cadfe6e8bf9bda439d29865470795f50391b53c2
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-30 06:21:01 +0200

    script Purple Bot

--- script/c210662930.lua
+++ script/c210662930.lua
@@ -2,5 +2,82 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    --Hide
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.spelltarget)
+    e1:SetOperation(s.spellop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+    --Defense Up Ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_FIELD)
+    e4:SetRange(LOCATION_MZONE+LOCATION_SZONE)
+    e4:SetTargetRange(LOCATION_MZONE,0)
+    e4:SetCode(EFFECT_UPDATE_DEFENSE)
+    e4:SetTarget(s.deftg)
+    e4:SetValue(150)
+    c:RegisterEffect(e4)
+    --Revive
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+    e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e5:SetCode(EVENT_PHASE+PHASE_END)
+    e5:SetRange(LOCATION_GRAVE)
+    e5:SetCost(s.sumcost)
+    e5:SetCondition(s.sumcon)
+    e5:SetTarget(s.sumtg)
+    e5:SetOperation(s.sumop)
+    c:RegisterEffect(e5)
+end
+--Hide Function
+function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.spellop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
+end
+--Attack Up function
+function s.deftg(e,c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_CYBERSE)
+end
+--Revive Function
+function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckLPCost(tp,500) end
+    Duel.PayLPCost(tp,500)
+end
+function s.sumcon(e,tp,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+	end
 end

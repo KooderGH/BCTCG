c23e4011a9abda5d95a56fdccea1dc68655f3c50
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-30 06:32:53 +0200

    buff Dictator Wizadron, fix some text

--- script/c210662907.lua
+++ script/c210662907.lua
@@ -60,10 +60,10 @@ function s.dictactorwizadronfilter(c)
 end
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.CheckReleaseGroup(c:GetControler(),s.dictactorwizadronfilter,2,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+    return Duel.CheckReleaseGroup(c:GetControler(),s.dictactorwizadronfilter,3,false,1,true,c,c:GetControler(),nil,false,nil,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-    local g=Duel.SelectReleaseGroup(tp,s.dictactorwizadronfilter,2,2,false,true,true,c,nil,nil,false,nil,nil)
+    local g=Duel.SelectReleaseGroup(tp,s.dictactorwizadronfilter,3,3,false,true,true,c,nil,nil,false,nil,nil)
     if g then
         g:KeepAlive()
         e:SetLabelObject(g)

2b3d9f9f7b46111bc66bd2955bb01ea0c6be35a2
poka-poka <regaalfiansyah@gmail.com>
2025-02-08 23:36:49 +0700

    Dark Heroes Emergency Call(79)

---
+++ script/c210666003.lua
@@ -0,0 +1,34 @@
+-- Dark Heroes Emergency Call
+-- Scripted by poka-poka
+-- Effect: If a monster is destroyed by battle or card effect; Special Summon 1 DARK warrior monster from your deck.
+local s,id=GetID()
+function s.initial_effect(c)
+    -- (1) Special Summon a DARK Warrior when a monster is destroyed
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_DESTROYED)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(Card.IsType,1,nil,TYPE_MONSTER)
+end
+function s.spfilter(c,e,tp)
+    return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end

d0f7bcafb5bf7686272a1e72e941b99206524901
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-26 20:10:40 +0200

    R. Ost rework, New effect

--- script/c210662032.lua
+++ script/c210662032.lua
@@ -2,15 +2,36 @@
 --Scripted By Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Critical Ability
+    --Traitless Ability
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_BATTLE_START)
-    e1:SetTarget(s.crittg)
-    e1:SetOperation(s.critop)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.stval)
     c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e2)
+    --Critical Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLE_START)
+    e3:SetTarget(s.crittg)
+    e3:SetOperation(s.critop)
+    c:RegisterEffect(e3)
+end
+--Traitless Ability Function
+function s.stval(e,c)
+    local tp=c:GetControler()
+    local v=Duel.GetLP(tp)
+    if v>=10000 then return 500 end
+    if v>=7000 and v<10000 then return 300 end
+    if v>=4000 and v<7000 then return 300 end
+    if v>=2000 and v<4000 then return 200 end
+    if v>=0 and v<2000 then return 100 end
 end
 --Critical Ability Function
 function s.crittg(e,tp,eg,ep,ev,re,r,rp,chk)

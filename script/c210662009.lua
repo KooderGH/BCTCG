c8accbdef653f8c103541f5675a14a8b88c5be73
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-26 20:53:53 +0200

    Rework All level 5 Traitless

--- script/c210662009.lua
+++ script/c210662009.lua
@@ -2,5 +2,59 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-
+    c:EnableUnsummonable()
+    --Special Summon Requirement
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+    --Traitless Ability
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.stval)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e2)
+end
+--Summon Requirement Function
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.filter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.filter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+--Traitless Ability Function
+function s.stval(e,c)
+    local tp=c:GetControler()
+    local v=Duel.GetLP(tp)
+    if v>=10000 then return 500 end
+    if v>=7000 and v<10000 then return 300 end
+    if v>=4000 and v<7000 then return 300 end
+    if v>=2000 and v<4000 then return 200 end
+    if v>=0 and v<2000 then return 100 end
 end
--- script/c210662035.lua
+++ script/c210662035.lua
@@ -2,15 +2,70 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Base Destroyer Ability
+    c:EnableUnsummonable()
+    --Special Summon Requirement
+    local e0=Effect.CreateEffect(c)
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetRange(LOCATION_HAND)
+    e0:SetCode(EFFECT_SPSUMMON_PROC)
+    e0:SetCondition(s.spcon)
+    e0:SetTarget(s.sptg)
+    e0:SetOperation(s.spop)
+    c:RegisterEffect(e0)
+    --Traitless Ability
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,1))
-    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCondition(function() return Duel.GetAttackTarget()==nil end)
-    e1:SetOperation(s.desop)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.stval)
     c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e2)
+    --Base Destroyer Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(function() return Duel.GetAttackTarget()==nil end)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
+end
+--Summon Requirement Function
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+    return Duel.CheckReleaseGroup(c:GetControler(),s.filter,1,false,1,true,c,c:GetControler(),nil,false,nil,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.filter,1,1,false,true,true,c,nil,nil,false,nil,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+--Traitless Ability Function
+function s.stval(e,c)
+    local tp=c:GetControler()
+    local v=Duel.GetLP(tp)
+    if v>=10000 then return 500 end
+    if v>=7000 and v<10000 then return 300 end
+    if v>=4000 and v<7000 then return 300 end
+    if v>=2000 and v<4000 then return 200 end
+    if v>=0 and v<2000 then return 100 end
 end
 --Base Destroyer Function
 function s.desop(e,tp,eg,ep,ev,re,r,rp)

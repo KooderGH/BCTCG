246d183dcc0df97a872082cccb72da8893bd40a8
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-26 19:25:11 +0200

    Finish Master A and Teacher A

--- script/c210662031.lua
+++ script/c210662031.lua
@@ -3,16 +3,27 @@
 local s,id=GetID()
 function s.initial_effect(c)
     --Traitless Ability
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetCode(EFFECT_UPDATE_ATTACK)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetValue(s.stval)
-    c:RegisterEffect(e4)
-    local e5=e4:Clone()
-    e5:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e5)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.stval)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e2)
+    --Excavate (Search Ability)
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.srtg)
+    e3:SetOperation(s.srop)
+    c:RegisterEffect(e3)
 end
 --Traitless Ability Function
 function s.stval(e,c)
@@ -23,4 +34,35 @@ function s.stval(e,c)
     if v>=4000 and v<7000 then return 300 end
     if v>=2000 and v<4000 then return 200 end
     if v>=0 and v<2000 then return 100 end
+end
+--Excavate Search Ability
+function s.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.filter(c)
+	return c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
+end
+function s.srop(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local c=e:GetHandler()
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<5 then return end
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	Duel.DisableShuffleCheck()
+	if g:IsExists(s.filter,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:FilterSelect(tp,s.filter,1,2,nil)
+		if #tg>0 then
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tg)
+			Duel.ShuffleHand(tp)
+			g:RemoveCard(tg)
+		end
+	end
+	local ct=#g
+	if ct>0 then
+		Duel.MoveToDeckTop(g,tp)
+		Duel.SortDecktop(tp,tp,ct)
+	end
 end

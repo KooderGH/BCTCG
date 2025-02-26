6a1d29326619f504664a055fd9cad1a9e726bd2f
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-26 16:33:30 +0200

    Buff level 3 and below traitless

--- script/c210662015.lua
+++ script/c210662015.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--spsummon
 	local e0=Effect.CreateEffect(c)
-	e0:SetDescription(aux.Stringid(12423762,0))
+	e0:SetDescription(aux.Stringid(0))
 	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e0:SetCode(EVENT_ATTACK_ANNOUNCE)
@@ -13,6 +13,15 @@ function s.initial_effect(c)
 	e0:SetTarget(s.sptg)
 	e0:SetOperation(s.spop)
 	c:RegisterEffect(e0)
+	--Damage Control
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.dcop)
+	c:RegisterEffect(e1)
 end
 --Special Summon
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
@@ -32,4 +41,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
+end
+--Damage Control effect
+function s.dcop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end

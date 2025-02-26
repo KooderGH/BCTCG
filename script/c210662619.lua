0eaaa1ad73f3b3ccc92384ce2d0ebc407fd33436
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-26 21:08:37 +0200

    Script Marmod (White)

--- script/c210662619.lua
+++ script/c210662619.lua
@@ -13,6 +13,15 @@ function s.initial_effect(c)
     local e2=e1:Clone()
     e2:SetCode(EFFECT_UPDATE_DEFENSE)
     c:RegisterEffect(e2)
+    --Toxic Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(s.toxiccon)
+    e3:SetTarget(s.toxictg)
+    e3:SetOperation(s.toxicop)
+    c:RegisterEffect(e3)
 end
 --Traitless Ability Function
 function s.stval(e,c)
@@ -23,4 +32,25 @@ function s.stval(e,c)
     if v>=4000 and v<7000 then return 300 end
     if v>=2000 and v<4000 then return 200 end
     if v>=0 and v<2000 then return 100 end
+end
+--Toxic on Battle function
+function s.toxiccon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.toxictg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local bc=e:GetHandler():GetBattleTarget()
+    if chk==0 then return bc and bc:IsFaceup() end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.toxicop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetHandler():GetBattleTarget()
+    if tc:IsRelateToBattle() and tc and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
+        Duel.NegateAttack()
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-500)
+        tc:RegisterEffect(e1)
+    end
 end

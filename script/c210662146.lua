a144e4012e957b81f8fd005395e8f3040952c8f3
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-27 20:27:34 +0200

    Trolly Blogger and Mr Mole scripted

--- script/c210662146.lua
+++ script/c210662146.lua
@@ -2,37 +2,48 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Traitless Ability
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(s.stval)
+    --once normal summoned, SS as many as possible
+    local e0=Effect.CreateEffect(c)
+    e0:SetDescription(aux.Stringid(id,0))
+    e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e0:SetProperty(EFFECT_FLAG_DELAY)
+    e0:SetCode(EVENT_SUMMON_SUCCESS)
+    e0:SetCountLimit(1)
+    e0:SetTarget(s.spamtg)
+    e0:SetOperation(s.spamop)
+    c:RegisterEffect(e0)
+    local e1=e0:Clone()
+    e1:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e2)
     --Knockback ability
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetCategory(CATEGORY_DISABLE)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e3:SetCondition(s.knockbackcon)
-    e3:SetTarget(s.knockbacktg)
-    e3:SetOperation(s.knockbackop)
-    c:RegisterEffect(e3)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCondition(s.knockbackcon)
+    e2:SetTarget(s.knockbacktg)
+    e2:SetOperation(s.knockbackop)
+    c:RegisterEffect(e2)
 end
---Traitless Ability Function
-function s.stval(e,c)
-    local tp=c:GetControler()
-    local v=Duel.GetLP(tp)
-    if v>=10000 then return 500 end
-    if v>=7000 and v<10000 then return 400 end
-    if v>=4000 and v<7000 then return 300 end
-    if v>=2000 and v<4000 then return 200 end
-    if v>=0 and v<2000 then return 100 end
+--Special summon
+function s.spfilter(c,e,tp)
+    return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spamtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spamop(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
 --Knockback function
 function s.knockbackcon(e,tp,eg,ep,ev,re,r,rp)
--- script/c210662235.lua
+++ script/c210662235.lua
@@ -2,58 +2,68 @@
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
-    --Traitless Ability
+    --Hide
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(s.stval)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.spelltarget)
+    e1:SetOperation(s.spellop)
     c:RegisterEffect(e1)
     local e2=e1:Clone()
-    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
-    --Knockback ability
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetCategory(CATEGORY_DISABLE)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e3:SetCondition(s.knockbackcon)
-    e3:SetTarget(s.knockbacktg)
-    e3:SetOperation(s.knockbackop)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e3)
+    --Knockback ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DISABLE)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE+LOCATION_SZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.knockbacktg)
+    e4:SetOperation(s.knockbackop)
+    c:RegisterEffect(e4)
 end
---Traitless Ability Function
-function s.stval(e,c)
-    local tp=c:GetControler()
-    local v=Duel.GetLP(tp)
-    if v>=10000 then return 500 end
-    if v>=7000 and v<10000 then return 400 end
-    if v>=4000 and v<7000 then return 300 end
-    if v>=2000 and v<4000 then return 200 end
-    if v>=0 and v<2000 then return 100 end
+--Hide Function
+function s.spelltarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.spellop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local c=e:GetHandler()
+    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCode(EFFECT_CHANGE_TYPE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+    e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+    c:RegisterEffect(e1)
 end
 --Knockback function
-function s.knockbackcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()==tp
-end
-function s.knockbacktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local bc=e:GetHandler():GetBattleTarget()
-    if chk==0 then return bc and bc:IsFaceup() end
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+function s.knockbacktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return true end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function s.knockbackop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local tc=e:GetHandler():GetBattleTarget()
-    if tc:IsRelateToBattle() and tc and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
         local e1=Effect.CreateEffect(c)
         e1:SetDescription(3206)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_CANNOT_ATTACK)
         e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
         tc:RegisterEffect(e1)
-        Duel.NegateAttack()
     end
 end

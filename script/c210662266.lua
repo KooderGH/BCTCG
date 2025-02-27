8228af8b3f8e9fd33cbe1a1ebeab3616dea1db28
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-02-27 18:37:26 +0200

    Dober P.D, Ms Madonna and Henry Scripted

--- script/c210662266.lua
+++ script/c210662266.lua
@@ -13,6 +13,13 @@ function s.initial_effect(c)
     local e2=e1:Clone()
     e2:SetCode(EFFECT_UPDATE_DEFENSE)
     c:RegisterEffect(e2)
+    --Freeze Ability
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(s.freezecon)
+    e3:SetOperation(s.freezeop)
+    c:RegisterEffect(e3)
 end
 --Traitless Ability Function
 function s.stval(e,c)
@@ -23,4 +30,48 @@ function s.stval(e,c)
     if v>=4000 and v<7000 then return 300 end
     if v>=2000 and v<4000 then return 200 end
     if v>=0 and v<2000 then return 100 end
+end
+--Freeze Function
+function s.freezecon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRelateToBattle() and Duel.GetTurnPlayer()==tp
+end
+function s.freezeop(e,tp,eg,ep,ev,re,r,rp)
+    local effp=e:GetHandler():GetControler()
+    local c=e:GetHandler()
+    if c:IsFaceup() and c:IsRelateToEffect(e) and Duel.TossCoin(tp,1)==COIN_HEADS then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_SKIP_DP)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(0,1)
+        if Duel.GetTurnPlayer()==effp then
+            e1:SetLabel(Duel.GetTurnCount())
+            e1:SetCondition(s.skipcon)
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+        else
+            e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+        end
+        Duel.RegisterEffect(e1,effp)
+        local e2=Effect.CreateEffect(e:GetHandler())
+        e2:SetCategory(CATEGORY_DRAW)
+        e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e2:SetCode(EVENT_PHASE+PHASE_END)
+        e2:SetCondition(s.retcon)
+        e2:SetLabel(Duel.GetTurnCount())
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+        e2:SetCountLimit(1)
+        e2:SetOperation(s.droperation)
+        Duel.RegisterEffect(e2,effp)
+        Duel.NegateAttack()
+    end
+end
+function s.skipcon(e)
+    return Duel.GetTurnCount()~=e:GetLabel()
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function s.droperation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Draw(1-tp,1,REASON_EFFECT)
 end
--- script/c210662442.lua
+++ script/c210662442.lua
@@ -1,4 +1,4 @@
--- Henry
+-- Ms Madonna
 --Scripted by Konstak
 local s,id=GetID()
 function s.initial_effect(c)
@@ -13,6 +13,27 @@ function s.initial_effect(c)
     local e2=e1:Clone()
     e2:SetCode(EFFECT_UPDATE_DEFENSE)
     c:RegisterEffect(e2)
+    --Wave on Battle
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DISABLE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(s.wavecon)
+    e3:SetTarget(s.wavetg)
+    e3:SetOperation(s.waveop)
+    c:RegisterEffect(e3)
+    --Knockback ability
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_DISABLE)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.knockbacktg)
+    e4:SetOperation(s.knockbackop)
+    c:RegisterEffect(e4)
 end
 --Traitless Ability Function
 function s.stval(e,c)
@@ -23,4 +44,44 @@ function s.stval(e,c)
     if v>=4000 and v<7000 then return 300 end
     if v>=2000 and v<4000 then return 200 end
     if v>=0 and v<2000 then return 100 end
+end
+--Wave on Battle Function
+function s.wavecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp
+end
+function s.wavetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.waveop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local d1=6
+    while d1>3 do
+        d1=Duel.TossDice(tp,1)
+    end
+    local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,d1)
+    Duel.NegateAttack()
+    if tc then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
+--Knockback function
+function s.knockbacktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return true end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.knockbackop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetDescription(3206)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CANNOT_ATTACK)
+        e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+        tc:RegisterEffect(e1)
+    end
 end

a80bb1b2cdfc935a089c305104576386125007ff
Kooder <81496357+KooderGH@users.noreply.github.com>
2025-09-14 19:41:46 +0800

    Awakened Doron Scripted

---
+++ script/c210661613.lua
@@ -0,0 +1,57 @@
+--Awakened Doron
+--Scripted by Kooder
+--Effects:
+-- This card can only be Ritual Summoned with the Ritual Spell Card, "Awaken thy Cat!".
+-- When this card is Ritual Summoned; Destroy all cards on the field. Cards and effects cannot be activated in response to this effect's activation.
+-- If this card is Special Summoned from the GY; Your opponent gains 4000 LP.
+local s,id=GetID()
+function s.initial_effect(c)
+-- destroy all cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCost(Cost.SelfTribute)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+-- spsummon from GY; opponent gains 4000 LP
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.spsummon)
+	e2:SetTarget(s.target1)
+	e2:SetOperation(s.activate1)
+	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRitualSummoned()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetChainLimit(aux.FALSE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.Destroy(g,REASON_EFFECT)
+end
+function s.spsummon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonLocation(LOCATION_GRAVE)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(4000)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,4000)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end

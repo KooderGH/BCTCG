e70574a5c1d64564d51a429ed330f5cf85112ef1
Kooder <81496357+KooderGH@users.noreply.github.com>
2025-09-15 20:25:48 +0800

    Awakened Musashi Scripted

---
+++ script/c210661323.lua
@@ -0,0 +1,147 @@
+--Awakened Musashi
+--Scripted by Kooder
+--Effects:
+-- This card can only be Ritual Summoned with the Ritual Spell Card, "Awaken thy Cat!".
+-- If this card was Ritual Summoned using only FIRE Warrior Monsters, apply the following effect when Summoned; Add up to 3 Equip Cards from your deck, GY, or banish to your hand.
+-- This card gains the following effect(s) based on the number of Equip Cards equipped to it:
+-- 1+: Once per turn (Ignition): Target 1 monster on the field: Lower it's attack by 1000.
+-- 2+: You can send 1 Equip Card you control that is equipped to this card to the GY, then target 1 FIRE Warrior monster in your GY; Special Summon it.
+-- 3+: This card gains 500 ATK/DEF for each Face-up Spell Card on the field.
+local s,id=GetID()
+function s.initial_effect(c)
+-- When summoned with only FIRE Warrior Monsters, add up to 3 Equip Cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(s.matcheck)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+-- 1+: Lower attack by 1000
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.eqcount)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.activate)
+	c:RegisterEffect(e3)
+-- 2+: 1 send equip to GY; spsummon 1 FIRE Warrior Monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.eqcount1)
+	e4:SetCost(s.spcost)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+-- 3+: 500 ATK/DEF for each Face-up Spell Card
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.eqcount2)
+	e5:SetValue(s.val)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e6)
+end
+local LOCATIONS_REMOVED_HAND_DECK_GRAVE=LOCATION_REMOVED|LOCATION_DECK|LOCATION_GRAVE
+function s.eqfilter(c)
+	return c:IsEquipSpell()
+end
+function s.fwfilter(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE)
+end
+function s.matcheck(e,c)
+	local c=e:GetHandler()
+	local g=c:GetMaterial()
+	local lg=g:Filter(s.fwfilter,nil,nil)
+	if #g==#lg then
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD&~(RESET_TOFIELD|RESET_LEAVE|RESET_TEMP_REMOVE),0,1)
+	end
+end
+function s.eqcount(e)
+    local c=e:GetHandler()
+	return c:GetEquipCount()>=1
+end
+function s.eqcount1(e)
+    local c=e:GetHandler()
+	return c:GetEquipCount()>=2
+end
+function s.eqcount2(e)
+    local c=e:GetHandler()
+	return c:GetEquipCount()>=3
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsRitualSummoned() and c:GetFlagEffect(id)~=0
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATIONS_REMOVED_HAND_DECK_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATIONS_REMOVED_HAND_DECK_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATIONS_REMOVED_HAND_DECK_GRAVE,0,1,3,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_UPDATE_ATTACK)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetValue(-1000)
+	e4:SetReset(RESET_EVENT|RESETS_STANDARD)
+	tc:RegisterEffect(e4)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetEquipGroup():IsExists(Card.IsAbleToGraveAsCost,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=e:GetHandler():GetEquipGroup():FilterSelect(tp,Card.IsAbleToGraveAsCost,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.fwfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.fwfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.fwfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.val(e,c)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsSpell),0,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)*500
+end
